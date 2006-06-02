5720f40ef3556f68679745939ca809f06207c2cc
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-06-02 21:21:53 +0000

    Needed for Min/Max definitions for Ceylan basic numerical datatypes.

---
+++ src/code/generic/CeylanTypes.cc
@@ -0,0 +1,30 @@
+#include "CeylanTypes.h"
+
+
+Ceylan::Sint8 Sint8Min = -128 ;
+Ceylan::Sint8 Sint8Max =  127 ;
+
+Ceylan::Uint8 Uint8Min = 0   ;
+Ceylan::Uint8 Uint8Max = 255 ;
+
+Ceylan::Sint16 Sint16Min = -32768 ;
+Ceylan::Sint16 Sint16Max =  32767 ;
+
+Ceylan::Uint16 Uint16Min =     0 ;
+Ceylan::Uint16 Uint16Max = 65535 ;
+
+Ceylan::Sint32 Sint32Min = -2147483647 ;
+Ceylan::Sint32 Sint32Max =  2147483647 ;
+
+Ceylan::Uint32 Uint32Min =          0 ;
+Ceylan::Uint32 Uint32Max = 2147483647 ; // should have been : 4294967294
+
+Ceylan::Float32 Float32Min = -3.4E-38 ;
+Ceylan::Float32 Float32Max =  3.4E38  ;
+
+Ceylan::Float64 Float64Min = -1.7E-308 ;
+Ceylan::Float64 Float64Max =  1.7E308  ;
+
+Ceylan::Float80 Float80Min = -3.4E-4932 ;
+Ceylan::Float80 Float80Max = 1.7E308    ; // should have been : 3.4E4932
+
