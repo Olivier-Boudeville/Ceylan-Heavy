0e95348b2860d1ce340bcd7908aaf6a04c2c272c
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-06-15 11:17:12 +0000

    float, double, long double replaced by their Ceylan counterparts, IsNull and AreEqual methods improved.

--- src/code/maths/CeylanMathsBasic.h
+++ src/code/maths/CeylanMathsBasic.h
@@ -1,5 +1,5 @@
-#ifndef CEYLAN_MATH_BASIC_H_
-#define CEYLAN_MATH_BASIC_H_
+#ifndef CEYLAN_MATHS_BASIC_H_
+#define CEYLAN_MATHS_BASIC_H_
 
 
 #include "CeylanTextDisplayable.h"  // for TextDisplayable
@@ -24,7 +24,7 @@ namespace Ceylan
 		 *
 		 * Instead of standard variables (ex : M_E), their GNU counterparts
 		 * (ex : M_El) are used, since their accuracy (precision) is better :
-		 * they are adequate for computation using `long double'.
+		 * they are adequate for computation using `Ceylan::LongFloat'.
 		 * 
 		 * Since depending on a GNU set of constants would not be portable
 		 * enough, their values are explicitly set here, one time for all.
@@ -69,7 +69,11 @@ namespace Ceylan
 		typedef Ceylan::Float32 AngleInRadians ;
 		
 		
-		/// Percentage values, which should range from 0 to 100, both included.
+		/**
+		 * Integer percentage values, which should range from 0 to 100, 
+		 * both included.
+		 *
+		 */
 		typedef Ceylan::Uint8 Percentage ;
 		
 		
@@ -86,7 +90,12 @@ namespace Ceylan
 		
 		
 		
-		// Section related to the e constant.
+		/*
+		 * Section related to most useful constants.
+		 *
+		 * @note The LongFloat type is used to decrease numerical errors.
+		 *
+		 */
 		
 		
 		/**
@@ -94,7 +103,7 @@ namespace Ceylan
 		 * (used to be the M_E maths.h constant).
 		 *
 		 */
-		const long double E = 2.7182818284590452353602874713526625L ;
+		const Ceylan::LongFloat E = 2.7182818284590452353602874713526625L ;
 
 
 		/**
@@ -102,7 +111,7 @@ namespace Ceylan
 		 * (used to be the M_LOG2E maths.h constant).
 		 *
 		 */
-		const long double Log2E = 1.4426950408889634073599246810018922L ;
+		const Ceylan::LongFloat Log2E = 1.4426950408889634073599246810018922L ;
 	
 	
 		/**
@@ -110,7 +119,7 @@ namespace Ceylan
 		 * (used to be the M_LOG10E maths.h constant).
 		 *
 		 */
-		const long double Log10E = 0.4342944819032518276511289189166051L ;
+		const Ceylan::LongFloat Log10E = 0.4342944819032518276511289189166051L ;
 	
 	
 		/**
@@ -118,7 +127,7 @@ namespace Ceylan
 		 * (used to be the M_LN2 maths.h constant).
 		 *
 		 */
-		const long double LogE2 = 0.6931471805599453094172321214581766L ;
+		const Ceylan::LongFloat LogE2 = 0.6931471805599453094172321214581766L ;
 	
 	
 		/**
@@ -126,7 +135,7 @@ namespace Ceylan
 		 * (used to be the M_LN10 maths.h constant).
 		 *
 		 */
-		const long double LogE10 = 2.3025850929940456840179914546843642L ;
+		const Ceylan::LongFloat LogE10 = 2.3025850929940456840179914546843642L ;
 
 
 
@@ -138,7 +147,7 @@ namespace Ceylan
 		 * (used to be the M_PI maths.h constant).
 		 *
 		 */
-		const long double Pi = 3.1415926535897932384626433832795029L ;
+		const Ceylan::LongFloat Pi = 3.1415926535897932384626433832795029L ;
 	
 	
 		/** 
@@ -146,7 +155,7 @@ namespace Ceylan
 		 * (used to be the M_PI_2 maths.h constant).
 		 *
 		 */
-		const long double Pi_div_2 = 1.5707963267948966192313216916397514L ;
+		const Ceylan::LongFloat Pi_div_2 = 1.5707963267948966192313216916397514L ;
 	
 	
 		/** 
@@ -154,7 +163,7 @@ namespace Ceylan
 		 * (used to be the M_PI_4 maths.h constant).
 		 *
 		 */
-		const long double Pi_div_4 = 0.7853981633974483096156608458198757L ;
+		const Ceylan::LongFloat Pi_div_4 = 0.7853981633974483096156608458198757L ;
 	
 	
 		/** 
@@ -162,7 +171,7 @@ namespace Ceylan
 		 * (used to be the M_1_PI maths.h constant).
 		 *
 		 */
-		const long double One_div_Pi = 0.3183098861837906715377675267450287L ;
+		const Ceylan::LongFloat One_div_Pi = 0.3183098861837906715377675267450287L ;
 	
 	
 		/** 
@@ -170,7 +179,7 @@ namespace Ceylan
 		 * (used to be the M_2_PI maths.h constant).
 		 *
 		 */
-		const long double Two_div_Pi = 0.6366197723675813430755350534900574L ;
+		const Ceylan::LongFloat Two_div_Pi = 0.6366197723675813430755350534900574L ;
 	
 	
 		/** 
@@ -178,7 +187,7 @@ namespace Ceylan
 		 * (used to be the M_2_SQRTPI maths.h constant).
 		 *
 		 */
-		const long double Two_div_sqrt_Pi 
+		const Ceylan::LongFloat Two_div_sqrt_Pi 
 			= 1.1283791670955125738961589031215452L ;
 	
 	
@@ -192,7 +201,7 @@ namespace Ceylan
 		 * (used to be the M_SQRT2 maths.h constant).
 		 *
 		 */
-		const long double Sqrt_2 = 1.4142135623730950488016887242096981L ;
+		const Ceylan::LongFloat Sqrt_2 = 1.4142135623730950488016887242096981L ;
 	
 	
 		/** 
@@ -200,32 +209,32 @@ namespace Ceylan
 		 * (used to be the M_SQRT1_2 maths.h constant).
 		 *
 		 */
-		const long double One_div_sqrt_2 
+		const Ceylan::LongFloat One_div_sqrt_2 
 			= 0.7071067811865475244008443621048490L ;
 	
 	
 		/**
 		 * A very small number, used for floating point comparisons :
-		 * two numbers x and y are deemed equal if | x - y | < Epsilon
+		 * two numbers x and y are deemed equal if | x - y | < EpsilonFloat32
 		 *
 		 */
-		const long double EpsilonFloat = 1.0e-7 ;
+		const Ceylan::LongFloat EpsilonFloat32 = 1.0e-7 ;
 
 
 		/**
 		 * A very small number, used for floating point comparisons : 
-		 * two numbers x and y are deemed equal if | x - y | < Epsilon
+		 * two numbers x and y are deemed equal if | x - y | < EpsilonFloat64
 		 *
 		 */
-		const long double EpsilonDouble = 1.0e-9 ;
+		const Ceylan::LongFloat EpsilonFloat64 = 1.0e-9 ;
 
 
 		/**
 		 * A very small number, used for floating point comparisons : 
-		 * two numbers x and y are deemed equal if | x - y | < Epsilon
+		 * two numbers x and y are deemed equal if | x - y | < EpsilonLongFloat
 		 *
 		 */
-		const long double EpsilonLongDouble = 1.0e-11 ;
+		const Ceylan::LongFloat EpsilonLongFloat = 1.0e-11 ;
 
 
 		/**
@@ -233,39 +242,80 @@ namespace Ceylan
 		 * two numbers x and y are deemed equal if | x - y | < Epsilon
 		 *
 		 */
-		const long double Epsilon = EpsilonFloat ;
+		const Ceylan::LongFloat Epsilon = EpsilonFloat32 ;
 
+		
 		
 		
 		// Templates could have been more widely used.
 
 
+
+		// IsNull section.
+		
+
 		/** 
 		 * Tells whether x is null as regard to floating point numbers :
-		 * | x | < epsilon.
+		 * | x | < EpsilonFloat32.
 		 *
 		 */
-		bool IsNull( float x, 
-			long double epsilon = EpsilonFloat ) throw() ;
-		
-		
-		/** 
-		 * Tells whether x is null as regard to floating point numbers :
-		 * | x | < epsilon.
-		 *
-		 */
-		bool IsNull( double x, 
-			long double epsilon = EpsilonDouble ) throw() ;
-		
+		bool IsNull( Ceylan::Float32 x ) throw() ;
+
 		
 		/** 
 		 * Tells whether x is null as regard to floating point numbers :
 		 * | x | < epsilon.
 		 *
 		 */
-		bool IsNull( long double x,
-			long double epsilon = EpsilonLongDouble ) throw() ;
+		bool IsNull( Ceylan::Float32 x, Ceylan::Float32 epsilon ) throw() ;
 		
+		
+		
+		/** 
+		 * Tells whether x is null as regard to floating point numbers :
+		 * | x | < EpsilonFloat64.
+		 *
+		 */
+		bool IsNull( Ceylan::Float64 x ) throw() ;
+		
+		
+		/** 
+		 * Tells whether x is null as regard to floating point numbers :
+		 * | x | < epsilon.
+		 *
+		 */
+		bool IsNull( Ceylan::Float64 x, Ceylan::Float64 epsilon ) throw() ;
+		
+		
+		
+		/** 
+		 * Tells whether x is null as regard to floating point numbers :
+		 * | x | < EpsilonLongFloat.
+		 *
+		 */
+		bool IsNull( Ceylan::LongFloat x ) throw() ;
+		
+		
+		/** 
+		 * Tells whether x is null as regard to floating point numbers :
+		 * | x | < epsilon.
+		 *
+		 */
+		bool IsNull( Ceylan::LongFloat x, Ceylan::LongFloat epsilon ) throw() ;
+		
+		
+		
+		
+		// AreEqual section.
+		
+		
+		
+		/** 
+		 * Tells whether x and y are 'equal' as floating point numbers :
+		 * | x - y | < EpsilonFloat32.
+		 *
+		 */
+		bool AreEqual( Ceylan::Float32 x, Ceylan::Float32 y ) throw() ;
 		
 		
 		/** 
@@ -273,8 +323,17 @@ namespace Ceylan
 		 * | x - y | < epsilon.
 		 *
 		 */
-		bool AreEqual( float x, float y, 
-			long double epsilon = EpsilonFloat ) throw() ;
+		bool AreEqual( Ceylan::Float32 x, Ceylan::Float32 y, 
+			Ceylan::Float32 epsilon ) throw() ;
+		
+		
+		
+		/** 
+		 * Tells whether x and y are 'equal' as floating point numbers :
+		 * | x - y | < EpsilonFloat64.
+		 *
+		 */
+		bool AreEqual( Ceylan::Float64 x, Ceylan::Float64 y ) throw() ;
 		
 		
 		/** 
@@ -282,17 +341,26 @@ namespace Ceylan
 		 * | x - y | < epsilon.
 		 *
 		 */
-		bool AreEqual( double x, double y, 
-			long double epsilon = EpsilonDouble ) throw() ;
+		bool AreEqual( Ceylan::Float64 x, Ceylan::Float64 y, 
+			Ceylan::Float64 epsilon ) throw() ;
 		
+
+		
+		/** 
+		 * Tells whether x and y are 'equal' as floating point numbers : 
+		 * | x - y | < EpsilonLongFloat.
+		 *
+		 */
+		bool AreEqual( Ceylan::LongFloat x, Ceylan::LongFloat y ) throw() ;
+
 		
 		/** 
 		 * Tells whether x and y are 'equal' as floating point numbers : 
 		 * | x - y | < epsilon.
 		 *
 		 */
-		bool AreEqual( long double x, long double y, 
-			long double epsilon = EpsilonLongDouble ) throw() ;
+		bool AreEqual( Ceylan::LongFloat x, Ceylan::LongFloat y, 
+			Ceylan::LongFloat epsilon ) throw() ;
 		
 	
 	
@@ -307,11 +375,60 @@ namespace Ceylan
 		 *
 		 * @param y the second value to compare.
 		 *
+		 * @note The chosen epsilon, the double of the criterion of relative 
+		 * equality, is the default epsilon used for the templated type for
+		 * tests against zero, and EpsilonFloat32 for the relative test.
+		 *
+		 */
+		template <typename T> bool AreRelativelyEqual( T x, T y ) throw()
+		{
+		
+		
+			/*
+			 * One goal, avoid division by zero on final formula :
+			 * x + y == 0 iff x == -y, in this case :  
+			 *   - if x == 0 then y == 0 and they are deemed equal
+			 *   - otherwise x != 0 implies x != y (since x == -y),
+			 * hence they are not deemed equal.
+			 *
+			 * EpsilonFloat32 not used, otherwise the IsNull calls are
+			 * deemed ambiguous.
+			 * 
+			 */
+			if ( IsNull( x + y ) )
+				return IsNull( x ) ;
+				
+				
+			/*
+			 * Here x + y != 0.
+			 * 
+			 * Relative comparison : compare their difference (|x-y|) 
+			 * to their mean value ((x+y)/2), here using 
+			 * EpsilonFloat32/2 as equality criterion.
+			 *
+			 */
+			return ( Abs( x - y ) / ( x + y ) < EpsilonFloat32 ) ;
+			
+		}
+		
+		
+		
+		/**
+		 * Relative comparison with user-supplied epsilon.
+		 *
+		 * @note Not that useful, since compilers usually complain about
+		 * an ambiguous call of overloaded `Abs(...)', which should be 
+		 * templated too.
+		 *
+		 * @param x the first value to compare.
+		 *
+		 * @param y the second value to compare.
+		 *
 		 * @param epsilon the double of the criterion of relative equality.
 		 *
 		 */
 		template <typename T> bool AreRelativelyEqual( T x, T y, 
-			long double epsilon = EpsilonFloat ) throw()
+			Ceylan::LongFloat epsilon ) throw()
 		{
 		
 		
@@ -323,8 +440,8 @@ namespace Ceylan
 			 * hence they are not deemed equal.
 			 *
 			 */
-			if ( IsNull( x + y ) )
-				return IsNull( x ) ;
+			if ( IsNull( x + y, epsilon ) )
+				return IsNull( x, epsilon ) ;
 				
 				
 			/*
@@ -372,7 +489,7 @@ namespace Ceylan
 		 * @example Floor(1.6)  =  1
 		 *
 		 */
-		float Floor( float x ) throw() ;
+		Ceylan::Float32 Floor( Ceylan::Float32 x ) throw() ;
 
 		
 		/**
@@ -392,7 +509,7 @@ namespace Ceylan
 		 * @example Floor(1.6)  =  1
 		 *
 		 */
-		double Floor( double x ) throw() ;
+		Ceylan::Float64 Floor( Ceylan::Float64 x ) throw() ;
 		
 		
 		/**
@@ -412,7 +529,7 @@ namespace Ceylan
 		 * @example Floor(1.6)  =  1
 		 *
 		 */
-		long double Floor( long double x ) throw() ;
+		Ceylan::LongFloat Floor( Ceylan::LongFloat x ) throw() ;
 		
 		
 		
@@ -434,7 +551,7 @@ namespace Ceylan
 		 * @example Ceil(1.6)  =  2
 		 *
 		 */
-		float Ceil( float x ) throw() ;
+		Ceylan::Float32 Ceil( Ceylan::Float32 x ) throw() ;
 		
 		
 		/**
@@ -454,7 +571,7 @@ namespace Ceylan
 		 * @example Ceil(1.6)  =  2
 		 *
 		 */
-		double Ceil( double x ) throw() ;
+		Ceylan::Float64 Ceil( Ceylan::Float64 x ) throw() ;
 
 		
 		/**
@@ -474,7 +591,7 @@ namespace Ceylan
 		 * @example Ceil(1.6)  =  2
 		 *
 		 */
-		long double Ceil( long double x ) throw() ;
+		Ceylan::LongFloat Ceil( Ceylan::LongFloat x ) throw() ;
 		
 		
 		
@@ -501,7 +618,7 @@ namespace Ceylan
 		 * @example Round(1.6)  =  2
 		 *
 		 */
-		float Round( float x ) throw() ;
+		Ceylan::Float32 Round( Ceylan::Float32 x ) throw() ;
 
 
 		/**
@@ -535,7 +652,7 @@ namespace Ceylan
 		 * @example Round(-3,23456, 3 ) = -3,235 or -3.2349999...
 		 *
 		 */
-		float Round( float x, Ceylan::Uint8 precision ) throw() ;
+		Ceylan::Float32 Round( Ceylan::Float32 x, Ceylan::Uint8 precision ) throw() ;
 
 		
 		
@@ -559,7 +676,7 @@ namespace Ceylan
 		 * @example Round(1.6)  =  2
 		 *
 		 */
-		double Round( double x ) throw() ;
+		Ceylan::Float64 Round( Ceylan::Float64 x ) throw() ;
 
 
 		/**
@@ -595,7 +712,7 @@ namespace Ceylan
 		 * @example Round(-3,23456, 3 ) = -3,235 or -3.2349999...
 		 *
 		 */
-		double Round( double x, Ceylan::Uint8 precision ) throw() ;
+		Ceylan::Float64 Round( Ceylan::Float64 x, Ceylan::Uint8 precision ) throw() ;
 				
 				
 		
@@ -619,7 +736,7 @@ namespace Ceylan
 		 * @example Round(1.6)  =  2
 		 *
 		 */
-		long double Round( long double x ) throw() ;
+		Ceylan::LongFloat Round( Ceylan::LongFloat x ) throw() ;
 
 
 		/**
@@ -655,7 +772,7 @@ namespace Ceylan
 		 * @example Round(-3,23456, 3 ) = -3,235 or -3.2349999...
 		 *
 		 */
-		long double Round( long double x, Ceylan::Uint8 precision ) throw() ;
+		Ceylan::LongFloat Round( Ceylan::LongFloat x, Ceylan::Uint8 precision ) throw() ;
 	
 			
 				
@@ -689,13 +806,13 @@ namespace Ceylan
 		// Floating point types :
 		 
 		/// Computes the absolute value of specified argument.
-		float Abs( float x ) throw() ;
+		Ceylan::Float32 Abs( Ceylan::Float32 x ) throw() ;
 		
 		/// Computes the absolute value of specified argument.
-		double Abs( double x ) throw() ;
+		Ceylan::Float64 Abs( Ceylan::Float64 x ) throw() ;
 		
 		/// Computes the absolute value of specified argument.
-		long double Abs( long double x ) throw() ;
+		Ceylan::LongFloat Abs( Ceylan::LongFloat x ) throw() ;
 		
 		
 		/// Min :
@@ -809,7 +926,7 @@ namespace Ceylan
 		 * @note No macro used to avoid nasty side effects.
 		 *
 		 */
-		float Min( float x, float y ) throw() ;
+		Ceylan::Float32 Min( Ceylan::Float32 x, Ceylan::Float32 y ) throw() ;
 
 		
 		/**
@@ -818,7 +935,7 @@ namespace Ceylan
 		 * @note No macro used to avoid nasty side effects.
 		 *
 		 */
-		double Min( double x, double y ) throw() ;
+		Ceylan::Float64 Min( Ceylan::Float64 x, Ceylan::Float64 y ) throw() ;
 
 		
 		/**
@@ -827,7 +944,7 @@ namespace Ceylan
 		 * @note No macro used to avoid nasty side effects.
 		 *
 		 */
-		long double Min( long double x, long double y ) throw() ;
+		Ceylan::LongFloat Min( Ceylan::LongFloat x, Ceylan::LongFloat y ) throw() ;
 		
 		
 		
@@ -944,7 +1061,7 @@ namespace Ceylan
 		 * @note No macro used to avoid nasty side effects.
 		 *
 		 */
-		float Max( float x, float y ) throw() ;
+		Ceylan::Float32 Max( Ceylan::Float32 x, Ceylan::Float32 y ) throw() ;
 
 		
 		/**
@@ -953,7 +1070,7 @@ namespace Ceylan
 		 * @note No macro used to avoid nasty side effects.
 		 *
 		 */
-		double Max( double x, double y ) throw() ;
+		Ceylan::Float64 Max( Ceylan::Float64 x, Ceylan::Float64 y ) throw() ;
 
 		
 		/**
@@ -962,7 +1079,7 @@ namespace Ceylan
 		 * @note No macro used to avoid nasty side effects.
 		 *
 		 */
-		long double Max( long double x, long double y ) throw() ;
+		Ceylan::LongFloat Max( Ceylan::LongFloat x, Ceylan::LongFloat y ) throw() ;
 		
 			
 
@@ -975,7 +1092,7 @@ namespace Ceylan
 		 * raised to the power of x.
 		 *
 		 */
-		float Exp( float x ) throw() ;
+		Ceylan::Float32 Exp( Ceylan::Float32 x ) throw() ;
 		
 		
 		/**
@@ -983,7 +1100,7 @@ namespace Ceylan
 		 * raised to the power of x.
 		 *
 		 */
-		double Exp( double x ) throw() ;
+		Ceylan::Float64 Exp( Ceylan::Float64 x ) throw() ;
 		
 		
 		/**
@@ -991,33 +1108,33 @@ namespace Ceylan
 		 * raised to the power of x.
 		 *
 		 */
-		long double Exp( long double x ) throw() ;
+		Ceylan::LongFloat Exp( Ceylan::LongFloat x ) throw() ;
 		
 		
 		
 		// Power :
 		
 		/// Computes the value of x raised to the power of y.
-		float Pow( float x, float y ) throw() ;
+		Ceylan::Float32 Pow( Ceylan::Float32 x, Ceylan::Float32 y ) throw() ;
 		
 		/// Computes the value of x raised to the power of y.		
-		double Pow( double x, double y ) throw() ;
+		Ceylan::Float64 Pow( Ceylan::Float64 x, Ceylan::Float64 y ) throw() ;
 		
 		/// Computes the value of x raised to the power of y.		
-		long double Pow( long double x, long double y ) throw() ;
+		Ceylan::LongFloat Pow( Ceylan::LongFloat x, Ceylan::LongFloat y ) throw() ;
 		
 		
 		
 		// Power of two :
 		
 		/// Computes the value of x² (x*x).
-		float Pow2( float x ) throw() ;
+		Ceylan::Float32 Pow2( Ceylan::Float32 x ) throw() ;
 		
 		/// Computes the value of x² (x*x).		
-		double Pow2( double x ) throw() ;
+		Ceylan::Float64 Pow2( Ceylan::Float64 x ) throw() ;
 		
 		/// Computes the value of x² (x*x).		
-		long double Pow2( long double x ) throw() ;
+		Ceylan::LongFloat Pow2( Ceylan::LongFloat x ) throw() ;
 		
 			
 				
@@ -1028,7 +1145,7 @@ namespace Ceylan
 		 * exception if x is negative.
 		 *
 		 */
-		float Sqrt( float x ) throw( MathsException ) ;
+		Ceylan::Float32 Sqrt( Ceylan::Float32 x ) throw( MathsException ) ;
 		
 		
 		/**
@@ -1036,7 +1153,7 @@ namespace Ceylan
 		 * exception if x is negative.
 		 *
 		 */
-		double Sqrt( double x ) throw( MathsException ) ;
+		Ceylan::Float64 Sqrt( Ceylan::Float64 x ) throw( MathsException ) ;
 		
 		
 		/**
@@ -1044,7 +1161,7 @@ namespace Ceylan
 		 * exception if x is negative.
 		 *
 		 */
-		long double Sqrt( long double x ) throw( MathsException ) ;
+		Ceylan::LongFloat Sqrt( Ceylan::LongFloat x ) throw( MathsException ) ;
 			
 			
 		
@@ -1061,7 +1178,7 @@ namespace Ceylan
 		 * @return a value between -1 and 1.
 		 *
 		 */
-		float Cos( float x ) throw() ;
+		Ceylan::Float32 Cos( Ceylan::Float32 x ) throw() ;
 		
 		
 		/**
@@ -1070,7 +1187,7 @@ namespace Ceylan
 		 * @return a value between -1 and 1.
 		 *
 		 */
-		double Cos( double x ) throw() ;
+		Ceylan::Float64 Cos( Ceylan::Float64 x ) throw() ;
 		
 		
 		/**
@@ -1079,7 +1196,7 @@ namespace Ceylan
 		 * @return a value between -1 and 1.
 		 *
 		 */
-		long double Cos( long double x ) throw() ;
+		Ceylan::LongFloat Cos( Ceylan::LongFloat x ) throw() ;
 		
 
 		
@@ -1092,7 +1209,7 @@ namespace Ceylan
 		 * @return a value between -1 and 1.
 		 *
 		 */
-		float Sin( float x ) throw() ;
+		Ceylan::Float32 Sin( Ceylan::Float32 x ) throw() ;
 		
 		
 		/**
@@ -1101,7 +1218,7 @@ namespace Ceylan
 		 * @return a value between -1 and 1.
 		 *
 		 */
-		double Sin( double x ) throw() ;
+		Ceylan::Float64 Sin( Ceylan::Float64 x ) throw() ;
 		
 		
 		/**
@@ -1110,7 +1227,7 @@ namespace Ceylan
 		 * @return a value between -1 and 1.
 		 *
 		 */
-		long double Sin( long double x ) throw() ;
+		Ceylan::LongFloat Sin( Ceylan::LongFloat x ) throw() ;
 		
 		
 		
@@ -1290,4 +1407,4 @@ namespace Ceylan
 }
 
 
-#endif // CEYLAN_MATH_BASIC_H_
+#endif // CEYLAN_MATHS_BASIC_H_
