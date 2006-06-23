bc2dcfcc15c514dfb863018e839461a5a368ac40
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-06-23 12:00:21 +0000

    LevelOfDetail constants defined here since extern now.

--- src/code/logs/CeylanLog.cc
+++ src/code/logs/CeylanLog.cc
@@ -6,7 +6,22 @@ using std::string ;
 using namespace Ceylan::Log ;
 
 
-/// Let's define in a centralized way informations shared at the framework level.
+
+/**
+ * Let's define in a centralized way informations shared at the 
+ * framework level.
+ *
+ *
+ */
+ 
+const LevelOfDetail Ceylan::Log::DefaultLevelOfDetailForMessage  =  5 ;
+const LevelOfDetail Ceylan::Log::MaximumLevelOfDetailForMessage  =  0 ;
+
+const LevelOfDetail Ceylan::Log::DefaultLevelOfDetailForListener = 10 ;
+const LevelOfDetail Ceylan::Log::MaximumLevelOfDetailForListener =  0 ;
+
+
+
 
 
 LogException::LogException( const std::string & reason ) throw() :
