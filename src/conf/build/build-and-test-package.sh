4115f6741c094f23a3fb66d36c6bf21ac3dcac49
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-06-23 12:13:59 +0000

    Added to ully test distributed packages in one command.

---
+++ src/conf/build/build-and-test-package.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+USAGE="`basename $0` : builds and installs the Ceylan library from a just-extracted distributed package, and builds, installs and runs the test suite against this install."
+
+SUFFIX=`date '+%Y%m%d-%Hh%M'`
+CEYLAN_INSTALL_ROOT=$HOME/tmp-ceylan-install-$SUFFIX
+CEYLAN_INSTALL_TEST_ROOT=$HOME/tmp-ceylan-test-install-$SUFFIX
+
+mkdir $CEYLAN_INSTALL_ROOT $CEYLAN_INSTALL_TEST_ROOT
+
+./configure --prefix=$CEYLAN_INSTALL_ROOT && make && make install && cd test && ./configure --prefix=$CEYLAN_INSTALL_TEST_ROOT --with-libCeylan=$CEYLAN_INSTALL_ROOT && make && make install && ./playTests.sh
+
+
