c84c64f26ac3af233a2c9a264b6641abed2934d6
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-06-25 12:34:55 +0000

    Better Ceylan datatype integration, comment update, reformatting.

--- src/code/interfaces/CeylanHashable.h
+++ src/code/interfaces/CeylanHashable.h
@@ -15,30 +15,36 @@ namespace Ceylan
     /**
 	 * Definition of a weak hash code.
 	 *
-	 * This hash code is described as weak since it can take only 256 different values.
-	 * It is especially useful for hash tables that could not have thousands of buckets.
+	 * This hash code is described as weak since it can take only 256 
+	 * different values.
+	 *
+	 * It is especially useful for hash tables that could not have thousands 
+	 * of buckets.
 	 *
 	 */
-    typedef Uint8 WeakHashCode ;
+    typedef Ceylan::Uint8 WeakHashCode ;
 
 
     /**
 	 * Definition of a strong hash code.
 	 *
-	 * This hash code is described as strong since it can take 2^32 different values.
-	 * It is especially useful in order to compare for equality two objects, since the probability
-	 * they have the same hash code should be low.
+	 * This hash code is described as strong since it can take 2^32 
+	 * different values.
+	 *
+	 * It is especially useful in order to compare for equality two objects,
+	 * since the probability they have the same hash code should be low.
 	 *
 	 */
-    typedef Uint32 StrongHashCode ;
+    typedef Ceylan::Uint32 StrongHashCode ;
+
 
 
     /**
-     * Interface that every object able to compute a hash value should implement, to ease
-     * hashtables handling.
+     * Interface that every object able to compute a hash value should
+	 * implement, to ease hashtable handling.
      *
-	 * @note It should be said whether the hashcodes depend on the state of the Hashable.
-	 * At least in theory, they should not.
+	 * @note It should be specified whether the hashcodes depend on the state 
+	 * of the Hashable. At least in theory, they should not.
 	 *
      * @see Object.
      *
@@ -59,6 +65,7 @@ namespace Ceylan
 			/// Returns the current weak hash code for this Hashable.
             virtual WeakHashCode getWeakHashCode() const throw() = 0 ;
 
+
 			/// Returns the current strong hash code for this Hashable.
             virtual StrongHashCode getStrongHashCode() const throw() = 0 ;
 
@@ -73,23 +80,38 @@ namespace Ceylan
 			 * @see TextDisplayable
              *
              */
-			virtual const std::string toString( VerbosityLevels level = high ) const throw() ;
+			virtual const std::string toString( VerbosityLevels level = high )
+				const throw() ;
 
 
-			/// Returns the weak hash code corresponding to the specified string.
-			static WeakHashCode GetWeakHashCode( const std::string & stringToHash ) throw() ;
+			/**
+			 * Returns the weak hash code corresponding to the specified 
+			 * string.
+			 *
+			 */
+			static WeakHashCode GetWeakHashCode( 
+				const std::string & stringToHash ) throw() ;
 			
-			/// Returns the strong hash code corresponding to the specified string.
-			static StrongHashCode GetStrongHashCode( const std::string & stringToHash ) throw() ;
+			
+			/**
+			 * Returns the strong hash code corresponding to the specified
+			 * string.
+			 *
+			 */
+			static StrongHashCode GetStrongHashCode( 
+				const std::string & stringToHash ) throw() ;
+
 
 
 		private:
 		
 		
 			/**
-			 * Copy constructor made private to ensure that it will be never called.
-			 * The compiler should complain whenever this undefined constructor is called, 
-			 * implicitly or not.
+			 * Copy constructor made private to ensure that it will be 
+			 * never called.
+			 *
+			 * The compiler should complain whenever this undefined 
+			 * constructor is called, implicitly or not.
 			 * 
 			 *
 			 */			 
@@ -97,9 +119,11 @@ namespace Ceylan
 			
 			
 			/**
-			 * Assignment operator made private to ensure that it will be never called.
-			 * The compiler should complain whenever this undefined operator is called, 
-			 * implicitly or not.
+			 * Assignment operator made private to ensure that it will be
+			 * never called.
+			 *
+			 * The compiler should complain whenever this undefined operator
+			 * is called, implicitly or not.
 			 * 
 			 *
 			 */			 
