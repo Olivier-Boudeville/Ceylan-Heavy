d4afec2e897cb8b8635e97f6bd58d87bd8fa867d
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-06-25 12:32:49 +0000

    Include guards fixed, reformatted.

--- src/code/interfaces/CeylanMeasurable.h
+++ src/code/interfaces/CeylanMeasurable.h
@@ -1,11 +1,11 @@
-#ifndef CEYLAN_MANAGEABLE_H_
-#define CEYLAN_MANAGEABLE_H_
+#ifndef CEYLAN_MEASURABLE_H_
+#define CEYLAN_MEASURABLE_H_
 
-
-#include <string>
 
 #include "CeylanTextDisplayable.h"  // for inheritance
 #include "CeylanSystem.h"           // for Size
+
+#include <string>
 
 
 
@@ -14,8 +14,8 @@ namespace Ceylan
 
 
     /**
-     * Interface which should be implemented for all objects whose size can be dynamically 
-	 * evaluated (at runtime).
+     * Interface which should be implemented for all objects whose size 
+	 * can be dynamically evaluated (at runtime).
 	 *
 	 * Measurable objects are notably able to evaluate their memory footprint.
 	 *
@@ -36,11 +36,13 @@ namespace Ceylan
 			
 			
 			/**
-			 * Returns the approximate size in memory, in bytes, currently taken by this object.
+			 * Returns the approximate size in memory, in bytes, currently
+			 * taken by this object.
 			 *
-			 * The general method for implementing this interface is to evaluate the static size of
-			 * the object (thanks to 'sizeof()') and to evaluate recursively all dynamically 
-			 * allocated members. The overall sum is the size to return.
+			 * The general method for implementing this interface is to 
+			 * evaluate the static size of the object (thanks to 'sizeof()') 
+			 * and to evaluate recursively all owned dynamically allocated
+			 * members. The overall sum is the size to return.
 			 * 
 			 */
 			virtual Ceylan::System::Size getSizeInMemory() const throw() = 0 ;
@@ -57,29 +59,31 @@ namespace Ceylan
 			 * @see TextDisplayable
              *
              */
-            virtual const std::string toString( Ceylan::VerbosityLevels level = Ceylan::high ) 
-				const throw() ;
+            virtual const std::string toString( 
+				Ceylan::VerbosityLevels level = Ceylan::high ) const throw() ;
 
 
 		private:
 		
 		
 			/**
-			 * Copy constructor made private to ensure that it will be never called.
-			 * The compiler should complain whenever this undefined constructor is called, 
-			 * implicitly or not.
-			 * 
+			 * Copy constructor made private to ensure that it will be 
+			 * never called.
 			 *
+			 * The compiler should complain whenever this undefined 
+			 * constructor is called, implicitly or not.
+			 * 
 			 */			 
 			Measurable( const Measurable & source ) throw() ;
 			
 			
 			/**
-			 * Assignment operator made private to ensure that it will be never called.
-			 * The compiler should complain whenever this undefined operator is called, 
-			 * implicitly or not.
-			 * 
+			 * Assignment operator made private to ensure that it will 
+			 * be never called.
 			 *
+			 * The compiler should complain whenever this undefined operator
+			 * is called, implicitly or not.
+			 * 
 			 */			 
 			Measurable & operator = ( const Measurable & source ) throw() ;
 
@@ -89,4 +93,4 @@ namespace Ceylan
 
 }
 
-#endif // CEYLAN_MANAGEABLE_H_
+#endif // CEYLAN_MEASURABLE_H_
