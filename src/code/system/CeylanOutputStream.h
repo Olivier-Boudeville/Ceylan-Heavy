cafb35b8f74c6b06e6d0d1130bbb46df249e016f
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-07-07 20:34:21 +0000

    Interface strengthen, definitions centralized.

--- src/code/system/CeylanOutputStream.h
+++ src/code/system/CeylanOutputStream.h
@@ -2,6 +2,7 @@
 #define CEYLAN_OUTPUT_STREAM_H_
 
 
+#include "CeylanSystem.h"     // for IOException
 #include "CeylanStream.h"     // for inheritance
 
 
@@ -29,7 +30,24 @@ namespace Ceylan
 
 
 			public:
+					
+					
 						
+				/// Exception thrown when a write operation failed.
+				class WriteFailedException: public IOException
+				{ 
+					public: 
+					
+						explicit WriteFailedException( 
+								const std::string & reason ) throw() : 
+							IOException( reason )
+						{
+						
+						}
+								
+				} ;
+				
+				
 				/// Basic constructor.
 				OutputStream() throw() ;
 	
@@ -39,6 +57,39 @@ namespace Ceylan
 	
 				/// Returns the output stream's unique ID.
 				virtual StreamID getOutputStreamID() const throw() = 0 ;
+
+
+				/**
+				 * Writes message to this OutputStream.
+				 *
+				 * @param message the message to write.
+				 *
+				 * @return The number of bytes actually written, which 
+				 * should be equal to the size of the string or lower.
+				 *
+				 * @throw WriteFailed if a write error occurred.
+				 *
+				 */
+				virtual Size write( const std::string & message ) 
+					throw( WriteFailedException ) = 0 ;
+
+
+				/**
+				 * Writes up to maxLength bytes from the specified buffer
+				 * to this OutputStream.
+				 *
+				 * @param buffer the buffer where to find bytes that must
+				 * be written. Its size must be at least maxLength bytes.
+				 *
+				 * @return The number of bytes actually written, which 
+				 * should be equal to maxLength.
+				 *
+				 * @throw WriteFailed if a write error occurred.
+				 *
+				 */
+				virtual Size write( const char * buffer, Size maxLength ) 
+					throw( WriteFailedException ) = 0 ;
+
 
 
 			private:
