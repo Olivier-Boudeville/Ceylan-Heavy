a92dde9ec9ca8617c7226e13ea151cdaa81e2882
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-07-07 20:37:46 +0000

    Signal interface added, with full signal definitions.

--- src/code/system/CeylanSignal.h
+++ src/code/system/CeylanSignal.h
@@ -2,76 +2,302 @@
 #define CEYLAN_SIGNAL_H_
 
 
-extern "C"
-{
+#include "CeylanTypes.h"         // for Ceylan::Uint16
+#include "CeylanSystem.h"        // for SystemException
 
-	typedef void (*SignalHandler)(int);
-	
-}
+
+#include <string>
 
 
 /**
- * UNIX-style signal management.
+ * UNIX-style POSIX reliable signal management.
  *
+ * @note The signal feature must be available to successfully manage signals.
+ *
+ * @see Features::areSignalsSupported
  *
  */
 namespace Ceylan
 {
 
 
-	namespace Signal 
+	namespace System
 	{
 	
 	
-
-		// Signal types.
-		
-		typedef Ceylan::Uint16 Signal ;
-		
-		/// Signal 
-		extern const Signal HangUp ;
-		
-		extern const Signal  ;
-		
-		enum Type
+		namespace Signal 
 		{
-        	HANGUP 			     	  = SIGHUP,
-         	INTERRUPT_FROM_KEYBOARD 	  = SIGINT,
-         	QUIT_FROM_KEYBOARD		  = SIGQUIT,
-         	ILLEGAL_INSTRUCTION		  = SIGILL,
-         	ABORT				  = SIGABRT,
-         	FLOATING_POINT_EXCEPTION	  = SIGFPE,
-         	KILL				  = SIGKILL,
-         	INVALID_MEMORY_REFERENCE	  = SIGSEGV,
-         	BROKEN_PIPE			  = SIGPIPE,
-         	TIMER_FROM_ALARM		  = SIGALRM,
-         	TERMINATION			  = SIGTERM,
-         	USER_DEFINED_1  		  = SIGUSR1,
-         	USER_DEFINED_2  		  = SIGUSR2,
-         	CHILD_STOPPED			  = SIGCHLD,
-         	CONTINUE_IF_STOPPED		  = SIGCONT,
-         	STOP_PROCESS			  = SIGSTOP,
-         	STOP_TYPED_AT_TTY		  = SIGTSTP,
-         	TTY_INPUT_FOR_BACKGROUND_PROCESS  = SIGTTIN,
-         	TTY_OUTPUT_FOR_BACKGROUND_PROCESS = SIGTTOU
-		} ;
 	
-	/// Sets a new handler for the signal type <b>t</b>.
-	bool setHandler( Type t, SignalHandler handler );
+
+			/// Mother class for all signal-related exceptions.
+			class SignalException: public SystemException
+			{ 
+				public: 
+				
+					explicit SignalException( const std::string & reason )
+						throw() ;
+					
+					virtual ~SignalException() throw() ; 
+						
+			} ;
 	
-	/// Turns on the default bahviour on signal type <b>t</b>.
-	bool setDefaultBehavior( Type t );
 	
-	/// Makes process ignore the signal type <b>t</b>.
-	bool ignore( Type t );
 	
-	/// Raises the signal <b>t</b>.
-	void raise( Type t );
+			/// SignalHandler is a pointer to function.
+			typedef void (*SignalHandler)(int) ;
+		
+		
+			/// Pre-defined handler which ignores signals.
+			extern const SignalHandler IgnoringHandler ;
+		
+		
+			/**
+			 * The default handler, triggering the corresponding action as
+			 * defined for the actual signal (ignore, terminate, core dump, 
+			 * etc.).
+			 *
+			 */
+			extern const SignalHandler DefaultHandler ;
+		
+		
+		
+			/// Signal numbers, as described in the original POSIX.1 standard.
+			typedef Ceylan::Uint16 SignalNumber ;
+		
+		
+			/**
+			 * Hangup detected on controlling terminal or death of controlling
+			 * process (SIGHUP).
+			 *
+			 * Default action is to terminate the process.
+			 *
+			 */
+			extern const SignalNumber HangUp ;
 	
-	/// Converts the sygnal number to human readable string
-	const char * typeToString( Type type );
+	
+			/**
+			 * Interrupt from keyboard (SIGINT).
+			 *
+			 * Default action is to terminate the process.
+			 *
+			 */
+			extern const SignalNumber InterruptFromKeyboard ;
+	
+	
+			/**
+			 * Quit from keyboard (SIGQUIT).
+			 *
+			 * Default action is to terminate the process and dump core.
+			 *
+			 */
+			extern const SignalNumber QuitFromKeyboard ;
+	
+	
+			/**
+			 * Illegal Instruction (SIGILL).
+			 *
+			 * Default action is to terminate the process and dump core.
+			 *
+			 */
+			extern const SignalNumber IllegalInstruction ;
+	
+	
+			/**
+			 * Abort signal from abort (SIGABRT).
+			 *
+			 * Default action is to terminate the process and dump core.
+			 *
+			 */
+			extern const SignalNumber Abort ;
+	
+	
+			/**
+			 * Floating point exception (SIGFPE).
+			 *
+			 * Default action is to terminate the process and dump core.
+			 *
+			 */
+			extern const SignalNumber FloatingPointException ;
+	
+	
+			/**
+			 * Kill signal (SIGKILL).
+			 *
+			 * Default action is to terminate the process.
+			 *
+			 * @note This signal cannot be caught, blocked, or ignored.
+			 *
+			 */
+			extern const SignalNumber Kill ;
+	
+	
+			/**
+			 * Invalid memory reference (SIGSEGV).
+			 *
+			 * Default action is to terminate the process and dump core.
+			 *
+			 */
+			extern const SignalNumber InvalidMemoryReference ;
+	
+	
+			/**
+			 * Broken pipe: write to pipe with no readers (SIGPIPE).
+			 *
+			 * Default action is to terminate the process.
+			 *
+			 */
+			extern const SignalNumber BrokenPipe ;
+	
+	
+			/**
+			 * Timer signal from alarm (SIGALRM).
+			 *
+			 * Default action is to terminate the process.
+			 *
+			 */
+			extern const SignalNumber TimerSignal ;
+	
+	
+			/**
+			 * Termination signal (SIGTERM).
+			 *
+			 * Default action is to terminate the process.
+			 *
+			 */
+			extern const SignalNumber Termination ;
+	
+	
+			/**
+			 * First user-defined signal (SIGUSR1).
+			 *
+			 * Default action is to terminate the process.
+			 *
+			 */
+			extern const SignalNumber FirstUserDefined ;
+	
+	
+			/**
+			 * Second user-defined signal (SIGUSR2).
+			 *
+			 * Default action is to terminate the process.
+			 *
+			 */
+			extern const SignalNumber SecondUserDefined ;
+	
+	
+			/**
+			 * Child stopped or terminated (SIGCHLD).
+			 *
+			 * Default action is to ignore the signal.
+			 *
+			 */
+			extern const SignalNumber ChildEnded ;
+	
+	
+			/**
+			 * Continue if stopped (SIGCONT).
+			 *
+			 * No default action.
+			 *
+			 */
+			extern const SignalNumber Continue ;
+	
+	
+			/**
+			 * Stop process (SIGSTOP).
+			 *
+			 * Default action is to stop the process.
+			 *
+			 * @note This signal cannot be caught, blocked, or ignored.
+			 *
+			 */
+			extern const SignalNumber Stop ;
+	
+	
+			/**
+			 * Stop typed at tty (SIGTSTP).
+			 *
+			 * Default action is to stop the process.
+			 *
+			 */
+			extern const SignalNumber TtyStopped ;
+	
+	
+			/**
+			 * tty input for background process (SIGTTIN).
+			 *
+			 * Default action is to stop the process.
+			 *
+			 */
+			extern const SignalNumber TtyInput ;
+	
+	
+			/**
+			 * tty output for background process (SIGTTOU).
+			 *
+			 * Default action is to stop the process.
+			 *
+			 */
+			extern const SignalNumber TtyOutput ;
+	
+	
+	
+			/**
+			 * Sets the default handler for the specified signal.
+			 *
+			 * @throw SignalException iff the operation failed, including
+			 * if signal support is not available.
+			 *
+			 */
+			void setToDefaultHandler( SignalNumber signalNumber )
+				throw( SignalException ) ;
+	
+
+			/**
+			 * Ignores specified signal from now on.
+			 *
+			 * @throw SignalException iff the operation failed, including
+			 * if signal support is not available.
+			 *
+			 */
+			void ignore( SignalNumber signalNumber ) throw( SignalException ) ;
 
 	
+			/**
+			 * Sets a new handler for the specified signal type.
+			 *
+			 * @throw SignalException iff the operation failed, including
+			 * if signal support is not available.
+			 *
+			 */
+			void setHandler( SignalNumber signalNumber, 
+				SignalHandler newHandler ) throw( SignalException ) ;
+	
+	
+			/**
+			 * Raises the specified signal : sends the signal to the current
+			 * process.
+			 *
+			 * @throw SignalException iff the operation failed, including
+			 * if signal support is not available.
+			 *
+			 */
+			void raise( SignalNumber signalNumber ) throw( SignalException ) ;
+
+	
+            /**
+             * Returns a user-friendly description of the signal.
+             *
+			 * @param signal the signal to describe.
+			 *
+			 */
+			const std::string toString( SignalNumber signalNumber ) 
+				throw() ;
+
+
+	
+		}
+		
 	}
 	
 }
