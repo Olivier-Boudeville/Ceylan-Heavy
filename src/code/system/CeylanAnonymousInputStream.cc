9ad2127cdc4b01c8fcda500a8ecff764aeb056b1
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-07-19 21:17:37 +0000

    Implementation of accepted connections on the server side.

---
+++ src/code/system/CeylanAnonymousInputStream.cc
@@ -0,0 +1,59 @@
+#include "CeylanAnonymousInputStream.h"
+
+#include "CeylanOperators.h"
+#include "CeylanLogPlug.h"
+
+
+#if CEYLAN_USES_CONFIG_H
+#include "CeylanConfig.h"      // for configure-time settings
+#endif // CEYLAN_USES_CONFIG_H
+
+
+
+
+using std::string ;
+using std::list ;
+
+
+using namespace Ceylan::System ;
+using namespace Ceylan::Log ;
+
+
+AnonymousInputStream::AnonymousInputStream( FileDescriptor fd )
+		throw( StreamException ):
+	_fdes( fd )
+{
+
+#if CEYLAN_USES_FILE_DESCRIPTORS
+
+#else // CEYLAN_USES_FILE_DESCRIPTORS
+
+	throw StreamException( "AnonymousInputStream constructor : "
+		"file descriptor feature not available." ) ;
+		
+#endif // CEYLAN_USES_FILE_DESCRIPTORS
+
+}
+
+
+AnonymousInputStream::~AnonymousInputStream() throw()
+{
+
+}
+
+
+StreamID AnonymousInputStream::getInputStreamID() const
+{
+
+	return static_cast<StreamID>( _fdes ) ;
+}
+
+
+const std::string AnonymousInputStream::toString( 
+	Ceylan::VerbosityLevels level ) const throw()
+{
+
+	return "AnonymousInputStream whose file descriptor is "
+		+ Ceylan::toString( _fdes ) ;
+		
+}
