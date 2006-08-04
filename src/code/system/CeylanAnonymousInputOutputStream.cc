9c474ac764f25b2dab240b3e711e0e725b37209e
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-08-04 10:47:26 +0000

    Most methods transferred in ServerAnonymousInputOutputStream.

--- src/code/system/CeylanAnonymousInputOutputStream.cc
+++ src/code/system/CeylanAnonymousInputOutputStream.cc
@@ -4,11 +4,6 @@
 #include "CeylanLogPlug.h"
 
 
-#if CEYLAN_USES_CONFIG_H
-#include "CeylanConfig.h"      // for configure-time settings
-#endif // CEYLAN_USES_CONFIG_H
-
-
 
 
 using std::string ;
@@ -19,40 +14,16 @@ using namespace Ceylan::System ;
 using namespace Ceylan::Log ;
 
 
-AnonymousInputOutputStream::AnonymousInputOutputStream( FileDescriptor fd )
-		throw( StreamException ):
-	_fdes( fd )
+AnonymousInputOutputStream::AnonymousInputOutputStream()
+		throw( StreamException )
 {
-
-#if CEYLAN_USES_FILE_DESCRIPTORS
-
-#else // CEYLAN_USES_FILE_DESCRIPTORS
-
-	throw StreamException( "AnonymousInputOutputStream constructor : "
-		"file descriptor feature not available." ) ;
-		
-#endif // CEYLAN_USES_FILE_DESCRIPTORS
 
 }
 
 
 AnonymousInputOutputStream::~AnonymousInputOutputStream() throw()
 {
-
-}
-
-
-StreamID AnonymousInputOutputStream::getInputStreamID() const
-{
-
-	return static_cast<StreamID>( _fdes ) ;
-}
-
-
-StreamID AnonymousInputOutputStream::getOutputStreamID() const
-{
-
-	return static_cast<StreamID>( _fdes ) ;
+			
 }
 
 
@@ -60,7 +31,6 @@ const std::string AnonymousInputOutputStream::toString(
 	Ceylan::VerbosityLevels level ) const throw()
 {
 
-	return "AnonymousInputOutputStream whose file descriptor is "
-		+ Ceylan::toString( _fdes ) ;
+	return "AnonymousInputOutputStream" ;
 		
 }
