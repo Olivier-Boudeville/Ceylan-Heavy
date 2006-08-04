51fdc02a75be4d66a14f1a90c8905a5aa174c0c2
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-08-04 10:54:44 +0000

    Inner SystemSpecificSocketAddress definition removed and replaced by CeylanSystemSpecificSocketAddress.h.

--- src/code/network/CeylanClientStreamSocket.cc
+++ src/code/network/CeylanClientStreamSocket.cc
@@ -4,6 +4,9 @@
 #include "CeylanNetwork.h"            // for HostDNSEntry
 #include "CeylanIPAddressvFour.h"     // for IPAddressvFour
 
+// for SystemSpecificSocketAddress :
+#include "CeylanSystemSpecificSocketAddress.h"  
+
 
 #if CEYLAN_USES_CONFIG_H
 #include "CeylanConfig.h"             // for configure-time feature settings
@@ -33,29 +36,12 @@ extern "C"
 
 }
 
+
 using namespace Ceylan::System ;
 using namespace Ceylan::Network ;
 using namespace Ceylan::Log ;
 using namespace Ceylan ;
 
-
-#if CEYLAN_USES_NETWORK
-
-
-/**
- * Avoid exposing system-dependent sockaddr_in in the headers :
- *
- * @note This definition had to be directly duplicated from
- * file CeylanStreamSocket.cc.
- *
- */
-struct Socket::SystemSpecificSocketAddress
-{
-	sockaddr_in _socketAddress ;	
-	
-} ;
-
-#endif // CEYLAN_USES_NETWORK
 
 
 
@@ -120,6 +106,7 @@ void ClientStreamSocket::connect( const string & serverHostname, Port port )
 
 	_serverHostName = serverHostname ;
 	
+	// Creates the socket and fills sin_family and sin_port :
 	createSocket( port ) ;
 	
 	try
@@ -147,9 +134,19 @@ void ClientStreamSocket::connect( const string & serverHostname, Port port )
 			"could not determine an IPv4 address from host '" 
 				+ _serverHostName + "'." ) ;
 	
+	// Blanks inherited _address before filling it with the server address :
+	getAddress().blank() ;
+	
+	
 	// It is actually something like an unsigned long (see man inet_ntoa) :
 	struct in_addr binaryIP ;
-			
+		
+	
+	/*
+	 * Also possible : 
+	 * binaryIP.sin_addr.s_addr = inet_addr( "129.199.129.1" ) ;
+	 *
+	 */
 	if ( ::inet_aton( serverIP->toString().c_str(), & binaryIP ) == 0 )
 		throw ClientStreamSocketException( "ClientStreamSocket::connect : "
 			"could not forge a network address from IP " 
