74500b890998df2a9fa8e31f9cd95d12960e064e
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-08-11 22:37:31 +0000

    isConnected, handleConnection, closeConnection, closeAcceptedConnections added, comments updated.

--- src/code/network/CeylanMultiplexedServerStreamSocket.h
+++ src/code/network/CeylanMultiplexedServerStreamSocket.h
@@ -14,19 +14,15 @@ namespace Ceylan
 {
 
 
-	namespace System
-	{
-	
-		/// Multiplexed servers create anonymous IO streams.
-		class AnonymousInputOutputStream ;
-		
-	}
-		
-
 	namespace Network
 	{
 
 
+
+		/// Multiplexed servers create anonymous socket streams.
+		class AnonymousStreamSocket ;
+	
+		
 
 		/**
 		 * Server-side implementation of a server listening socket and its 
@@ -35,16 +31,28 @@ namespace Ceylan
 		 *
 		 * This server basically manages n+1 sockets : the listening one, 
 		 * which, whenever incoming connections are accepted, leads to a 
-		 * new socket creation being created, and n simultaneously connected
-		 * sockets, each one being viewed as an anonymous input/output stream.
-		 *
-		 * @see ServerMultiplexedStreamSocket for a server that manages any
+		 * new socket being created, and n simultaneously connected
+		 * sockets, each one being viewed as an anonymous socket dedicated to
+		 * one of the living connections.
+		 * 
+		 * @see SequentialServerStreamSocket for a server that manages any
 		 * number of clients too, but sequentially rather than in parallel.
 		 *
 		 * @see ClientStreamSocket
 		 *
 		 * The clearInput method operates on the listening socket, it does
 		 * not apply to any of the transport streams.
+		 *
+		 * Following methods have to be subclassed so that the server can
+		 * perform its specific task :
+		 *   - handleConnection
+		 *
+		 * Following methods may be subclassed for specific servers :
+		 *   - accepted
+		 *
+		 *
+		 * Read/write methods communicate with the listening socket, as there
+		 * may be more than one living connection at a time.
 		 *
 		 */
 		class MultiplexedServerStreamSocket: public ServerStreamSocket
@@ -56,11 +64,11 @@ namespace Ceylan
 
 				/**
 				 * Mother class for all stream socket-related exceptions, 
-				 * on the server side.
+				 * on the multiplexed server side.
 				 *
 				 */
 				class MultiplexedServerStreamSocketException: 
-					public StreamSocketException
+					public ServerStreamSocketException
 				{ 
 					public: 
 					
@@ -78,7 +86,8 @@ namespace Ceylan
 				/**
 				 * Constructs a new multiplexed server-side socket.
 				 *
-				 * @param port the TCP port of this server socket.
+				 * @param listeningPort the TCP port of this listening server
+				 * socket.
 				 *
 				 * @param reuse tells whether the local addresses are allowed
 				 * to be reused in bind().
@@ -86,17 +95,25 @@ namespace Ceylan
 				 * @throw SocketException if socket creation failed.
 				 *
 				 */
-				explicit MultiplexedServerStreamSocket( Port port, 
+				explicit MultiplexedServerStreamSocket( Port listeningPort, 
 						bool reuse = true )	throw( SocketException ) ;
 	
 				
 				/// Virtual destructor.
 				virtual ~MultiplexedServerStreamSocket() throw() ;
 	
+
+				/**
+				 * Tells whether this socket is currently connected to a 
+				 * client.
+				 *
+				 */
+				virtual bool isConnected() const throw() ;
+	
 	
 				/**
 				 * Activates this server so that it can handle incoming 
-				 * requests.
+				 * requests in parallel.
 				 *
 				 * This main loop while last as long as the server is not 
 				 * requested to stop.
@@ -119,24 +136,46 @@ namespace Ceylan
 				 * @see stop
 				 *
 				 */
-				virtual void accept() throw( ServerStreamSocketException ) ;
+				virtual AnonymousStreamSocket * accept() 
+					throw( ServerStreamSocketException ) ;
 	
 
 
 				/**
 				 * Manages a connection for which data is available.
 				 *
+				 * The server calls regularly this method, as soon as a
+				 * connection has data waiting to be read. This method, 
+				 * designed to be overriden so that the connection can be
+				 * managed according to the user needs, is expected to read
+				 * these data, to process them and, if needed, to write data
+				 * back to the corresponding client. 
+				 *
+				 * Then this method must return so that this server can
+				 * go on serving requests. 
+				 *
+				 * These slice of communication should not be too long, 
+				 * otherwise such request being processed would freeze all
+				 * other connections.
+				 *
 				 * This method is to be subclassed so that this multiplexed
 				 * server can perform its task on each per-connection socket,
 				 * viewed as an abstract input/output stream.
 				 *
 				 * @note It is the responability of this method to return 
-				 * approriately, i.e. if it does not return or if it returns
+				 * appropriately, i.e. if it does not return or if it returns
 				 * after a long time, all other connections will be frozen
 				 * in the meantime.
 				 *
-				 * @param stream the input/output stream that is dedicated to
-				 * a particular connection to this server. When this method is
+				 * @param connection the connection-based anonymous socket in
+				 * charge of this connection that has data to be read.
+				 *
+				 * @return true iff the connection is still alive after 
+				 * this operation. Otherwise, i.e. if false is returned, it 
+				 * will be closed and removed by this server.
+				 *
+				 * This input/output stream that is dedicated to a particular
+				 * connection to this server, and when this method is
 				 * called by the server, the stream is selected, i.e. has 
 				 * already data available for reading. This data may be 
 				 * interpreted by this method as a request that can be decoded,
@@ -146,11 +185,27 @@ namespace Ceylan
 				 * @throw MultiplexedServerStreamSocketException if an error
 				 * occurred.
 				 *
+				 * @see closeConnection, which should be used by this method
+				 * when the underlying protocol determines the connection is
+				 * to be terminated.
+				 *
 				 */
-				virtual void handleConnection( 
-						System::AnonymousInputOutputStream & stream )
+				virtual bool handleConnection( 
+						AnonymousStreamSocket & connection )
 					throw( MultiplexedServerStreamSocketException ) = 0 ;
+				
 					
+				/**
+				 * Closes specified connection.
+				 *
+				 * @param connection a reference to the connection that is 
+				 * to be terminated by this server.
+				 *
+				 */
+				virtual void closeConnection( 
+						AnonymousStreamSocket & connection )
+					throw( MultiplexedServerStreamSocketException ) ;
+	
 	
             	/**
             	 * Returns a user-friendly description of the state of 
@@ -172,17 +227,31 @@ namespace Ceylan
 	
 			protected:
 
-				
-				/**
-				 * The list of current accepted connection-based sockets.
+
+				/** 
+				 * Closes any accepted connection, including its socket.
 				 *
-				 * It is a set of pointers and not a list, since not only it
-				 * has to be enumerated, but also it is searched for specific
-				 * elements (with find).
+				 * @return true iff an operation had to be performed.
+				 *
+				 * @throw CloseException if the close operation failed.
 				 *
 				 */
-				std::set<System::AnonymousInputOutputStream *>
+				virtual bool closeAcceptedConnections() 
+					throw( Stream::CloseException ) ;
+				 
+				
+				/**
+				 * The list of current accepted connection-based anonymous 
+				 * sockets.
+				 *
+				 * It is a set of pointers and not a list, since not only it
+				 * has to be enumerated, but also it has to be searched for
+				 * specific elements (with find).
+				 *
+				 */
+				std::set<AnonymousStreamSocket *>
 					_currentConnections ;
+
 
 
 			private:
