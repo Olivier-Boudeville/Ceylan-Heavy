e534210608c5c1f8331ed74f26ccb437642c7bd3
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-08-11 22:29:22 +0000

    Both sequential and multiplexed servers tested now.

--- test/network/playTests-local.sh
+++ test/network/playTests-local.sh
@@ -13,20 +13,32 @@
 
 #echo "playTests-local.sh sourced"
 
-server_test_name="testCeylanSequentialServerStream"
+
+
+
+sequential_server_test_name="testCeylanSequentialServerStream"
+multiplexed_server_test_name="testCeylanMultiplexedServerStream"
 client_test_name="testCeylanClientStream"
 
-server_test_exec="${TEST_ROOT}/$m/${server_test_name}"
+sequential_server_test_exec="${TEST_ROOT}/$m/${sequential_server_test_name}"
+multiplexed_server_test_exec="${TEST_ROOT}/$m/${multiplexed_server_test_name}"
 client_test_exec="${TEST_ROOT}/$m/${client_test_name}"
+
 
 # Have to be excluded from automatic selection since must be managed
 # specifically here :
-EXCLUDED_TESTS="${server_test_name} ${client_test_name}"
+EXCLUDED_TESTS="${sequential_server_test_name} ${multiplexed_server_test_name} ${client_test_name}"
+
+
+
+
+### Testing sequential server :
+
 
 # First, launch the server :
 
 #echo "Will launch the server"
-t=${server_test_exec}
+t=${sequential_server_test_exec}
 if [ "$is_batch" == "0" ] ; then
 	echo -e "\n\n########### Running now $t" >>${TESTLOGFILE}
 	$t --batch 1>>${TESTLOGFILE} 2>&1 &
@@ -52,8 +64,55 @@ wait ${SERVER_PID}
 return_code="$?"
 #echo "Server return code is $return_code"
 
-display_launching ${server_test_name}
-display_test_result "${server_test_name}" "${server_test_exec}" "$return_code"
+display_launching ${sequential_server_test_name}
+display_test_result "${sequential_server_test_name}" "${sequential_server_test_exec}" "$return_code"
 
 test_count=$(($test_count+2))
+
+
+
+
+
+### Testing multiplexed server :
+
+
+multiplexed_server_test_exec="${TEST_ROOT}/$m/${multiplexed_server_test_name}"
+client_test_exec="${TEST_ROOT}/$m/${client_test_name}"
+
+
+# First, launch the server :
+
+#echo "Will launch the server"
+t=${multiplexed_server_test_exec}
+if [ "$is_batch" == "0" ] ; then
+	echo -e "\n\n########### Running now $t" >>${TESTLOGFILE}
+	$t --batch 1>>${TESTLOGFILE} 2>&1 &
+	SERVER_PID="$!"
+else
+	$t --interactive &
+	SERVER_PID="$!"
+fi			
+
+
+# Then do as if client test was a classical test :
+display_launching ${client_test_name}
+
+# Ensure that the server is ready before the client :
+sleep 1
+
+#echo "Will launch the client"
+run_test ${client_test_name} ${client_test_exec}
+
+#echo "Wait for the server"
+# Now inspect the server result :
+wait ${SERVER_PID} 
+return_code="$?"
+#echo "Server return code is $return_code"
+
+display_launching ${multiplexed_server_test_name}
+display_test_result "${multiplexed_server_test_name}" "${multiplexed_server_test_exec}" "$return_code"
+
+# test client already counted :
+test_count=$(($test_count+1))
+
 
