5e7bc1f2ce587f51436695c24f7f01b4e59134d5
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-08-26 20:42:16 +0000

    Marshaller can use a MemoryStream now. retrieveData added.

--- src/code/middleware/CeylanMarshaller.cc
+++ src/code/middleware/CeylanMarshaller.cc
@@ -2,13 +2,14 @@
 
 #include "CeylanLogPlug.h"            // for LogPlug
 #include "CeylanInputOutputStream.h"  // for InputOutputStream
-
+#include "CeylanMemoryStream.h"       // for MemoryStream
 
 
 using std::string ;
 
 using namespace Ceylan ;
 using namespace Ceylan::Log ;
+using namespace Ceylan::System ;
 using namespace Ceylan::Middleware ;
 
 
@@ -51,11 +52,16 @@ EncodeException::~EncodeException() throw()
 
 
 
-Marshaller::Marshaller( System::InputOutputStream & lowerLevelStream ) throw() :
+Marshaller::Marshaller( System::InputOutputStream & lowerLevelStream,
+		System::Size bufferedSize ) throw() :
 	TextDisplayable(),
-	_lowerLevelStream( & lowerLevelStream )
+	_lowerLevelStream( & lowerLevelStream ),
+	_bufferStream( 0 )
 {
 
+	if ( bufferedSize != 0 )
+		_bufferStream = new MemoryStream( bufferedSize /* bytes */ ) ;
+		
 }
 
 
@@ -64,6 +70,54 @@ Marshaller::~Marshaller() throw()
 
 	// _lowerLevelStream not owned.
 	
+	if ( _bufferStream != 0 )
+		delete _bufferStream ;
+	
+}
+
+
+Size Marshaller::retrieveData() throw( DecodeException )
+{
+
+	if ( _bufferStream == 0 )
+		throw DecodeException( "Marshaller::retrieveData : "
+			"no buffer available for this operation." ) ;
+	
+	Size targetFreeSize = _bufferStream->getSizeOfNextFreeChunk() ;
+	
+	if ( targetFreeSize == 0 )
+		throw DecodeException( "Marshaller::retrieveData : "
+			"buffer full." ) ;
+			
+	Size readSize ;
+			
+	try
+	{
+	
+		// Avoid useless buffer copy thanks to in-place writing :	
+		readSize =_lowerLevelStream->read(
+			_bufferStream->getAddressOfNextFreeChunk(), targetFreeSize ) ;
+			
+	}
+	catch( const InputStream::ReadFailedException & e )
+	{
+		throw DecodeException( "Marshaller::retrieveData : " + e.toString() ) ;
+	}
+
+	try
+	{
+	
+		_bufferStream->increaseFilledBlockOf( readSize ) ;	
+		
+	}
+	catch( const MemoryStream::MemoryStreamException & e )
+	{
+		throw DecodeException( "Marshaller::retrieveData has a bug : "
+			+ e.toString() ) ;
+	}
+		
+	return 	_bufferStream->getBlockLength() ;
+			
 }
 
 
@@ -71,8 +125,16 @@ const string Marshaller::toString( Ceylan::VerbosityLevels level )
 	const throw()
 {
 
-	return "Marshaller using as underlying lower-level stream "
+	string res = "Marshaller using as underlying lower-level stream "
 		+ _lowerLevelStream->toString( level )  ;
+	
+	if ( _bufferStream != 0 )
+		res += ". This marshaller has following internal buffered stream : "
+			+ _bufferStream->toString( level ) ;
+	else
+		res += ". This marshaller has no internal buffered stream" ;
+		
+	return res ;	
 	
 }
 
