2624d2b2c03c1c401bbd97fbf58e6f5a33d5ffb1
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-08-26 07:14:04 +0000

    Namespace specification was lacking.

--- src/code/generic/CeylanTypes.cc
+++ src/code/generic/CeylanTypes.cc
@@ -1,30 +1,34 @@
 #include "CeylanTypes.h"
 
 
-Ceylan::Sint8 Sint8Min = -128 ;
-Ceylan::Sint8 Sint8Max =  127 ;
+Ceylan::Sint8 Ceylan::Sint8Min = -128 ;
+Ceylan::Sint8 Ceylan::Sint8Max =  127 ;
 
-Ceylan::Uint8 Uint8Min = 0   ;
-Ceylan::Uint8 Uint8Max = 255 ;
+Ceylan::Uint8 Ceylan::Uint8Min = 0   ;
+Ceylan::Uint8 Ceylan::Uint8Max = 255 ;
 
-Ceylan::Sint16 Sint16Min = -32768 ;
-Ceylan::Sint16 Sint16Max =  32767 ;
+Ceylan::Sint16 Ceylan::Sint16Min = -32768 ;
+Ceylan::Sint16 Ceylan::Sint16Max =  32767 ;
 
-Ceylan::Uint16 Uint16Min =     0 ;
-Ceylan::Uint16 Uint16Max = 65535 ;
+Ceylan::Uint16 Ceylan::Uint16Min =     0 ;
+Ceylan::Uint16 Ceylan::Uint16Max = 65535 ;
 
-Ceylan::Sint32 Sint32Min = -2147483647 ;
-Ceylan::Sint32 Sint32Max =  2147483647 ;
+Ceylan::Sint32 Ceylan::Sint32Min = -2147483647 ;
+Ceylan::Sint32 Ceylan::Sint32Max =  2147483647 ;
 
-Ceylan::Uint32 Uint32Min =          0 ;
-Ceylan::Uint32 Uint32Max = 2147483647 ; // should have been : 4294967294
+Ceylan::Uint32 Ceylan::Uint32Min =          0 ;
 
-Ceylan::Float32 Float32Min = -3.4E-38 ;
-Ceylan::Float32 Float32Max =  3.4E38  ;
+// Should have been 4294967294 :
+Ceylan::Uint32 Ceylan::Uint32Max = 2147483647 ; 
 
-Ceylan::Float64 Float64Min = -1.7E-308 ;
-Ceylan::Float64 Float64Max =  1.7E308  ;
+Ceylan::Float32 Ceylan::Float32Min = -3.4E-38 ;
+Ceylan::Float32 Ceylan::Float32Max =  3.4E38  ;
 
-//Ceylan::Float80 Float80Min = -3.4E-4932 ;
-//Ceylan::Float80 Float80Max = 1.7E308    ; // should have been : 3.4E4932
+Ceylan::Float64 Ceylan::Float64Min = -1.7E-308 ;
+Ceylan::Float64 Ceylan::Float64Max =  1.7E308  ;
+
+//Ceylan::Float80 Ceylan::Float80Min = -3.4E-4932 ;
+
+// Should have been 3.4E4932 :
+//Ceylan::Float80 Ceylan::Float80Max = 1.7E308    ; 
 
