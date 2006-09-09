8dd3c872563c351d4d0e41729c68a9478119ad55
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-09-09 21:10:54 +0000

    Blocking attribute support and toString method added.

--- src/code/system/CeylanInputOutputStream.cc
+++ src/code/system/CeylanInputOutputStream.cc
@@ -6,9 +6,9 @@ using namespace Ceylan::System ;
 
 
 
-InputOutputStream::InputOutputStream() throw() :
-	InputStream(),
-	OutputStream()
+InputOutputStream::InputOutputStream( bool blocking ) throw() :
+	InputStream( blocking ),
+	OutputStream( blocking )
 {
 
 }
@@ -19,3 +19,13 @@ InputOutputStream::~InputOutputStream() throw()
 
 }
 
+
+const std::string InputOutputStream::toString( Ceylan::VerbosityLevels level ) 
+	const throw()
+{
+
+	return "InputOutputStream, which is both an" 
+		+ InputStream::toString( level ) + ", and an "
+		+ OutputStream::toString( level ) ;
+
+}
