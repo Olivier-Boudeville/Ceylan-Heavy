01b92b08f5538ee1ade211a40b9dc6e2ff128d7b
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-09-12 15:42:25 +0000

    Ported from bash to sh, then to NetBSD, debug traces managed.

--- test/middleware/playTests-local.sh
+++ test/middleware/playTests-local.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# Not designed to be executed, must be sourced.
 
 # This script is a necessary special case since the server must be run
 # before the client is launched.
@@ -11,7 +11,7 @@
 # that no Ceylan server is already running, which may block the test.
 #
 
-#echo "playTests-local.sh sourced"
+DEBUG_INTERNAL "middleware/playTests-local.sh sourced"
 
 
 
@@ -32,20 +32,20 @@ multiplexed_lw_protocol_client_test_exec="${TEST_ROOT}/$m/${multiplexed_lw_proto
 
 
 # Have to be excluded from automatic selection since must be managed
-# specifically here :
+# specifically here :
 EXCLUDED_TESTS="${marshalled_server_test_name} ${multiplexed_lw_protocol_server_test_name} ${multiplexed_lw_protocol_client_test_name}"
 
 
 
 
-### Testing marshalled server :
+### Testing marshalled server :
 
 
 # First, launch the server :
 
-#echo "Will launch the server"
+DEBUG_INTERNAL "Will launch the server"
 t=${marshalled_server_test_exec}
-if [ "$is_batch" == "0" ] ; then
+if [ "$is_batch" = "0" ] ; then
 	echo -e "\n\n########### Running now $t" >>${TESTLOGFILE}
 	$t --batch 1>>${TESTLOGFILE} 2>&1 &
 	SERVER_PID="$!"
@@ -55,25 +55,25 @@ else
 fi			
 
 
-# Then do as if client test was a classical test :
+# Then do as if client test was a classical test :
 display_launching ${marshalled_client_test_name}
 
-# Ensure that the server is ready before the client :
+# Ensure that the server is ready before the client :
 sleep 1
 
-#echo "Will launch the client"
+DEBUG_INTERNAL "Will launch the client"
 run_test ${marshalled_client_test_name} ${marshalled_client_test_exec}
 
-#echo "Wait for the server"
+DEBUG_INTERNAL "Wait for the server"
 # Now inspect the server result :
 wait ${SERVER_PID} 
 return_code="$?"
-#echo "Server return code is $return_code"
+DEBUG_INTERNAL "Server return code is $return_code"
 
 display_launching ${marshalled_server_test_name}
 display_test_result "${marshalled_server_test_name}" "${marshalled_server_test_exec}" "$return_code"
 
-# Client already tested :
+# Client already tested :
 test_count=$(($test_count+1))
 
 
@@ -85,9 +85,9 @@ test_count=$(($test_count+1))
 
 # First, launch the server :
 
-#echo "Will launch the server"
+DEBUG_INTERNAL "Will launch the server"
 t=${multiplexed_lw_protocol_server_test_exec}
-if [ "$is_batch" == "0" ] ; then
+if [ "$is_batch" = "0" ] ; then
 	echo -e "\n\n########### Running now $t" >>${TESTLOGFILE}
 	$t --batch 1>>${TESTLOGFILE} 2>&1 &
 	SERVER_PID="$!"
@@ -103,19 +103,19 @@ display_launching ${multiplexed_lw_protocol_client_test_name}
 # Ensure that the server is ready before the client :
 sleep 1
 
-#echo "Will launch the client"
+DEBUG_INTERNAL "Will launch the client"
 run_test ${multiplexed_lw_protocol_client_test_name} ${multiplexed_lw_protocol_client_test_exec}
 
-#echo "Wait for the server"
+DEBUG_INTERNAL "Wait for the server"
 # Now inspect the server result :
 wait ${SERVER_PID} 
 return_code="$?"
-#echo "Server return code is $return_code"
+DEBUG_INTERNAL "Server return code is $return_code"
 
 display_launching ${multiplexed_lw_protocol_server_test_name}
 display_test_result "${multiplexed_lw_protocol_server_test_name}" "${multiplexed_server_test_exec}" "$return_code"
 
 # test client not already counted :
-test_count=$(($test_count+1))
+test_count=$(($test_count+2))
 
 
