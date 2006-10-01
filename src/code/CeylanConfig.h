bc15bb4b67f265e0b0c72423a3cb2a3490156f3b
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-10-01 15:57:56 +0000

    Redirects to Windows counterpart (CeylanConfigForWindows.h) on this platform.

---
+++ src/code/CeylanConfig.h
@@ -0,0 +1,18 @@
+#ifndef _SRC_CODE_CEYLANCONFIG_H
+#define _SRC_CODE_CEYLANCONFIG_H 1
+
+
+/*
+ * This is the CeylanConfig.h version for Windows.
+ *
+ * On UNIX platforms, this file will be overwritten by the configure-generated
+ * CeylanConfig.h
+ *
+ */
+
+#ifdef CEYLAN_RUNS_ON_WINDOWS
+#include "CeylanConfigForWindows.h"
+#endif // CEYLAN_RUNS_ON_WINDOWS
+
+/* once: _SRC_CODE_CEYLANCONFIG_H */
+#endif // _SRC_CODE_CEYLANCONFIG_H
