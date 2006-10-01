f9681be1e0d6536a81cb209207f374c32cf8da54
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-10-01 13:07:23 +0000

    Needed for Windows, only.

---
+++ src/code/generic/CeylanHeaderVersion.cc
@@ -0,0 +1,23 @@
+#include "CeylanHeaderVersion.h"
+
+
+#ifdef CEYLAN_RUNS_ON_WINDOWS
+
+#include "CeylanConfigForWindows.h"    // for CEYLAN_LIBTOOL_VERSION
+
+/*
+ * This is the CeylanHeaderVersion.cc version for Windows.
+ *
+ * On UNIX platforms, this file will be overwritten by the configure-generated
+ * CeylanHeaderVersion.cc
+ *
+ */
+
+const std::string Ceylan::actualCeylanHeaderLibtoolVersion
+    = CEYLAN_LIBTOOL_VERSION ;
+
+#else // CEYLAN_RUNS_ON_WINDOWS
+
+#error "This version of CeylanHeaderVersion.cc is for Windows platforms only."
+
+#endif // CEYLAN_RUNS_ON_WINDOWS
