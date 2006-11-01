0e2a0ace783d2caa50a0b36a12791857e5bfb25a
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-11-01 10:52:15 +0000

    At least it compiles fine, now !

--- src/code/network/CeylanNetwork.cc
+++ src/code/network/CeylanNetwork.cc
@@ -45,11 +45,12 @@ extern "C"
 #include <winsock2.h>  // for network errors
 #endif // CEYLAN_USES_WINSOCK2_H
 
+
 }
 
 // Templates cannot be declared to have 'C' linkage :
 #ifdef CEYLAN_USES_WS2TCPIP_H
-//#include <ws2tcpip.h>  // for getaddrinfo
+#include <ws2tcpip.h>  // for getaddrinfo
 #endif // CEYLAN_USES_WS2TCPIP_H
 
 
@@ -156,7 +157,7 @@ struct HostDNSEntry::SystemSpecificHostEntry
  *
  */
  
-//FIXME
+
 
 HostDNSEntry::HostDNSEntry( const std::string & hostName )
 		throw( NetworkException ) :
@@ -276,6 +277,8 @@ HostDNSEntry::HostDNSEntry( const IPAddress & ip ) throw( NetworkException )
 
 #endif // CEYLAN_USES_NETDB_H
 
+#endif // CEYLAN_USES_WS2TCPIP_H
+
 }
 					
 												
@@ -342,7 +345,7 @@ list<string> & HostDNSEntry::getAliasList() const throw()
 
 	while ( currentAlias != 0 )
 	{
-		res->push_back( string( currentAlias->ai_canonname ) ) ;
+		res.push_back( string( currentAlias->ai_canonname ) ) ;
 		currentAlias = currentAlias->ai_next ;
 	}
 
@@ -451,15 +454,25 @@ list<IPAddress *> & HostDNSEntry::getAddresses() const throw()
 
 	do
 	{
-
-		currentAddressStruct = currentAddressInfo->ai_addr ;
 		
+		/*
+		 * currentAddressInfo->ai_addr is a pointer to 'struct sockaddr'
+		 * whereas we need a pointer to 'struct sockaddr_in' : 
+		 *
+		 * @see http://msdn.microsoft.com/library/default.asp?url=/library/en-us/winsock/winsock/sockaddr_2.asp
+		 *
+		 */
+		currentAddressStruct = reinterpret_cast<struct sockaddr_in*>( 
+			currentAddressInfo->ai_addr ) ;
+
+		char * decodedAddress ;
+
 		switch( currentAddressInfo->ai_family  )		
 		{
 	
 			case IPv4:
-				currentEffectiveAddress = & currentAddressStruct.sin_addr ;			
-				char * decodedAddress = ::inet_ntoa( *currentEffectiveAddress ) ;
+				currentEffectiveAddress = & currentAddressStruct->sin_addr ;			
+				decodedAddress = ::inet_ntoa( *currentEffectiveAddress ) ;
 				if ( decodedAddress != 0 )
 					res.push_back( 
 						new IPAddressvFour( string( decodedAddress ) ) ) ;	
@@ -483,7 +496,9 @@ list<IPAddress *> & HostDNSEntry::getAddresses() const throw()
 		currentAddressInfo = currentAddressInfo->ai_next ;
 
 	}
-	while ( currentAddressInfo != 0 )
+	while ( currentAddressInfo != 0 ) ;
+
+	return res ;
 
 
 #else // CEYLAN_USES_WS2TCPIP_H
@@ -673,3 +688,700 @@ void HostDNSEntry::manageHostEntry() throw( NetworkException )
 }
 
 
+
+// Separate functions.
+
+
+
+const string Ceylan::Network::getLocalHostName() throw( NetworkException )
+{
+
+#ifdef CEYLAN_USES_WINSOCK2_H
+
+		// HOST_NAME_MAX does not seem to be widely defined :
+	char hostBuffer[ HostDNSEntry::HostNameMaxLength  + 1 ] ;
+
+	if ( ::gethostname( hostBuffer, HostDNSEntry::HostNameMaxLength ) != 0 )
+		throw NetworkException( "Ceylan::Network::getLocalHostName : "
+			"unable to determine local host name (error code : " 
+			+ Ceylan::toString( WSAGetLastError() ) + ")." ) ;
+	
+	return string( hostBuffer ) ;
+
+#else // CEYLAN_USES_WINSOCK2_H
+
+#ifdef CEYLAN_USES_GETHOSTNAME
+
+	// int uname(struct utsname *buf) in sys/utsname.h coud be used as well.
+	
+	// HOST_NAME_MAX does not seem to be widely defined :
+	char hostBuffer[ HostDNSEntry::HostNameMaxLength  + 1 ] ;
+
+	if ( ::gethostname( hostBuffer, HostDNSEntry::HostNameMaxLength ) != 0 )
+		throw NetworkException( "Ceylan::Network::getLocalHostName : "
+			"unable to determine local host name : " + explainError() ) ;
+
+	return string( hostBuffer ) ;
+
+#else // CEYLAN_USES_GETHOSTNAME
+
+	throw NetworkException( "Ceylan::Network::getLocalHostName : "
+		"not available on this platform." ) ;
+		
+#endif // CEYLAN_USES_GETHOSTNAME
+
+#endif // CEYLAN_USES_WINSOCK2_H
+
+}
+
+
+void Ceylan::Network::setLocalHostName( const string & newHostName )
+	throw( NetworkException )
+{
+
+#ifdef CEYLAN_USES_SETHOSTNAME
+
+	// Some versions of Solaris do not declare it in the right header :
+	if ( ::sethostname( newHostName.c_str(), newHostName.size() ) )
+		throw NetworkException( "Ceylan::Network::setLocalHostName : "
+			"unable to set local host name to "
+			+ newHostName + " : " + explainError( getError() ) ) ;
+			
+#else // CEYLAN_USES_SETHOSTNAME
+
+	throw NetworkException( "Ceylan::Network::setLocalHostName : "
+		"not available on this platform." ) ;
+		
+#endif // CEYLAN_USES_SETHOSTNAME
+
+}
+
+
+const string Ceylan::Network::getLocalHostDomainName() throw( NetworkException )
+{
+
+#ifdef CEYLAN_USES_GETDOMAINNAME
+	
+	char domainBuffer[ 256 ] ;
+
+	if ( ::getdomainname( domainBuffer, 255 ) != 0 )
+		throw NetworkException( "Unable to determine local host domain name : "
+			+ explainError( getError() ) ) ;
+
+	string res( domainBuffer ) ;
+	
+	if ( res == "(none)" )
+		return "" ;
+		
+	return res ;
+	
+#else // CEYLAN_USES_GETDOMAINNAME
+
+	throw NetworkException( "Ceylan::Network::getLocalHostDomainName : "
+		"not available on this platform." ) ;
+		
+#endif // CEYLAN_USES_GETDOMAINNAME
+
+}
+
+
+void Ceylan::Network::setLocalHostDomainName( const string & newDomainName )
+	throw( NetworkException )
+{
+
+#ifdef CEYLAN_USES_SETDOMAINNAME
+	
+	if ( ::setdomainname( newDomainName.c_str(), newDomainName.size() ) )
+		throw NetworkException( "Unable to set local host domain name to "
+			+ newDomainName + " : "
+			+ explainError( getError() ) ) ;
+			
+#else // CEYLAN_USES_SETDOMAINNAME
+
+	throw NetworkException( "Ceylan::Network::setLocalHostDomainName : "
+		"not available on this platform." ) ;
+		
+#endif // CEYLAN_USES_SETDOMAINNAME
+			
+
+}
+
+
+
+
+const string Ceylan::Network::getMostPreciseLocalHostName() 
+	throw( NetworkException )
+{
+
+	string guessedFullHostname ;
+	
+#ifdef CEYLAN_USES_UNAME
+
+	struct utsname buf ;
+
+	if ( ::uname( & buf ) != 0 )
+	{		
+		LogPlug::error( "Ceylan::Network::getMostPreciseLocalHostName : "
+			"unable to determine name of local host : " 
+			+ explainError( getError() ) ) ;
+	}		
+	else
+	{		
+	
+		guessedFullHostname = buf.nodename ;
+
+		// Needing a FQDN, checking for dots in the host name :
+		if ( Ceylan::countChars( guessedFullHostname, '.' ) != 0 )
+			return guessedFullHostname ;
+			
+	}
+						
+#endif // CEYLAN_USES_UNAME 	
+	
+	
+	/*
+	 * Here we have no real FQDN, let's try with the DNS :
+	 *
+	 */
+
+	/*
+	 * Do not catch any exception, since we cannot succeed without the 
+	 * basic hostname :
+	 *
+	 */
+	string thisHostname = getLocalHostName() ;
+	
+	bool found = false ;
+	
+	try
+	{
+
+		guessedFullHostname = getFQDNFromHostname( thisHostname ) ;
+		found = true ;
+		
+	}
+	catch( const NetworkException & e )
+	{
+		LogPlug::debug( "Ceylan::Network::getMostPreciseLocalHostName : "
+			"FQDN not available : " + e.toString() ) ;
+	}
+
+	if ( found )
+		return guessedFullHostname ;
+
+	/*
+	 * Damned, not found.
+	 * Last try : hostname.domainname, let's go for the domain name :
+	 *
+	 */
+	
+	string domain ;
+
+	try
+	{
+
+		// This is the *domain* here :
+		domain = getLocalHostDomainName() ;
+
+	}
+	catch( const NetworkException & e )
+	{
+		LogPlug::debug( "Ceylan::Network::getMostPreciseLocalHostName : "
+			"domain name not available : " + e.toString() ) ;
+		domain = "" ;
+
+	}
+
+	if ( ! domain.empty() )
+	{
+		// We seem to have a good domain name, let's keep it.
+		return thisHostname + "." + domain ;
+	}
+
+	// Failed to have a FQDN, returning the mere hostname instead :
+	return thisHostname ;
+
+}
+
+
+
+
+// Section for all hosts.
+		
+		
+				
+const string Ceylan::Network::getFQDNFromIP( const IPAddress & ip )
+	throw( NetworkException )
+{
+
+	HostDNSEntry searched( ip ) ;
+	
+	return getFQDNFromDNSEntry( searched ) ;
+
+}
+			
+			
+const string Ceylan::Network::getFQDNFromIPv4( const std::string & ipString )
+	throw( NetworkException ) 
+{
+
+	IPAddressvFour ip( ipString ) ;
+	
+	return getFQDNFromIP( ip ) ;
+	
+}
+			
+									
+const string Ceylan::Network::getFQDNFromHostname( 
+	const std::string & hostname ) throw( NetworkException )
+{
+
+	HostDNSEntry searched( hostname ) ;
+	
+	return getFQDNFromDNSEntry( searched ) ;
+		
+}
+
+
+const string Ceylan::Network::getFQDNFromDNSEntry( const HostDNSEntry & entry )
+	throw( NetworkException ) 
+{
+
+	string current = entry.getOfficialHostName() ;
+	
+	// Try the official name as FQDN :
+	if ( Ceylan::countChars( current, '.' ) != 0 )
+		return current ;
+	
+	// No, try alias :
+	list<string> * alias = & entry.getAliasList() ;
+	
+	for ( list<string>::const_iterator it = alias->begin() ;
+			it != alias->end(); it++ )
+	{		
+		if ( Ceylan::countChars( (*it), '.' ) != 0 ) 
+		{
+			current = *it ;
+			break ;
+		}
+	}
+	
+	delete alias ;
+	
+	return current ;	
+		
+}
+
+
+bool Ceylan::Network::isAValidHostName( const string & hostnameString ) throw()
+{
+
+#if CEYLAN_USES_REGEX
+	
+	Ceylan::RegExp target( hostnameString ) ;
+
+	return target.matches(
+		"^(([a-z]|[A-Z]{1,1})([a-z]|[A-Z]|[.]|[0-9]|[-]){0,})$" ) ;
+	
+#else // CEYLAN_USES_REGEX
+	
+	// Check disabled :
+	return true ;
+	
+#endif // CEYLAN_USES_REGEX
+
+}
+
+
+/*
+ * On Windows, error codes should be interpreted according to the 
+ * following table :
+ * http://msdn.microsoft.com/library/en-us/winsock/winsock/windows_sockets_error_codes_2.asp
+ *
+ * Sadly the conversion from an error code to a textual message seems
+ * to be up to the programmer.
+ * 
+ */
+string Ceylan::Network::interpretSocketError( SocketError errorCode ) 
+	throw()
+{
+
+#if CEYLAN_RUNS_ON_WINDOWS
+
+	switch( errorCode )
+	{
+
+	case WSAEINTR:
+		return "interrupted function call (WSAEINTR) : "
+			"a blocking operation was interrupted "
+			"by a call to WSACancelBlockingCall" ;
+		break ;
+
+	case WSAEACCES:
+		return "permission denied (WSAEACCES) : "
+			"an attempt was made to access a socket in a way forbidden "
+			"by its access permissions" ;
+		break ;
+
+	case WSAEFAULT:
+		return "bad address (WSAEFAULT) : "
+			"the system detected an invalid pointer address in attempting "
+			"to use a pointer argument of a call" ;
+		break ;
+
+	case WSAEINVAL:
+		return "invalid argument (WSAEINVAL) : "
+			"some invalid argument was supplied" ;
+		break ;
+
+	case WSAEMFILE:
+		return "too many open sockets (WSAEMFILE)" ;
+		break ;
+
+	case WSAEWOULDBLOCK:
+		return "resource temporarily unavailable (WSAEWOULDBLOCK) : "
+			"this error is returned from operations on nonblocking sockets "
+			"that cannot be completed immediately" ;
+		break ;
+
+	case WSAEINPROGRESS:
+		return "operation now in progress (WSAEINPROGRESS) : "
+			"a blocking operation is currently executing" ;
+		break ;
+
+	case WSAEALREADY:
+		return "operation already in progress (WSAEALREADY) : "
+			"an operation was attempted on a nonblocking socket "
+			"with an operation already in progress" ;
+		break ;
+
+	case WSAENOTSOCK:
+		return "socket operation on nonsocket (WSAENOTSOCK) : "
+			"an operation was attempted on something that is not a socket" ;
+		break ;
+
+	case WSAEDESTADDRREQ:
+		return "destination address required (WSAEDESTADDRREQ) : "
+			"a required address was omitted from an operation on a socket" ;
+		break ;
+
+	case WSAEMSGSIZE:
+		return "message too long (WSAEMSGSIZE) : "
+			"a message sent on a datagram socket was larger than supported" ;
+		break ;
+
+	case WSAEPROTOTYPE:
+		return "wrong protocol type for socket (WSAEPROTOTYPE)" ;
+		break ;
+
+	case WSAENOPROTOOPT:
+		return "bad protocol option (WSAENOPROTOOPT) : "
+			"unknown, invalid or unsupported option or level" ;
+		break ;
+
+	case WSAEPROTONOSUPPORT:
+		return "requested protocol not supported on this system "
+			"(WSAEPROTONOSUPPORT)" ;
+		break ;
+
+	case WSAESOCKTNOSUPPORT:
+		return "socket type not supported (WSAESOCKTNOSUPPORT) : "
+			"the support for the specified socket type does not exist "
+			"in this address family" ;
+		break ;
+
+	case WSAEOPNOTSUPP:
+		return "operation not supported (WSAEOPNOTSUPP) : "
+			"the attempted operation is not supported "
+			"for the type of object referenced" ;
+		break ;
+
+	case WSAEPFNOSUPPORT:
+		return "protocol family not supported on this system "
+			"(WSAEPFNOSUPPORT)" ;
+		break ;
+
+	case WSAEAFNOSUPPORT:
+		return "address family not supported by protocol family "
+			"(WSAEAFNOSUPPORT)" ;
+		break ;
+
+	case WSAEADDRINUSE:
+		return "address already in use (WSAEADDRINUSE) : "
+			"typically, only one usage of each socket address "
+			"(protocol/IP address/port) is permitted" ;
+		break ;
+
+	case WSAEADDRNOTAVAIL:
+		return "cannot assign requested address (WSAEADDRNOTAVAIL) : "
+			"the requested address is not valid in its context" ;
+		break ;
+
+	case WSAENETDOWN:
+		return "network is down (WSAENETDOWN) : "
+			"a socket operation encountered a dead network" ;
+		break ;
+
+	case WSAENETUNREACH:
+		return "network is unreachable (WSAENETUNREACH) : "
+			"a socket operation was attempted to an unreachable network" ;
+		break ;
+
+	case WSAENETRESET:
+		return "network dropped connection on reset (WSAENETRESET)" ;
+		break ;
+
+	case WSAECONNABORTED:
+		return "software caused connection abort (WSAECONNABORTED) : "
+			"an established connection was aborted by the software "
+			"in your host computer" ;
+		break ;
+
+	case WSAECONNRESET:
+		return "connection reset by peer (WSAECONNRESET) : "
+			"an existing connection was forcibly closed by the remote host" ;
+		break ;
+
+	case WSAENOBUFS:
+		return "no buffer space available (WSAENOBUFS)" ;
+		break ;
+
+	case WSAEISCONN:
+		return "socket is already connected (WSAEISCONN) : "
+			"a connect request was made on an already-connected socket" ;
+		break ;
+
+	case WSAENOTCONN:
+		return "socket is not connected (WSAENOTCONN) : "
+			"a request to send or receive data was disallowed because "
+			"the socket is not connected and/or no address was supplied" ;
+		break ;
+
+	case WSAESHUTDOWN:
+		return "cannot send after socket shutdown (WSAESHUTDOWN) : "
+			"a request to send or receive data was disallowed because "
+			"the socket had already been shut down in that direction" ;
+		break ;
+
+	case WSAETIMEDOUT:
+		return "a connection attempt failed because of a time-out "
+			"(WSAETIMEDOUT)" ;
+		break ;
+
+	case WSAECONNREFUSED:
+		return "connection refused (WSAECONNREFUSED) : "
+			"no connection could be made because "
+			"the target computer actively refused it" ;
+		break ;
+
+	case WSAEHOSTDOWN:
+		return "host is down (WSAEHOSTDOWN) : "
+			"a socket operation failed because the destination host is down" ;
+		break ;
+
+	case WSAEHOSTUNREACH:
+		return "no route to host (WSAEHOSTUNREACH) : "
+			"a socket operation was attempted to an unreachable host" ;
+		break ;
+
+	case WSAEPROCLIM:
+		return "too many processes (WSAEPROCLIM) : "
+			"a Windows Sockets implementation may have a limit on "
+			"the number of applications that can use it simultaneously" ;
+		break ;
+
+	case WSASYSNOTREADY:
+		return "network subsystem is unavailable (WSASYSNOTREADY)" ;
+		break ;
+
+	case WSAVERNOTSUPPORTED:
+		return "Winsock.dll version out of range (WSAVERNOTSUPPORTED) : "
+			"the current Windows Sockets implementation "
+			"does not support the Windows Sockets specification version "
+			"requested by the application" ;
+		break ;
+
+	case WSANOTINITIALISED:
+		return "successful WSAStartup not yet performed (WSANOTINITIALISED)" ;
+		break ;
+
+	case WSAEDISCON:
+		return "graceful shutdown in progress (WSAEDISCON)" ;
+		break ;
+
+	case WSATYPE_NOT_FOUND:
+		return "class type not found (WSATYPE_NOT_FOUND)" ;
+		break ;
+
+	case WSAHOST_NOT_FOUND:
+		return "host not found (WSAHOST_NOT_FOUND) : "
+			"no such host is known" ;
+		break ;
+
+	case WSATRY_AGAIN:
+		return "nonauthoritative host not found (WSATRY_AGAIN) : "
+			"this is usually a temporary error during host name resolution" ;
+		break ;
+
+	case WSANO_RECOVERY:
+		return "nonrecoverable error (WSANO_RECOVERY) occurred "
+			"during a database lookup" ;
+		break ;
+
+	case WSANO_DATA:
+		return "valid name, no data record of requested type (WSANO_DATA)" ;
+		break ;
+
+	case WSA_INVALID_HANDLE:
+		return "specified event object handle is invalid "
+			"(WSA_INVALID_HANDLE)" ;
+		break ;
+
+	case WSA_INVALID_PARAMETER:
+		return "one or more parameters are invalid (WSA_INVALID_PARAMETER)" ;
+		break ;
+
+	case WSA_IO_INCOMPLETE:
+		return "overlapped I/O event object not in signaled state "
+			"(WSA_IO_INCOMPLETE) : the application has tried to determine the"
+			" status of an overlapped operation which is not yet completed" ;
+		break ;
+
+	case WSA_IO_PENDING:
+		return "overlapped operations will complete later (WSA_IO_PENDING) : "
+			"the application has initiated an overlapped operation that "
+			"cannot be completed immediately" ;
+		break ;
+
+	case WSA_NOT_ENOUGH_MEMORY:
+		return "insufficient memory available (WSA_NOT_ENOUGH_MEMORY)" ;
+		break ;
+
+	case WSA_OPERATION_ABORTED:
+		return "overlapped operation aborted (WSA_OPERATION_ABORTED)" ;
+		break ;
+
+/* 
+ * Not supported, at least not on Windows XP :
+
+	case WSAINVALIDPROCTABLE:
+		return "invalid procedure table from service provider "
+			"(WSAINVALIDPROCTABLE)" ;
+		break ;
+
+	case WSAINVALIDPROVIDER:
+		return "invalid service provider version number "
+			"(WSAINVALIDPROVIDER)" ;
+		break ;
+
+	case WSAPROVIDERFAILEDINIT:
+		return "unable to initialize a service provider "
+			"(WSAPROVIDERFAILEDINIT)" ;
+		break ;
+*/
+
+	case WSASYSCALLFAILURE:
+		return "system call failure (WSASYSCALLFAILURE) : "
+			"generic error code" ;
+		break ;
+
+	default: 
+		return "unknown socket error code #" 
+			+ Ceylan::toString( errorCode ) + " (abnormal)" ;
+
+	}
+
+#else // CEYLAN_RUNS_ON_WINDOWS
+
+	return "nothing appropriate for the current platform "
+		"(abnormal)" ;
+
+#endif // CEYLAN_RUNS_ON_WINDOWS
+
+}
+
+
+SocketError Ceylan::Network::getSocketError() throw()
+{
+	return WSAGetLastError() ;
+}
+
+
+std::string Ceylan::Network::explainSocketError() throw()
+{
+
+#if CEYLAN_RUNS_ON_WINDOWS
+
+	return interpretSocketError( getSocketError() ) ;
+
+#else // CEYLAN_RUNS_ON_WINDOWS
+
+	return "Ceylan::Network::explainSocketError should not be "
+		"used on this platform" ;
+
+#endif // CEYLAN_RUNS_ON_WINDOWS
+
+}
+
+
+
+// NetworkManager section.
+
+
+#if CEYLAN_RUNS_ON_WINDOWS
+
+NetworkManager NetworkManager::_Manager ;
+
+NetworkManager::NetworkManager() throw( NetworkException ) 
+{
+
+#if CEYLAN_DEBUG_SYSTEM
+
+	Ceylan::display( "NetworkManager constructor : "
+		"initializing network subsystem." ) ;
+
+#endif // CEYLAN_DEBUG_SYSTEM
+
+	WORD requestedVersion = MAKEWORD( 2, 2 ) ;
+	WSADATA wsaData ;
+ 
+	int result = WSAStartup( requestedVersion, & wsaData ) ;
+
+	if ( result != 0 )
+		throw NetworkException( "NetworkManager constructor failed : "
+			"no usable WinSock DLL found (error code : "
+			+ Ceylan::toString( result ) + ")." ) ;
+
+	/*
+	 * Confirms that the WinSock DLL supports 2.2.
+	 *
+	 * @note If the DLL supports versions greater than 2.2 in addition to 2.2, 
+	 * it will still return 2.2 in wVersion since that is the requested version.
+	 *
+	 */
+	if ( LOBYTE( wsaData.wVersion ) != 2 || HIBYTE( wsaData.wVersion ) != 2 )
+	{
+		WSACleanup() ;
+		throw NetworkException( "NetworkManager constructor failed : "
+			"this version of Winsock DLL is not supported." ) ;
+	}
+
+}
+ 
+
+NetworkManager::~NetworkManager() throw()
+{
+
+#if CEYLAN_DEBUG_SYSTEM
+
+	Ceylan::display( "NetworkManager destructor : "
+		"shutting down network subsystem." ) ;
+
+#endif // CEYLAN_DEBUG_SYSTEM
+
+	WSACleanup() ;
+
+}
+
+
+#endif // CEYLAN_RUNS_ON_WINDOWS
