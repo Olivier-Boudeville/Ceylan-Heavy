a1644456c38646ce9e25843f62a1468c292ffb69
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-11-25 14:09:29 +0000

    Visitors may change visited elements (non-const then), XMLSearchingVisitor added.

--- src/code/modules/CeylanXMLVisitor.h
+++ src/code/modules/CeylanXMLVisitor.h
@@ -5,10 +5,12 @@
 #include "CeylanTree.h"             // for Ceylan::Tree
 #include "CeylanXML.h"              // for XML general operations
 #include "CeylanVisitor.h"          // for inheritance
+#include "CeylanXMLElement.h"       // for Markup::MarkupName
 
 
 #include <string>
 #include <stack>   // for stack
+#include <list>    // for list
 
 
 
@@ -100,8 +102,12 @@ namespace Ceylan
 				 *
 				 * @throw VisitException if this visit failed.
 				 *
+				 * @note The markup is not 'const' to allow for visitors able
+				 * to change the visited elements, ex : a searching visitor
+				 * may need to return a list of non-const markups.
+				 *
 				 */
-				virtual void visit( const XMLMarkup & xmlMarkup ) 
+				virtual void visit( XMLMarkup & xmlMarkup ) 
 					throw( VisitException ) = 0 ;
 				
 	
@@ -112,8 +118,12 @@ namespace Ceylan
 				 *
 				 * @throw VisitException if this visit failed.
 				 *
+				 * @note The text is not 'const' to allow for visitors able
+				 * to change the visited elements, ex : a searching visitor
+				 * may need to return a list of non-const texts.
+				 *
 				 */
-				virtual void visit( const XMLText & xmlText ) 
+				virtual void visit( XMLText & xmlText ) 
 					throw( VisitException ) = 0 ;
 				
 	
@@ -228,8 +238,11 @@ namespace Ceylan
 				 *
 				 * @throw VisitException if this saving failed.
 				 *
+				 * @note If the signature was not inherited, the specified 
+				 * markup would by 'const'.
+				 *
 				 */
-				virtual void visit( const XMLMarkup & xmlMarkup ) 
+				virtual void visit( XMLMarkup & xmlMarkup ) 
 					throw( VisitException ) ;
 				
 				
@@ -242,8 +255,11 @@ namespace Ceylan
 				 *
 				 * @throw VisitException if this saving failed.
 				 *
+				 * @note If the signature was not inherited, the specified 
+				 * text would by 'const'.
+				 *
 				 */
-				virtual void visit( const XMLText & xmlText ) 
+				virtual void visit( XMLText & xmlText ) 
 					throw( VisitException ) ;
 				
 	
@@ -307,6 +323,165 @@ namespace Ceylan
 				 */			 
 				XMLSavingVisitor & operator = ( 
 					const XMLSavingVisitor & source ) throw() ;
+					
+					
+		} ;			
+				
+				
+			
+		/**
+		 * XML visitor dedicated to the searching in an XML tree of specific
+		 * elements.
+		 *
+		 * @note Using a visitor here leads to inefficient search algorithm.
+		 *
+		 * @note This visitor does not care about height information, hence 
+		 * being height-aware is useless here.
+		 *
+		 */
+		class CEYLAN_DLL XMLSearchingVisitor : public XMLVisitor
+		{
+		
+		
+			public:
+
+
+
+				/**
+				 * Creates a XML visitor dedicated to the searching of a XML
+				 * tree.
+				 *
+				 * @param searchedMarkupName the name of the searched markup,
+				 * for example 'para'.
+				 *
+				 */
+				XMLSearchingVisitor( MarkupName & searchedMarkupName ) 
+					throw() ;
+
+
+				/**
+				 * Virtual destructor.
+				 *
+				 */
+				virtual ~XMLSearchingVisitor() throw() ;
+		
+				
+				/**
+				 * Find the XML markups from the visited tree that match the
+				 * markup name specified in the constructor of this visitor.
+				 *
+				 * @note The target tree must have been visited first so that
+				 * nodes have been already searched, for example :
+				 * <pre>
+				 * XMLSearchingVisitor myXMLSearchingVisitor( "ul" ) ;
+				 * myXMLTree.accept( myXMLSearchingVisitor ) ; 
+				 * // Here the visitor has computed the list, then :
+				 * std::list<XMLMarkup *> * myList = &
+				 *	 myXMLSearchingVisitor.getMatchingMarkups() ;
+				 * ...
+				 * // No deleting of myList nor of its elements, as ownership
+				 * // kept by the visitor.
+				 * </pre>
+				 *
+				 * @return the internal list of the visitor, which keeps its
+				 * ownership. This list references the XML markups, from the 
+				 * visited tree, that match the specified markup name.
+				 * These markups remain the property of the visited tree,
+				 * therefore the elements of the list must not
+				 * be deallocated by the caller of getMatchingMarkups.
+				 *
+				 * @throw VisitException if the operation failed.
+				 *
+				 * @note getMatchingMarkups should be called only once the 
+				 * tree has been visited, otherwise an empty list will be
+				 * returned.
+				 *
+				 */
+				std::list<XMLMarkup *> & getMatchingMarkups() 
+					throw( VisitException ) ;
+
+				
+				/**
+				 * Visits specified XML markup and records it if it matches
+				 * the search.
+				 *
+				 * @param xmlMarkup the XML markup to visit.
+				 *
+				 * @throw VisitException if the visit failed.
+				 *
+				 */
+				virtual void visit( XMLMarkup & xmlMarkup ) 
+					throw( VisitException ) ;
+				
+				
+	
+				/**
+				 * Visits specified XML text.
+				 *
+				 * @param xmlText the XML text to visit.
+				 *
+				 * @throw VisitException if this visit failed.
+				 *
+				 */
+				virtual void visit( XMLText & xmlText ) 
+					throw( VisitException ) ;
+				
+	
+			
+				/**
+				 * Returns a user-friendly description of the state of this
+				 * object.
+				 *
+				 * @param level the requested verbosity level.
+				 *
+				 * @note Text output format is determined from overall settings.
+				 *
+				 * @see TextDisplayable
+				 *
+				 */
+				 virtual const std::string toString( 
+			 			Ceylan::VerbosityLevels level = Ceylan::high ) 
+					const throw() ;
+						
+		
+		
+		
+			protected:
+			
+			
+				/// The markup that is searched for.
+				MarkupName _searchedMarkup ;
+				
+				/// The list that will contain all nodes whose content matches.
+				std::list<XMLMarkup *> _matchingNodes ;
+				
+
+			private:
+
+
+				/**
+				 * Copy constructor made private to ensure that it will never
+				 * be called.
+				 *
+				 * The compiler should complain whenever this undefined
+				 * constructor is called, implicitly or not.
+				 * 
+				 * @note Made to avoid unwanted hidden clone of the Singleton.
+				 *
+				 */			 
+				XMLSearchingVisitor( const XMLSearchingVisitor & source )
+					throw() ;
+			
+			
+				/**
+				 * Assignment operator made private to ensure that it will 
+				 * never be called.
+				 * The compiler should complain whenever this undefined 
+				 * operator is called, implicitly or not.
+				 * 
+				 */			 
+				XMLSearchingVisitor & operator = ( 
+					const XMLSearchingVisitor & source ) throw() ;
 					
 					
 		} ;			
