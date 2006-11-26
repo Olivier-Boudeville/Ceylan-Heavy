76a4a06d9f03601cdd11065b52277155eade142c
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-11-26 13:31:10 +0000

    A full test is now managed : a XML tree is saved into file, then read then saved again, and a diff is made between both files.

--- test/modules/testCeylanXMLParser.cc
+++ test/modules/testCeylanXMLParser.cc
@@ -10,6 +10,9 @@ using namespace Ceylan::XML ;
 #include <string>
 using std::string ;
 
+#include <list>
+using std::list ;
+
 
 
 
@@ -32,102 +35,277 @@ int main( int argc, char * argv[] )
 		LogPlug::info( "Testing XML parser." ) ;
 
 
-		XMLParser myTestParser( "testCeylanXMLParser-output.xml" ) ;
-		LogPlug::info( "Parser created : " + myTestParser.toString() ) ;
-	
-		string xmlFilename = "test-parser.xml" ;
+		bool testWritingXML = true ;
+		bool testReadingXML = true ;
 		
-		LogPlug::info( "Now, recreating programatically the content of '"
-			+ xmlFilename + "'." ) ;
+		string generatedFilename = "testCeylanXMLParser-generated.xml" ;
+		string processedFilename = "testCeylanXMLParser-processed.xml" ;
+		
+		
+		if ( testWritingXML )
+		
+		{
+
+			XMLParser myTestParser( generatedFilename ) ;
+			LogPlug::info( "Parser created : " + myTestParser.toString() ) ;
+	
+			string xmlFilename = "test-parser.xml" ;
+		
+			LogPlug::info( "Now, recreating programatically the content of '"
+				+ xmlFilename + "'." ) ;
 			
 		/*
 		 * The file content should be :
 		 
-<para width="100" height="200" color="brown">
-	<b city="London">Hello world !
-		<c length="14.1">Parse me</c>
-		<c/>
+<?xml version="1.0" encoding="ISO-8859-15"?>
+<para color="brown" height="200" width="100">
+	<b city="London">
+		Hello world !
+		<c length="14.1">
+			Parse me
+		</c>
 	</b>
-	
-	<d>Someone listening ?</d>
-	
+	<d>
+		Someone listening ?
+	</d>
+	<c height="7.1">
+	</c>
 </para>
 		 
 		 */
 		 
 		 	
-		// Not an automatic variable, as the markup will be owned by the tree :
-		XMLMarkup & myFirstMarkup = * new XMLMarkup( "para" ) ;
+			/*
+			 * Not an automatic variable, as the markup will be owned by 
+			 * the tree :
+			 *
+			 */
+			XMLMarkup & myFirstMarkup = * new XMLMarkup( "para" ) ;
 				
-		myFirstMarkup.setAttribute( "width" , "100"   ) ;
-		myFirstMarkup.setAttribute( "height", "200"   ) ;
-		myFirstMarkup.setAttribute( "color" , "brown" ) ;
+			myFirstMarkup.setAttribute( "width" , "100"   ) ;
+			myFirstMarkup.setAttribute( "height", "200"   ) ;
+			myFirstMarkup.setAttribute( "color" , "brown" ) ;
 
-		LogPlug::info( "Markup created : " + myFirstMarkup.toString() ) ;
+			LogPlug::info( "Markup created : " + myFirstMarkup.toString() ) ;
 
 
-		// Not an automatic variable, as the tree will be owned by the parser :
-		XMLParser::XMLTree & myTestTree = 
-			* new XMLParser::XMLTree( myFirstMarkup ) ;
+			/*
+			 * Not an automatic variable, as the tree will be owned by 
+			 * the parser :
+			 *
+			 */
+			XMLParser::XMLTree & myTestTree = 
+				* new XMLParser::XMLTree( myFirstMarkup ) ;
 
-		XMLMarkup & mySecondMarkup = * new XMLMarkup( "b" ) ;	
-		mySecondMarkup.setAttribute( "city", "London" ) ;
-			
-		XMLParser::XMLTree & myBTree = 
-			*new XMLParser::XMLTree( mySecondMarkup ) ;
-			
-		myTestTree.addSon( myBTree ) ;
-
-		// Not an automatic variable, as the markup will be owned by the tree :
-		XMLText & myFirstText = * new XMLText( "Hello world !" ) ;
-		LogPlug::info( "Text created : " + myFirstText.toString() ) ;
-
-		XMLParser::XMLTree & myFirstTextTree = 
-			*new XMLParser::XMLTree( myFirstText ) ;
-
-		myBTree.addSon( myFirstTextTree ) ;
-		
-		XMLMarkup & myThirdMarkup = * new XMLMarkup( "c" ) ;	
-		myThirdMarkup.setAttribute( "length", "14.1" ) ;
-
-		XMLParser::XMLTree & myCTree = 
-			*new XMLParser::XMLTree( myThirdMarkup ) ;
-		
-		myBTree.addSon( myCTree ) ;
-		
-		XMLText & mySecondText = * new XMLText( "Parse me" ) ;
+			XMLMarkup & mySecondMarkup = * new XMLMarkup( "b" ) ;
+			mySecondMarkup.setAttribute( "city", "London" ) ;
 	
-		XMLParser::XMLTree & mySecondTextTree = 
-			*new XMLParser::XMLTree( mySecondText ) ;
-		
-		myCTree.addSon( mySecondTextTree ) ;
-		
-		XMLMarkup & myFourthMarkup = * new XMLMarkup( "d" ) ;
-			
-		XMLParser::XMLTree & myDTree = 
-			*new XMLParser::XMLTree( myFourthMarkup ) ;
-		
-		myTestTree.addSon( myDTree ) ;
-		
-		XMLText & myThirdText = * new XMLText( "Someone listening ?" ) ;
+			XMLParser::XMLTree & myBTree =
+				*new XMLParser::XMLTree( mySecondMarkup ) ;
 	
-		XMLParser::XMLTree & myThirdTextTree = 
-			*new XMLParser::XMLTree( myThirdText ) ;
-	
-		myDTree.addSon( myThirdTextTree ) ;
-			
-		
-		LogPlug::info( "Tree created : " + myTestTree.toString() ) ;
-		
-		
-		myTestParser.setXMLTree( myTestTree ) ;
+			myTestTree.addSon( myBTree ) ;
 
-		LogPlug::info( "Parser recorded tree : " + myTestParser.toString() ) ;
+			/* 
+			 * Not an automatic variable, as the markup will be owned 
+			 * by the tree :
+			 *
+			 */
+			XMLText & myFirstText = * new XMLText( "Hello world !" ) ;
+			LogPlug::info( "Text created : " + myFirstText.toString() ) ;
+
+			XMLParser::XMLTree & myFirstTextTree =
+				*new XMLParser::XMLTree( myFirstText ) ;
+
+			myBTree.addSon( myFirstTextTree ) ;
+	
+			XMLMarkup & myThirdMarkup = * new XMLMarkup( "c" ) ;
+			myThirdMarkup.setAttribute( "length", "14.1" ) ;
+
+			XMLParser::XMLTree & myCTree =
+				*new XMLParser::XMLTree( myThirdMarkup ) ;
+	
+			myBTree.addSon( myCTree ) ;
+	
+			XMLText & mySecondText = * new XMLText( "Parse me" ) ;
+	
+			XMLParser::XMLTree & mySecondTextTree =
+				*new XMLParser::XMLTree( mySecondText ) ;
+	
+			myCTree.addSon( mySecondTextTree ) ;
+	
+			XMLMarkup & myFourthMarkup = * new XMLMarkup( "d" ) ;
+	
+			XMLParser::XMLTree & myDTree =
+				*new XMLParser::XMLTree( myFourthMarkup ) ;
+	
+			myTestTree.addSon( myDTree ) ;
+	
+			XMLText & myThirdText = * new XMLText( "Someone listening ?" ) ;
+	
+			XMLParser::XMLTree & myThirdTextTree =
+				*new XMLParser::XMLTree( myThirdText ) ;
+	
+			myDTree.addSon( myThirdTextTree ) ;
+
+			XMLMarkup & myFifthMarkup = * new XMLMarkup( "c" ) ;
+			myFifthMarkup.setAttribute( "height", "7.1" ) ;
+	
+	
+			XMLParser::XMLTree & mySecondCTree =
+				*new XMLParser::XMLTree( myFifthMarkup ) ;
+			myTestTree.addSon( mySecondCTree ) ;
+	
+	
+			LogPlug::info( "Tree created : " + myTestTree.toString() ) ;
+	
+	
+			myTestParser.setXMLTree( myTestTree ) ;
+
+			LogPlug::info( "Parser recorded tree : " 
+				+ myTestParser.toString() ) ;
+	
+	
+			// Storing the result in a XML file :
+			myTestParser.saveToFile() ;
+	
+	
+			LogPlug::info( 
+				"Now using the tree is memory to perform searches." ) ;
+	
+			LogPlug::info( "Searching for the father content of '"
+				+ encodeToHTML( myFirstText.toString() ) + "' : it should be '"
+				+ encodeToHTML( mySecondMarkup.toString() ) + "'." ) ;
+	
+			XMLElement * father = myTestTree.getFatherContent( myFirstText ) ;
+	
+			if ( father != & mySecondMarkup )
+			{
+	
+				if ( father != 0 )
+					throw Ceylan::TestException(
+						"Searching for the father content of '"
+						+ encodeToHTML( myFirstText.toString() )
+						+ "' failed : it should be '"
+						+ encodeToHTML( mySecondMarkup.toString() ) 
+						+ "', but '"
+						+ encodeToHTML( father->toString() ) 
+						+ "' was returned." ) ;
+				else
+					throw Ceylan::TestException(
+						"Searching for the father content of '"
+						+ encodeToHTML( myFirstText.toString() )
+						+ "' failed : it should be '"
+						+ encodeToHTML( mySecondMarkup.toString() )
+						+ "', but none was found." ) ;
+	
+			}
+			else
+				LogPlug::info( "Search succeeded." ) ;
+	
+			LogPlug::info( "Searching for the father content of '"
+				+ encodeToHTML( myFirstMarkup.toString() )
+				+ "' : none should be found, "
+				"as it is associated with the root node." ) ;
+	
+			father = myTestTree.getFatherContent( myFirstMarkup ) ;
+	
+			if ( father != 0 )
+				throw Ceylan::TestException(
+					"No parent content should be found for a root node, "
+					"whereas '" + encodeToHTML( father->toString() )
+					+ "' was found." ) ;
+	
+			LogPlug::info( "No parent content found, that is correct." ) ;
+	
+			LogPlug::info( "Searching for the content of sons of '"
+				+ encodeToHTML( mySecondMarkup.toString() )
+				+ "'." ) ;
+	
+			list<XMLElement *> sonsContents ;
+			myTestTree.appendSonsContentsOf( mySecondMarkup, sonsContents ) ;
+	
+			list<string> toDisplay ;
+	
+			for ( list<XMLElement *>::iterator it = sonsContents.begin() ;
+					it != sonsContents.end(); it++ )
+				toDisplay.push_back( encodeToHTML( (*it)->toString() ) ) ;
+	
+			LogPlug::info( "Child content of node associated with content '"
+				+ encodeToHTML( mySecondMarkup.toString() ) + "' is : "
+				+ formatStringList( toDisplay ) ) ;
+	
+	
+			LogPlug::info( "Testing now XMLSearchingVisitor." ) ;
+	
+			string searchedMarkup = "c" ;
+			XMLSearchingVisitor myTestVisitor( searchedMarkup ) ;
+			myTestTree.accept( myTestVisitor ) ;
+	
+			list<XMLMarkup *> found = myTestVisitor.getMatchingMarkups() ;
+			toDisplay.clear() ;
+	
+			for ( list<XMLMarkup *>::iterator it = found.begin() ;
+					it != found.end(); it++ )
+				toDisplay.push_back( encodeToHTML( (*it)->toString() ) ) ;
+
+			LogPlug::info( "Found in test tree following markups matching '"
+				+ searchedMarkup + "' : "
+				+ formatStringList( toDisplay ) ) ;
+
+		}
 		
 		
-		// Storing the result in a XML file :
-		myTestParser.saveToFile() ;
+		if ( testReadingXML )
+		{
+				
+				
+			LogPlug::info( "Testing the parsing of XML strings" ) ;
+							
+			XML::AttributeMap attributeMap ;	
+			
+			// Whitespaces are intended :
+			const string testAttributes = 
+				"hello = \" world\" a b=\"1\" change c" ;
+				
+			XMLParser::ParseAttributeSequence( testAttributes, attributeMap ) ;
+			
+			LogPlug::info( "The parsing of '" + testAttributes 
+				+ "' results in : " 
+				+ Ceylan::formatStringMap( attributeMap, 
+					/* surroundByTicks */ true ) ) ;
+			
+			if ( attributeMap.size() != 5 )
+				throw Ceylan::TestException( "Parsed map has " 
+					+ Ceylan::toString( attributeMap.size() ) 
+					+ " elements, instead of 5." ) ;
+							
+			XMLParser myTestParser( generatedFilename ) ;
+			LogPlug::info( "Parser created : " + myTestParser.toString() ) ;
+	
+			LogPlug::info( "Now, recreating a XML tree "
+				"from the content of '" + generatedFilename + "'." ) ;
 		
+			myTestParser.loadFromFile() ;
+			
+			LogPlug::info( "New parser state : " + myTestParser.toString() ) ;
+			
+			myTestParser.saveToFile( processedFilename ) ;	
+			
+		}
+
+
+		if ( testWritingXML && testReadingXML )
+		{
+		
+			LogPlug::info( "Checking that the two generated files "
+				"are exactly the same." ) ;
+				
+			if ( ! System::File::Diff( generatedFilename, processedFilename ) )
+				throw TestException( "The two generated XML files ('" 
+					+ generatedFilename + "' and '" + processedFilename
+					+ "') are different." ) ;
+		
+		}
 		
 		LogPlug::info( "End of XML parser test." ) ;
 		
