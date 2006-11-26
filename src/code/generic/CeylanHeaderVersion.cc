f059228e4fb7fa7a2d89c3689703a1ce60503615
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-11-26 13:35:41 +0000

    Comment added to avoid mistakes in check-in.

--- src/code/generic/CeylanHeaderVersion.cc
+++ src/code/generic/CeylanHeaderVersion.cc
@@ -1,23 +1,23 @@
 #include "CeylanHeaderVersion.h"
 
 
-#ifdef CEYLAN_RUNS_ON_WINDOWS
-
-#include "CeylanConfigForWindows.h"    // for CEYLAN_LIBTOOL_VERSION
-
 /*
- * This is the CeylanHeaderVersion.cc version for Windows.
+ * The file CeylanHeaderVersion.cc is generated from 
+ * CeylanHeaderVersion.cc.in, so that the embedded library version can be
+ * automatically updated whenever appropriate.
  *
- * On UNIX platforms, this file will be overwritten by the configure-generated
- * CeylanHeaderVersion.cc
+ * @note This file should not be check-ined if updated by the configure script :
+ * if you do not see the value "CEYLAN_LIBTOOL_VERSION" surrounded by '@' 
+ * characters, this file has been already updated by the script, and, as
+ * a generated file, should not be check-ined.
  *
  */
+const std::string Ceylan::actualCeylanHeaderLibtoolVersion =
+	"0.3.0" ;
 
-const std::string Ceylan::actualCeylanHeaderLibtoolVersion
-    = CEYLAN_LIBTOOL_VERSION ;
 
-#else // CEYLAN_RUNS_ON_WINDOWS
-
-#error "This version of CeylanHeaderVersion.cc is for Windows platforms only."
-
-#endif // CEYLAN_RUNS_ON_WINDOWS
+/*
+ * @note Do not ckeck-in this file if there is no '@' in the variable value, 
+ * as in this case it is a generated file !
+ *
+ */
