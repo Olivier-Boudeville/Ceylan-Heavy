b90645241df5bd3e56e0560be56f9b5cedf37acd
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-11-26 17:53:23 +0000

    testCeylan*.xml removed.

--- test/cleanGeneratedConfigFiles.sh
+++ test/cleanGeneratedConfigFiles.sh
@@ -13,6 +13,6 @@ RM="/bin/rm"
 $FIND . \( -name 'autom4te.cache' -o -name '.deps' -o -name '.libs' -o -name 'testCeylan*-logs' \) -exec $RM -rf '{}' ';' 2>/dev/null
 
 # Files :
-$FIND . \( -name ltmain.sh -o -name aclocal.m4 -o -name install-sh -o -name missing -o -name depcomp -o -name stamp-h1 -o -name configure -o -name 'auto*.log' -o -name 'auto*.err' -o -name config.guess -o -name configure.ac -o -name config.log -o -name config.status -o -name config.sub -o -name libtool -o -name Makefile -o -name Makefile.in  -o -name '*.o' -o -name '*.loT' -o -name '*.lo' -o -name 'testCeylan*.log' -o -name testsOutcome.txt \) -exec $RM -f '{}' ';' 2>/dev/null
+$FIND . \( -name ltmain.sh -o -name aclocal.m4 -o -name install-sh -o -name missing -o -name depcomp -o -name stamp-h1 -o -name configure -o -name 'auto*.log' -o -name 'auto*.err' -o -name config.guess -o -name configure.ac -o -name config.log -o -name config.status -o -name config.sub -o -name libtool -o -name Makefile -o -name Makefile.in  -o -name '*.o' -o -name '*.loT' -o -name '*.lo' -o -name 'testCeylan*.log' -o -name 'testCeylan*.xml' -o -name testsOutcome.txt \) -exec $RM -f '{}' ';' 2>/dev/null
 								 
 echo "    Cleaning done."
