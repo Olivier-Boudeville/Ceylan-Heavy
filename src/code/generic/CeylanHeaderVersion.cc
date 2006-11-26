9bbcc3d8ce7c4633e39086d9331ef407424d8bbb
Olivier Boudeville <olivier.boudeville@esperide.com>
2006-11-26 13:46:13 +0000

    This file had been overwritten by mistake.

--- src/code/generic/CeylanHeaderVersion.cc
+++ src/code/generic/CeylanHeaderVersion.cc
@@ -1,23 +1,23 @@
 #include "CeylanHeaderVersion.h"
 
 
-/*
- * The file CeylanHeaderVersion.cc is generated from 
- * CeylanHeaderVersion.cc.in, so that the embedded library version can be
- * automatically updated whenever appropriate.
- *
- * @note This file should not be check-ined if updated by the configure script :
- * if you do not see the value "CEYLAN_LIBTOOL_VERSION" surrounded by '@' 
- * characters, this file has been already updated by the script, and, as
- * a generated file, should not be check-ined.
- *
- */
-const std::string Ceylan::actualCeylanHeaderLibtoolVersion =
-	"0.3.0" ;
+#ifdef CEYLAN_RUNS_ON_WINDOWS
 
+#include "CeylanConfigForWindows.h"    // for CEYLAN_LIBTOOL_VERSION
 
 /*
- * @note Do not ckeck-in this file if there is no '@' in the variable value, 
- * as in this case it is a generated file !
+ * This is the CeylanHeaderVersion.cc version for Windows.
+ *
+ * On UNIX platforms, this file will be overwritten by the configure-generated
+ * CeylanHeaderVersion.cc
  *
  */
+
+const std::string Ceylan::actualCeylanHeaderLibtoolVersion
+    = CEYLAN_LIBTOOL_VERSION ;
+
+#else // CEYLAN_RUNS_ON_WINDOWS
+
+#error "This version of CeylanHeaderVersion.cc is for Windows platforms only."
+
+#endif // CEYLAN_RUNS_ON_WINDOWS
