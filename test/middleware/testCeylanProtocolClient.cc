a58a79d3ac4d28ee271e4689608a38924ab79644
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-03-30 09:11:07 +0000

    --online command line support was lacking.

--- test/middleware/testCeylanProtocolClient.cc
+++ test/middleware/testCeylanProtocolClient.cc
@@ -372,6 +372,11 @@ int main( int argc, char * argv[] )
 				interactiveMode = false ;
 				tokenEaten = true ;
 			} else
+			if ( token == "--online" )
+			{
+				// Ignored :
+				tokenEaten = true ;
+			} else
 			if ( token == "--interactive" )
 			{
 				LogPlug::info( "Running in interactive mode." ) ;
