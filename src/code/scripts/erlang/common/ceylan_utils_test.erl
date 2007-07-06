27081a639a76f2f5e071da6b7d50deac833ef23f
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-07-06 22:02:45 +0000

    Test stops, output with offset.

--- src/code/scripts/erlang/common/ceylan_utils_test.erl
+++ src/code/scripts/erlang/common/ceylan_utils_test.erl
@@ -4,13 +4,14 @@
 
 run() ->
 	io:format( "Testing module ~s.~n", [ ?MODULE ] ),
-	io:format( "Timestamp is ~s.~n", [ ceylan_utils:get_timestamp() ] ),
-	io:format( "An image file would be ~s.~n", 
+	io:format( "   Timestamp is ~s.~n", [ ceylan_utils:get_timestamp() ] ),
+	io:format( "   An image file would be ~s.~n", 
 		[ ceylan_utils:get_image_file_gif( "MyImage" ) ] ),		
-	io:format( "Output with term_toString : ~s, ~s and ~s.~n", 
+	io:format( "   Output with term_toString : ~s, ~s and ~s.~n", 
 		[ 
 			ceylan_utils:term_toString(an_atom), 
 			ceylan_utils:term_toString([1,2]), 
 			ceylan_utils:term_toString("A string")
 		]),
-	io:format( "End of test for module ~s.~n", [ ?MODULE ] ).
+	io:format( "End of test for module ~s.~n", [ ?MODULE ] ),
+	erlang:halt().
