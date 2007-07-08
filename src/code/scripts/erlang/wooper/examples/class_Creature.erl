e474712ea9fcf248eb624df7ed2bd1bc6c47c2d4
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-07-08 22:16:14 +0000

    Integrated according to newer conventions.

--- src/code/scripts/erlang/wooper/examples/class_Creature.erl
+++ src/code/scripts/erlang/wooper/examples/class_Creature.erl
@@ -4,12 +4,13 @@
 % Determines what are the mother classes of this class (if any) :
 -define(superclasses,[]).
 
+-define(wooper_construct_attributes,Age,Gender).
+
 % isHotBlooded/1 and canEat/2 are abstract hence not mentioned here :
 -define(wooper_export,new/2,construct/3,getAge/1,setAge/2,declareBirthday/1,
 	getGender/1,listen/3).
 
 
--define(wooper_construct_attributes,Age,Gender).
 
 % Allows to define WOOPER base variables and methods for that class :
 -include("wooper_class_root.hrl").
@@ -39,21 +40,22 @@ listen(Pid,Action,Arguments) ->
 
 % Returns the age of this creature.
 getAge(State) ->
-	{return,State,getAttribute(State,age)}.
+	?wooper_return_state_result(State,getAttribute(State,age)).
 	
 	
 % Sets the age of this creature.
 setAge(State,NewAge) ->
-	{return,setAttribute(State,age,NewAge)}.
+	?wooper_return_state_only(setAttribute(State,age,NewAge)).
 
 
 % Increments the age of this creature.
 declareBirthday(State) ->
-	{return,setAttribute(State,age,getAttribute(State,age)+1)}.
+	?wooper_return_state_only(
+		setAttribute(State,age,getAttribute(State,age)+1)).
 	
 	
 % Returns the gender of this creature.
 getGender(State) ->
-	{return,State,getAttribute(State,gender)}.
+	?wooper_return_state_result(State,getAttribute(State,gender)).
 	
 
