9b6effeb2f16ece776c7d8b1348a8e5080f78311
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-07-20 22:20:18 +0000

    Updated in quasi-final form ?

--- src/code/scripts/erlang/wooper/examples/class_ViviparousBeing.erl
+++ src/code/scripts/erlang/wooper/examples/class_ViviparousBeing.erl
@@ -1,16 +1,48 @@
 -module(class_ViviparousBeing).
 
 
-% Determines what are the mother classes of this class (if any) :
--define(superclasses,[]).
+% Determines what are the mother classes of this class (if any):
+-define(wooper_superclasses,[]).
 
--define(wooper_export,getMeanChildrenCount/0).
+% Parameters taken by the constructor ('construct'). 
+-define(wooper_construct_attributes,).
 
-% Allows to define WOOPER base variables and methods for that class :
--include("wooper_class_root.hrl").
+% Construction-related exported operators:
+-define(wooper_construct_export,new/0,construct/1).
 
-% Let's say an average means something here :
-getMeanChildrenCount() ->
-	4.
+% Declarations of class-specific methods (besides inherited ones).
+-define(wooper_method_export,getMeanChildrenCount/1,getBirthGivenCount/1,
+	giveBirth/2).
+
+% Allows to define WOOPER base variables and methods for that class:
+-include("wooper.hrl").
+
+
+% Constructs a new Viviparous being (parameter-less constructor).
+construct(State) ->
+	?setAttribute(State,birth_given_count,0).
 	
+	
+	
+	
+% Method implementations.
+
+
+% Let's say an average means something here:
+% (this is a static method, as it does not depend on a state)
+getMeanChildrenCount(State) ->
+	?wooper_return_state_result( State, 4 ).
+	
+	
+% Returns the number of times this viviparous being gave birth:	
+getBirthGivenCount(State) ->
+	?wooper_return_state_result( State, 
+		?getAttribute(State,birth_given_count) ).
+		
+		
+% Increase the number of times this viviparous being gave birth:	
+giveBirth(State,NumberOfNewChildren) ->
+	?wooper_return_state_only( ?setAttribute(State,birth_given_count, 
+		?getAttribute(State,birth_given_count) + NumberOfNewChildren ) ).
+		
 	
