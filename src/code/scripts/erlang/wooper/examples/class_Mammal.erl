86fd608d513002108d7853d17a3760f6d5ad1bc6
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-07-20 22:21:47 +0000

    Updated in quasi-final form ?

--- src/code/scripts/erlang/wooper/examples/class_Mammal.erl
+++ src/code/scripts/erlang/wooper/examples/class_Mammal.erl
@@ -1,20 +1,23 @@
 -module(class_Mammal).
 
 
-% Determines what are the mother classes of this class (if any) :
--define(superclasses,[class_Creature]).
+% Determines what are the mother classes of this class (if any):
+-define(wooper_superclasses,[class_Creature]).
 
 
-% Parameters taken by the constructor. They are here the ones of the mother
-% class (creature) plus fur color :
+% Parameters taken by the constructor ('construct'). 
+% They are here the ones of the mother class (creature) plus fur color:
 -define(wooper_construct_attributes,Age,Gender,FurColor).
 
--define(wooper_helper_export,new/3,construct/4).
+% Construction-related exported operators:
+-define(wooper_construct_export,new/3,construct/4).
 
+% Declarations of class-specific methods (besides inherited ones).
 -define(wooper_method_export,isHotBlooded/1,getFurColor/1).
 
 
-% Allows to define WOOPER base variables and methods for that class :
+
+% Allows to define WOOPER base variables and methods for that class:
 -include("wooper.hrl").
 
 
@@ -25,10 +28,11 @@ construct(State,?wooper_construct_attributes) ->
 	
 	
 	
+	
 % Method implementations.
 
 
-% All mammals are hot-blooded :
+% All mammals are hot-blooded:
 isHotBlooded(State) ->
 	?wooper_return_state_result(State,true).
 
@@ -36,5 +40,6 @@ isHotBlooded(State) ->
 % Attribute names could be defined in '-define().' header (.hrl) clauses,
 % to ensure consistency.
 getFurColor(State) ->
-	?wooper_return_state_result(State, ?getAttribute(State,fur_color)).
+	?wooper_return_state_result( State, ?getAttribute(State,fur_color) ).
+	
 	
