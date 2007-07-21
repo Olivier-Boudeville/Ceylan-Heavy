e2b8a463e99e87636c3c8ec7ca3139ffd6a328f5
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-07-21 22:45:38 +0000

    Boilerplate added.

--- src/code/scripts/erlang/wooper/src/wooper_class_manager.hrl
+++ src/code/scripts/erlang/wooper/src/wooper_class_manager.hrl
@@ -1,3 +1,15 @@
+% Header sharing defines for the WOOPER class manager.
+
+% See documentation at:
+% http://ceylan.sourceforge.net/main/documentation/wooper/
+
+
+% Creation date: Friday, July 12, 2007.
+% Author: Olivier Boudeville (olivier.boudeville@esperide.com).
+
+% Licensed under a disjunctive tri-license: MPL/GPL/LGPL, see:
+% http://ceylan.sourceforge.net/main/documentation/wooper/index.html#license
+
 
 % The class manager name that will be registered :
 -define(WooperClassManagerName,wooper_class_manager).
