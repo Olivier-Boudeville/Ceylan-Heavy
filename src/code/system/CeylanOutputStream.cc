3417f5f3fe8800e36349aba183767073e974862a
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-08-05 15:05:34 +0000

    CEYLAN_USES_CONFIG_H must be tested with #ifdef (not #if).

--- src/code/system/CeylanOutputStream.cc
+++ src/code/system/CeylanOutputStream.cc
@@ -6,7 +6,7 @@
 #include "CeylanEndianness.h"  // for ceylan_bswap_*, etc.
 
 
-#if CEYLAN_USES_CONFIG_H
+#ifdef CEYLAN_USES_CONFIG_H
 #include "CeylanConfig.h"      // for configure-time settings
 #endif // CEYLAN_USES_CONFIG_H
 
