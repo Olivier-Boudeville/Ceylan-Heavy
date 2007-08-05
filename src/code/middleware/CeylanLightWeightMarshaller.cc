b1796232577ef8428074e15c829edfcb74c9eedd
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-08-05 15:18:29 +0000

    CEYLAN_USES_CONFIG_H must be tested with #ifdef (not #if).

--- src/code/middleware/CeylanLightWeightMarshaller.cc
+++ src/code/middleware/CeylanLightWeightMarshaller.cc
@@ -3,7 +3,7 @@
 #include "CeylanLogPlug.h"            // for LogPlug
 #include "CeylanInputOutputStream.h"  // for InputOutputStream
 
-#if CEYLAN_USES_CONFIG_H
+#ifdef CEYLAN_USES_CONFIG_H
 #include "CeylanConfig.h"             // for configure-time feature settings
 #endif // CEYLAN_USES_CONFIG_H
 
