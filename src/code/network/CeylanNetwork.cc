8ce2402d67701458510237a6b17c2880e344a158
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-08-05 15:09:10 +0000

    CEYLAN_USES_CONFIG_H must be tested with #ifdef (not #if).

--- src/code/network/CeylanNetwork.cc
+++ src/code/network/CeylanNetwork.cc
@@ -8,7 +8,7 @@
 #include "CeylanIPAddressvFour.h"       // for IPAddressvFour
 
 
-#if CEYLAN_USES_CONFIG_H
+#ifdef CEYLAN_USES_CONFIG_H
 #include "CeylanConfig.h"               // for configure-time settings
 #endif // CEYLAN_USES_CONFIG_H
 
