dd3a3731f7ba23050d0d3c35c083fa76eeadb6f3
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-08-05 14:58:19 +0000

    CEYLAN_USES_CONFIG_H must be tested with #ifdef (not #if).

--- src/code/interfaces/CeylanLocatable.cc
+++ src/code/interfaces/CeylanLocatable.cc
@@ -7,7 +7,7 @@
 #include "CeylanTypes.h"        // for ListSize
 
 
-#if CEYLAN_USES_CONFIG_H
+#ifdef CEYLAN_USES_CONFIG_H
 #include "CeylanConfig.h"       // for configure-time settings
 #endif // CEYLAN_USES_CONFIG_H
 
