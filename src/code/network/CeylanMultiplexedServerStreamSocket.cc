98c39e5efc1658f17969a79c7044146fa91256d4
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-08-05 15:07:40 +0000

    CEYLAN_USES_CONFIG_H must be tested with #ifdef (not #if).

--- src/code/network/CeylanMultiplexedServerStreamSocket.cc
+++ src/code/network/CeylanMultiplexedServerStreamSocket.cc
@@ -10,7 +10,7 @@
 #include "CeylanInputStream.h"            // for Select
 
 
-#if CEYLAN_USES_CONFIG_H
+#ifdef CEYLAN_USES_CONFIG_H
 #include "CeylanConfig.h"      // for configure-time feature settings
 #endif // CEYLAN_USES_CONFIG_H
 
