034e3e040135b61069fdad20db44052f1e878445
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-08-05 15:04:02 +0000

    CEYLAN_USES_CONFIG_H must be tested with #ifdef (not #if).

--- src/code/system/CeylanEnvironmentVariables.cc
+++ src/code/system/CeylanEnvironmentVariables.cc
@@ -2,7 +2,7 @@
 
 #include "CeylanSystem.h"      // for getError, explainError
 
-#if CEYLAN_USES_CONFIG_H
+#ifdef CEYLAN_USES_CONFIG_H
 #include "CeylanConfig.h"      // for configure-time settings
 #endif // CEYLAN_USES_CONFIG_H
 
