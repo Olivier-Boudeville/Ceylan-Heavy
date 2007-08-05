bf369d3e40563c7460e1415cdfb1babc9bda757e
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-08-05 15:02:01 +0000

    CEYLAN_USES_STRING_H and CEYLAN_USES_CONFIG_H must be tested with #ifdef (not #if).

--- src/code/system/CeylanMemoryStream.cc
+++ src/code/system/CeylanMemoryStream.cc
@@ -5,7 +5,7 @@
 #include "CeylanStringUtils.h" // for StringSize
 
 
-#if CEYLAN_USES_CONFIG_H
+#ifdef CEYLAN_USES_CONFIG_H
 #include "CeylanConfig.h"      // for configure-time settings
 #endif // CEYLAN_USES_CONFIG_H
 
@@ -13,7 +13,7 @@
 extern "C"
 {
 
-#if CEYLAN_USES_STRING_H
+#ifdef CEYLAN_USES_STRING_H
 #include <string.h>            // for memcpy
 #endif // CEYLAN_USES_STRING_H
 
