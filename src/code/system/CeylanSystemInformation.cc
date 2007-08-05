d3317f232a2184974a59f5f03b677a6e0c0961b1
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-08-05 15:03:40 +0000

    CEYLAN_USES_CONFIG_H must be tested with #ifdef (not #if).

--- src/code/system/CeylanSystemInformation.cc
+++ src/code/system/CeylanSystemInformation.cc
@@ -15,7 +15,7 @@ using namespace Ceylan::Log ;           // for LogPlug
 using namespace Ceylan::System ;        // for SystemException
 
 
-#if CEYLAN_USES_CONFIG_H
+#ifdef CEYLAN_USES_CONFIG_H
 #include "CeylanConfig.h"               // for configure-time settings
 #endif // CEYLAN_USES_CONFIG_H
 
