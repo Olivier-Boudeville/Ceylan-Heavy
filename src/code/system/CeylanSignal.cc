cd988a2ca5dff7c74030155f6f6c90e10d0d2776
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-08-05 15:00:59 +0000

    CEYLAN_USES_CONFIG_H must be tested with #ifdef (not #if).

--- src/code/system/CeylanSignal.cc
+++ src/code/system/CeylanSignal.cc
@@ -1,7 +1,7 @@
 #include "CeylanSignal.h"
 
 
-#if CEYLAN_USES_CONFIG_H
+#ifdef CEYLAN_USES_CONFIG_H
 #include "CeylanConfig.h"      // for configure-time feature settings
 #endif // CEYLAN_USES_CONFIG_H
 
