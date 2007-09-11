f511c809fa3a202fc47191c328c0fc4593adcc7b
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-09-11 13:51:14 +0000

    The newer File/Directory/FileSystemManager API has been integrated.

--- src/code/logs/CeylanLogAggregatorHTML.cc
+++ src/code/logs/CeylanLogAggregatorHTML.cc
@@ -3,7 +3,8 @@
 
 #include "CeylanOperators.h"
 #include "CeylanFile.h"          // for File
-#include "CeylanDirectory.h"
+#include "CeylanHolder.h"        // for Holder
+#include "CeylanDirectory.h"     // for Directory
 #include "CeylanTimestamp.h"
 #include "CeylanStringUtils.h"   // for substituteInString
 
@@ -18,7 +19,7 @@
 #endif // CEYLAN_USES_CONFIG_H
 
 
-// for cout, endl or for cerr, when log system fails badly :
+// for cout, endl or for cerr, when log system fails badly:
 #include <iostream>            
 
 
@@ -38,7 +39,6 @@ const LevelOfDetail LogAggregatorHTML::DefaultGlobalLevelOfDetail
 const string LogAggregatorHTML::HTMLPageSuffix = ".html" ;
 
 
-
 LogAggregatorHTML::LogAggregatorHTML( 
 		const string & callerDescription,
 		const string & logDirectoryName,
@@ -51,6 +51,7 @@ LogAggregatorHTML::LogAggregatorHTML(
 		_outputDirectory( 0 )
 {
 
+		
 	try 
 	{
 	
@@ -60,17 +61,17 @@ LogAggregatorHTML::LogAggregatorHTML(
 		 *
 		 */
 		  
-		CEYLAN_LOG( "LogAggregatorHTML constructor : creating directory "
-			+ _logDirectoryName ) ;
+		CEYLAN_LOG( "LogAggregatorHTML constructor: creating directory '"
+			+ _logDirectoryName + "'" ) ;
 			
-		_outputDirectory = new System::Directory( _logDirectoryName ) ;
-		
+		_outputDirectory = & System::Directory::Create( _logDirectoryName ) ;
+				
 	} 
-	catch( const Directory::DirectoryException & e )
+	catch( const System::DirectoryException & e )
 	{
-		throw LogAggregatorException( 
-			"Could not access LogAggregatorHTML output directory, "
-			+ _logDirectoryName + " : " + e.toString() ) ;
+		throw LogAggregatorException( "LogAggregatorHTML constructor: "
+			"could not create LogAggregatorHTML output directory, "
+			+ _logDirectoryName + ": " + e.toString() ) ;
 	}	
 
 }
@@ -95,7 +96,7 @@ LogAggregatorHTML::~LogAggregatorHTML() throw()
 		{
 		
 			std::cerr << "Error while aggregating logs "
-				"in LogAggregatorHTML destructor : "
+				"in LogAggregatorHTML destructor: "
 				<< e.toString() << std::endl ;
 			// Never throw an exception from a destructor !		
 		}
@@ -112,94 +113,117 @@ void LogAggregatorHTML::aggregate() throw( LogAggregatorException )
 {
 
 	CEYLAN_LOG( "LogAggregatorHTML aggregation started" ) ; 
+
 	
 #if CEYLAN_DEBUG
 	if ( _outputDirectory == 0 )
-		throw LogAggregatorException( "LogAggregatorHTML::aggregate : "
+		throw LogAggregatorException( "LogAggregatorHTML::aggregate: "
 			"null output directory pointer." ) ;
 #endif // CEYLAN_DEBUG
-	
-	Timestamp stampBegin ;
-	
+
+	try
 	{
+		
+		Timestamp stampBegin ;
 	
-		// First, the frameset :
-		File framesetPage( _outputDirectory->getPath() 
-			+ Directory::Separator + "index" + HTMLPageSuffix ) ;
+	
+		// First, the frameset:
+		File & framesetPage = File::Create( 
+			Directory::JoinPath( _outputDirectory->getPath(), 
+				"index" + HTMLPageSuffix ) ) ;
 	
 		string newFrameset = FrameSet ;
 		
 		Ceylan::substituteInString( newFrameset, "ST_CALLER_DESCRIPTION", 
 			Ceylan::encodeToHTML( _callerDescription ) ) ; 
+			
 		framesetPage.write( newFrameset ) ;
+		
+		delete & framesetPage ;
 	
-	}
 	
 	
-	// Second, let's begin the default page :
-	File defaultPage( _outputDirectory->getPath() 
-		+ Directory::Separator + "MainLog" + HTMLPageSuffix ) ;
+		/*
+		 * Second, let's begin the default page (use holders to manage files
+		 * life-cycle):
+		 *
+		 */
+		Holder<File> defaultPageFileHolder( File::Create(
+			Directory::JoinPath( _outputDirectory->getPath(),
+				"MainLog" + HTMLPageSuffix ) ) ) ;
 		
 	
-	defaultPage.write( DefaultPageHeader ) ;
+		defaultPageFileHolder->write( DefaultPageHeader ) ;
 	
-	defaultPage.write( "Log plug session initiated for source " 
-		+ LogPlug::GetSourceName() ) ;
+		defaultPageFileHolder->write( "Log plug session initiated for source " 
+			+ LogPlug::GetSourceName() ) ;
 		
-	defaultPage.write( "<p>" + stampBegin.toString() 
-		+ " Aggregation of Log messages started.</p>\n" ) ;
+		defaultPageFileHolder->write( "<p>" + stampBegin.toString() 
+			+ " Aggregation of Log messages started.</p>\n" ) ;
 
 	
-	/*
-	 * Third, builds in parallel the browser menu and the pages it 
-	 * references :
-	 *
-	 */
-	File logBrowserMenu( _outputDirectory->getPath() 
-		+ Directory::Separator + "LogSystem" + HTMLPageSuffix ) ;
+		/*
+		 * Third, builds in parallel the browser menu and the pages it 
+		 * references:
+		 *
+		 */
+		Holder<File> logBrowserMenuFileHolder( File::Create(
+			Directory::JoinPath( _outputDirectory->getPath(),
+				"LogSystem" + HTMLPageSuffix ) ) ) ;
 	
-	logBrowserMenu.write( MenuHeader ) ;
+		logBrowserMenuFileHolder->write( MenuHeader ) ;
 		
 	
-	for ( list<LogChannel *>::const_iterator it = _channelList.begin() ;
-		it != _channelList.end() ; it++ )
+		for ( list<LogChannel *>::const_iterator it = _channelList.begin() ;
+			it != _channelList.end() ; it++ )
+		{
+	
+			CEYLAN_LOG( "Creating page for channel " + (*it)->getName()
+				+ " which has " + Ceylan::toString( (*it)->getMessageCount() )
+				+ " messsage(s)." ) ;
+			
+			// Add a menu reference, each channel with its message count:
+			logBrowserMenuFileHolder->write( 
+				"<tr><td>[<a href=\"" 
+				+ File::TransformIntoValidFilename( (*it)->getName() )
+				+ ".html\" target=\"mainFrame\">"
+				+ Ceylan::toString( (*it)->getMessageCount() )
+				+ "</a>]</td><td><a href=\"" 
+				+ File::TransformIntoValidFilename( (*it)->getName() )
+				+ ".html\" target=\"mainFrame\">" 
+				+ Ceylan::encodeToHTML( (*it)->getName() ) + "</a></td></tr>\n" 
+			) ;
+			
+			// Builds the corresponding channel page: 	
+			write( * (*it) ) ;
+		
+		}
+	
+	
+		logBrowserMenuFileHolder->write( MenuFooter ) ;
+
+
+		// Let's finish the default page.
+		Timestamp stampEnd ;
+		defaultPageFileHolder->write( "<p>" + stampEnd.toString() 
+			+ " Aggregation of Log messages ended.</p>\n" ) ;
+		defaultPageFileHolder->write( DefaultPageFooter ) ;
+	
+		std::cout << "Logs can be inspected from file://"
+			<< _outputDirectory->getPath() << "/index.html" << std::endl ;
+		
+		// Automatic files will be automatically closed thanks to holders.
+		
+	} 
+	catch( const SystemException & e )
 	{
-	
-		CEYLAN_LOG( "Creating page for channel " + (*it)->getName()
-			+ " which has " + Ceylan::toString( (*it)->getMessageCount() )
-			+ " messsage(s)." ) ;
-			
-		// Add a menu reference, each channel with its message count :
-		logBrowserMenu.write( 
-			"<tr><td>[<a href=\"" 
-			+ File::TransformIntoValidFilename( (*it)->getName() )
-			+ ".html\" target=\"mainFrame\">"
-			+ Ceylan::toString( (*it)->getMessageCount() )
-			+ "</a>]</td><td><a href=\"" 
-			+ File::TransformIntoValidFilename( (*it)->getName() )
-			+ ".html\" target=\"mainFrame\">" 
-			+ Ceylan::encodeToHTML( (*it)->getName() ) + "</a></td></tr>\n" 
-		) ;
-			
-		// Builds the corresponding channel page : 	
-		write( * (*it) ) ;
 		
-	}
-	
-	
-	logBrowserMenu.write( MenuFooter ) ;
-
-
-	// Let's finish the default page.
-	Timestamp stampEnd ;
-	defaultPage.write( "<p>" + stampEnd.toString() 
-		+ " Aggregation of Log messages ended.</p>\n" ) ;
-	defaultPage.write( DefaultPageFooter ) ;
-	
-	std::cout << "Logs can be inspected from file://"
-		<< _outputDirectory->getPath() << "/index.html" << std::endl ;
+		// This exception covers all file and directories operations:
 		
-	// Automatic files will be automatically closed.
+		throw LogAggregatorException( "LogAggregatorHTML::aggregate: "
+			"file-related operation failed: " + e.toString() ) ;
+		
+	}			
 	
 }
 
@@ -209,7 +233,7 @@ void LogAggregatorHTML::store( LogMessage & message ) throw( LogException )
 
 	CEYLAN_LOG( "Storing a new message " + message.toString() ) ;
 		
-	// Use standard LogAggregator method in all cases (no immediate write) :
+	// Use standard LogAggregator method in all cases (no immediate write):
 	LogAggregator::store( message ) ;
 
 }
@@ -224,95 +248,134 @@ const string LogAggregatorHTML::toString( Ceylan::VerbosityLevels level )
 }	
 
 
+
+
+
+// Write section.
+
+
 void LogAggregatorHTML::write( const LogChannel & channel )
-	const throw()
+	const throw( LogException )
 {
 
 	CEYLAN_LOG( "Writing on disk channel " + channel.toString() ) ;
-	
-	
-	File logChannelPage( 
-		_outputDirectory->getPath() 
-		+ Directory::Separator 
-		+ File::TransformIntoValidFilename( channel.getName() )
-		+ HTMLPageSuffix ) ;
+		
+
+	Holder<File> logChannelPageHolderFile( File::Create(
+		Directory::JoinPath( _outputDirectory->getPath(),
+			File::TransformIntoValidFilename( channel.getName() )
+				+ HTMLPageSuffix ) ) ) ;
 
 	LevelOfDetail sourceLevelOfDetail ;
 	
-	WriteChannelHeader( channel, logChannelPage ) ;
+	WriteChannelHeader( channel, logChannelPageHolderFile.get() ) ;
 		
 	// Level of detail globally overriden ?
 	if ( _useGlobalLevelOfDetail )			
-	{
 		sourceLevelOfDetail = _globalLevelOfDetail ;
-	} 
 	else
-	{
 		sourceLevelOfDetail = MaximumLevelOfDetailForMessage ;
-	}
-		
+			
 	for ( list<LogMessage *>::const_iterator it = 
-			channel._messages.begin() ; 
-		it != channel._messages.end() ; it++ )
+		channel._messages.begin(); it != channel._messages.end() ; it++ )
 	{
 	
 #if CEYLAN_DEBUG
-		if ( ! (*it) )
+		if ( (*it) == 0 )
 		{
-			CEYLAN_LOG( "Error, LogChannel::toString : "
+			CEYLAN_LOG( "Error, LogAggregatorHTML::write: "
 				"null pointer in message list, skipping." ) ;
 			break ;
 		}	
 #endif // CEYLAN_DEBUG
 
-		write( * (*it), logChannelPage ) ;
+		write( * (*it), logChannelPageHolderFile.get() ) ;
 		
 	}	
 	
-	WriteChannelFooter( channel, logChannelPage ) ;
-
+	WriteChannelFooter( channel, logChannelPageHolderFile.get() ) ;
+			
 
 }
 
 
+void LogAggregatorHTML::write( const LogMessage & message, 
+	Ceylan::System::File & targetFile ) const throw( LogException )
+{
+
+	CEYLAN_LOG( "Writing on disk message " + message.toString() ) ;
+	
+	try
+	{
+	
+		/*
+		 * HTML logs are clear and separated, no level-of-detail filtering:
+		 * everything is written.
+		 *
+		 */	
+		targetFile.write( "<li>" 
+			+ /* Ceylan::encodeToHTML( */ message.getPreformattedText() /* ) */
+			+ "</li>\n" ) ;
+			
+	}
+	catch( const OutputStream::WriteFailedException & e )
+	{
+
+		throw LogException( "LogAggregatorHTML::write failed: "
+			+ e.toString() ) ;
+	
+	}
+	
+}
+
+
 void LogAggregatorHTML::WriteChannelHeader( const LogChannel & channel,
-	Ceylan::System::File & targetFile ) throw()
+	Ceylan::System::File & targetFile ) throw( LogException )
 {
 
 	string newHeader = ChannelHeader ;
+	
 	Ceylan::substituteInString( newHeader, "ST_CHANNEL_NAME", 
 		Ceylan::encodeToHTML( channel.getName() ) ) ; 
 	
-	targetFile.write( newHeader ) ;
+	try
+	{
+	
+		targetFile.write( newHeader ) ;
+		
+	}
+	catch( const OutputStream::WriteFailedException	& e )
+	{
+		throw LogException( "LogAggregatorHTML::WriteChannelHeader failed: "
+			+ e.toString() ) ;
+	}
+	
 	
 }
 
 
 void LogAggregatorHTML::WriteChannelFooter( const LogChannel & channel,
-	Ceylan::System::File & targetFile ) throw()
+	Ceylan::System::File & targetFile ) throw( LogException )
 {
+
 	string newFooter = ChannelFooter ;
+	
 	Ceylan::substituteInString( newFooter, "ST_AGGREGATION_DATE", 
 		Ceylan::encodeToHTML( timeToString( getTime() ) ) ) ; 
 	
-	targetFile.write( newFooter ) ;
+	try
+	{
+	
+		targetFile.write( newFooter ) ;
+		
+	}
+	catch( const OutputStream::WriteFailedException	& e )
+	{
+		throw LogException( "LogAggregatorHTML::WriteChannelFooter failed: "
+			+ e.toString() ) ;
+	}
+	
+		
 }
 
-
-void LogAggregatorHTML::write( const LogMessage & message, 
-	Ceylan::System::File & targetFile ) const throw()
-{
-
-	CEYLAN_LOG( "Writing on disk message " + message.toString() ) ;
-	
-	/*
-	 * HTML logs are clear and separated, no level-of-detail filtering :
-	 * everything is written.
-	 *
-	 */	
-	targetFile.write( "<li>" 
-		+ /* Ceylan::encodeToHTML( */ message.getPreformattedText() /* ) */
-		+ "</li>\n" ) ;
-	
-}
 
