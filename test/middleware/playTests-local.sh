f3fc2984cc814d6c594c53612d2c75281145cd0d
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-09-11 13:34:36 +0000

    Call to pending server checking added. Slight reformatting.

--- test/middleware/playTests-local.sh
+++ test/middleware/playTests-local.sh
@@ -3,7 +3,7 @@
 # This script is a necessary special case since the server must be run
 # before the client is launched.
 
-# One can ensure with : 
+# One can ensure with: 
 # ps -edf|grep testCeylan
 #   - and - 
 # netstat --tcp --listening |grep 6969
@@ -13,8 +13,11 @@
 
 DEBUG_INTERNAL "middleware/playTests-local.sh sourced"
 
+# We chose to keep here the classical log plug, so that log writing remains
+# immediate and not deferred as with the HTML plug.
 
-# The classical network client 'testCeylanClientStream' will do the job :
+
+# The classical network client 'testCeylanClientStream' will do the job:
 
 marshalled_server_test_name="testCeylanMultiLwMrshSrv"
 marshalled_client_test_name="testCeylanClientStream"
@@ -39,16 +42,17 @@ fi
 
 
 # Have to be excluded from automatic selection since must be managed
-# specifically here :
+# specifically here:
 EXCLUDED_TESTS="${marshalled_server_test_name} ${multiplexed_lw_protocol_server_test_name} ${multiplexed_lw_protocol_client_test_name}"
 
 
 
 
-### Testing marshalled server :
+### Testing marshalled server:
 
+check_no_ceylan_server_running
 
-# First, launch the server :
+# First, launch the server:
 
 DEBUG_INTERNAL "Will launch the server"
 t=${marshalled_server_test_exec}
@@ -64,17 +68,17 @@ else
 fi			
 
 
-# Then do as if client test was a classical test :
+# Then do as if client test was a classical test:
 display_launching ${marshalled_client_test_name}
 
-# Ensure that the server is ready before the client :
+# Ensure that the server is ready before the client:
 sleep 1
 
 DEBUG_INTERNAL "Will launch the client"
 run_test ${marshalled_client_test_name} ${marshalled_client_test_exec}
 
 DEBUG_INTERNAL "Wait for the server"
-# Now inspect the server result :
+# Now inspect the server result:
 wait ${SERVER_PID} 
 return_code="$?"
 DEBUG_INTERNAL "Server return code is $return_code"
@@ -82,17 +86,18 @@ DEBUG_INTERNAL "Server return code is $return_code"
 display_launching ${marshalled_server_test_name}
 display_test_result "${marshalled_server_test_name}" "${marshalled_server_test_exec}" "$return_code"
 
-# Client already tested :
+# Client already tested:
 test_count=`expr $test_count + 1`
 
 
 
 
 
-### Testing protocol server :
+### Testing protocol server:
 
+check_no_ceylan_server_running
 
-# First, launch the server :
+# First, launch the server:
 
 DEBUG_INTERNAL "Will launch the server"
 t=${multiplexed_lw_protocol_server_test_exec}
@@ -108,17 +113,17 @@ else
 fi			
 
 
-# Then do as if client test was a classical test :
+# Then do as if client test was a classical test:
 display_launching ${multiplexed_lw_protocol_client_test_name}
 
-# Ensure that the server is ready before the client :
+# Ensure that the server is ready before the client:
 sleep 1
 
 DEBUG_INTERNAL "Will launch the client"
 run_test ${multiplexed_lw_protocol_client_test_name} ${multiplexed_lw_protocol_client_test_exec}
 
 DEBUG_INTERNAL "Wait for the server"
-# Now inspect the server result :
+# Now inspect the server result:
 wait ${SERVER_PID} 
 return_code="$?"
 DEBUG_INTERNAL "Server return code is $return_code"
@@ -126,6 +131,6 @@ DEBUG_INTERNAL "Server return code is $return_code"
 display_launching ${multiplexed_lw_protocol_server_test_name}
 display_test_result "${multiplexed_lw_protocol_server_test_name}" "${multiplexed_server_test_exec}" "$return_code"
 
-# test client not already counted :
+# test client not already counted:
 test_count=`expr $test_count + 2`
 
