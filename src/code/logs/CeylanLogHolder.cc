82cf3d39be208f0bbe8462fe4b3953773288e4f7
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-09-11 13:52:33 +0000

    Slight reformatting.

--- src/code/logs/CeylanLogHolder.cc
+++ src/code/logs/CeylanLogHolder.cc
@@ -1,6 +1,6 @@
 #include "CeylanLogHolder.h"
 
-// All plugs needed, to be able to pick one of them :
+// All plugs needed, to be able to pick one of them:
 #include "CeylanLogPlugConsole.h"   // for console plug
 #include "CeylanLogPlugClassical.h" // for classical plug
 #include "CeylanLogPlugHTML.h"      // for HTML plug
@@ -30,7 +30,7 @@ KnownPlugs LogHolder::DefaultPlug = classicalPlug ;
 
 
 LogHolder::LogHolder( Ceylan::Uint16 argCount, 
-		const char * const arguments[] ) throw( LogException ) :
+		const char * const arguments[] ) throw( LogException ):
 	_chosenPlug( DefaultPlug )
 {
 
@@ -74,12 +74,12 @@ LogHolder::LogHolder( Ceylan::Uint16 argCount,
 	 * 'Ceylan/Ceylan-0.2/bin/interfaces', and if we execute it from
 	 * 'Ceylan/Ceylan-0.2/tests-outputs'  (hence with
 	 * '../bin/interfaces/testLockable'), with speakerName set to 
-	 * 'arguments[0]' the log files 
-	 * would be created under 'Ceylan/Ceylan-0.2/bin/interfaces'.
+	 * 'arguments[0]' the log files would be created under
+	 * 'Ceylan/Ceylan-0.2/bin/interfaces'.
 	 *
 	 * The preferred behaviour would be to write them under current
 	 * directory (hence, 'tests-outputs'). 
-	 * In this case StripFilename should be used :
+	 * In this case StripFilename should be used:
 	 *
 	 */
 	string speakerName ;
@@ -87,13 +87,12 @@ LogHolder::LogHolder( Ceylan::Uint16 argCount,
 	Ceylan::System::Directory::StripFilename( arguments[0], 
 		/* base path */ 0, & speakerName ) ;
 		
-	CEYLAN_LOG( "LogHolder : speaker name for logs will be " 
-		+ speakerName ) ;
+	CEYLAN_LOG( "LogHolder: speaker name for logs will be " + speakerName ) ;
 	
 	
 	/*
 	 * LogHolder usually out of a try/catch pair, avoid propagating 
-	 * exception :
+	 * exception:
 	 *
 	 */
 	  
@@ -103,23 +102,23 @@ LogHolder::LogHolder( Ceylan::Uint16 argCount,
 		switch( _chosenPlug )
 		{
 	
-			case consolePlug :
-				CEYLAN_LOG( "LogHolder : using console plug." ) ;
+			case consolePlug:
+				CEYLAN_LOG( "LogHolder: using console plug." ) ;
 				LogPlugConsole::StartService( speakerName ) ;
 				break ;
 	
-			case classicalPlug :
-				CEYLAN_LOG( "LogHolder : using classical plug." ) ;
+			case classicalPlug:
+				CEYLAN_LOG( "LogHolder: using classical plug." ) ;
 				LogPlugClassical::StartService( speakerName ) ;
 				break ;
 	
-			case HTMLPlug :
-				CEYLAN_LOG( "LogHolder : using HTML plug." ) ;
+			case HTMLPlug:
+				CEYLAN_LOG( "LogHolder: using HTML plug." ) ;
 				LogPlugHTML::StartService( speakerName ) ;
 				break ;
 			
 			default:	
-				CEYLAN_LOG( "Warning : LogHolder : "
+				CEYLAN_LOG( "Warning: LogHolder: "
 					"no known plug specified, defaulting to classical." ) ;
 				LogPlugClassical::StartService( speakerName ) ;
 				break ;
@@ -129,7 +128,7 @@ LogHolder::LogHolder( Ceylan::Uint16 argCount,
 	}
 	catch( const Ceylan::Exception & e )
 	{
-		Ceylan::emergencyShutdown( "LogHolder constructor failed : "
+		Ceylan::emergencyShutdown( "LogHolder constructor failed: "
 			+ e.toString() ) ;
 	}
 
@@ -142,23 +141,23 @@ LogHolder::~LogHolder() throw()
 	switch( _chosenPlug )
 	{
 	
-		case consolePlug :
-			CEYLAN_LOG( "LogHolder : stopping console plug." ) ;
+		case consolePlug:
+			CEYLAN_LOG( "LogHolder: stopping console plug." ) ;
 			LogPlugConsole::StopService() ;
 			break ;
 	
-		case classicalPlug :
-			CEYLAN_LOG( "LogHolder : stopping classical plug." ) ;
+		case classicalPlug:
+			CEYLAN_LOG( "LogHolder: stopping classical plug." ) ;
 			LogPlugClassical::StopService() ;
 			break ;
 	
-		case HTMLPlug :
-			CEYLAN_LOG( "LogHolder : stopping HTML plug." ) ;
+		case HTMLPlug:
+			CEYLAN_LOG( "LogHolder: stopping HTML plug." ) ;
 			LogPlugHTML::StopService() ;
 			break ;
 			
 		default:	
-			std::cerr << "LogHolder destructor : "
+			std::cerr << "LogHolder destructor: "
 				"no valid plug available ! Aborting." << std::endl ;
 			::exit( Ceylan::ExitFailure ) ;
 			break ;
@@ -175,15 +174,15 @@ const string LogHolder::toString( Ceylan::VerbosityLevels level )
 	switch( _chosenPlug )
 	{
 	
-		case consolePlug :
+		case consolePlug:
 			return "LogHolder uses console plug" ;
 			// break ;
 			
-		case classicalPlug :
+		case classicalPlug:
 			return "LogHolder uses classical plug" ;
 			// break ;
 	
-		case HTMLPlug :
+		case HTMLPlug:
 			return "LogHolder uses HTML plug" ;
 			// break ;
 			
