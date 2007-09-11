d088ce3cc9ae5f3df8f24d5dd9ce8c9335566218
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-09-11 13:32:23 +0000

    Call to pending server checking added. Slight reformatting.

--- test/network/playTests-local.sh
+++ test/network/playTests-local.sh
@@ -3,7 +3,7 @@
 # This script is a necessary special case since the server must be run
 # before the client is launched.
 
-# One can ensure with : 
+# One can ensure with: 
 # ps -edf|grep testCeylanServer
 #   - and - 
 # netstat --tcp --listening |grep 6969
@@ -13,6 +13,8 @@
 
 DEBUG_INTERNAL "playTests-local.sh sourced"
 
+# We chose to keep here the classical log plug, so that log writing remains
+# immediate and not deferred as with the HTML plug.
 
 
 
@@ -36,16 +38,17 @@ fi
 
 
 # Have to be excluded from automatic selection since must be managed
-# specifically here :
+# specifically here:
 EXCLUDED_TESTS="${sequential_server_test_name} ${multiplexed_server_test_name} ${client_test_name}"
 
 
 
 
-### Testing sequential server :
+### Testing sequential server:
 
+check_no_ceylan_server_running
 
-# First, launch the server :
+# First, launch the server:
 
 DEBUG_INTERNAL "Will launch the server"
 t=${sequential_server_test_exec}
@@ -61,17 +64,17 @@ else
 fi			
 
 
-# Then do as if client test was a classical test :
+# Then do as if client test was a classical test:
 display_launching ${client_test_name}
 
-# Ensure that the server is ready before the client :
+# Ensure that the server is ready before the client:
 sleep 1
 
 DEBUG_INTERNAL "Will launch the client"
 run_test ${client_test_name} ${client_test_exec}
 
 DEBUG_INTERNAL "Wait for the server"
-# Now inspect the server result :
+# Now inspect the server result:
 wait ${SERVER_PID} 
 return_code="$?"
 DEBUG_INTERNAL "Server return code is $return_code"
@@ -85,10 +88,11 @@ test_count=`expr $test_count + 2`
 
 
 
-### Testing multiplexed server :
+### Testing multiplexed server:
 
+check_no_ceylan_server_running
 
-# First, launch the server :
+# First, launch the server:
 
 DEBUG_INTERNAL "Will launch the server"
 t=${multiplexed_server_test_exec}
@@ -104,17 +108,17 @@ else
 fi			
 
 
-# Then do as if client test was a classical test :
+# Then do as if client test was a classical test:
 display_launching ${client_test_name}
 
-# Ensure that the server is ready before the client :
+# Ensure that the server is ready before the client:
 sleep 1
 
 DEBUG_INTERNAL "Will launch the client"
 run_test ${client_test_name} ${client_test_exec}
 
 DEBUG_INTERNAL "Wait for the server"
-# Now inspect the server result :
+# Now inspect the server result:
 wait ${SERVER_PID} 
 return_code="$?"
 DEBUG_INTERNAL "Server return code is $return_code"
@@ -122,6 +126,6 @@ DEBUG_INTERNAL "Server return code is $return_code"
 display_launching ${multiplexed_server_test_name}
 display_test_result "${multiplexed_server_test_name}" "${multiplexed_server_test_exec}" "$return_code"
 
-# test client already counted :
+# test client already counted:
 test_count=`expr $test_count + 1`
 
