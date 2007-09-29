0f01c24de55b2b748e8f55272a8c618b02b2436b
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-09-29 08:44:15 +0000

    For the DS, uses a display console to check logs.

--- src/code/logs/CeylanLogAggregatorConsole.cc
+++ src/code/logs/CeylanLogAggregatorConsole.cc
@@ -1,14 +1,15 @@
 #include "CeylanLogAggregatorConsole.h"
 
 
-#include "CeylanOperators.h"
-#include "CeylanTimestamp.h"   // for TimeStamp
-
 #include "CeylanLogChannel.h"  // for LogChannel
 #include "CeylanLogMessage.h"  // for LogMessage
 #include "CeylanLogLight.h"    // for CEYLAN_LOG
 #include "CeylanLog.h"         // for MaximumLevelOfDetailForMessage
 
+#include "CeylanOperators.h"
+#include "CeylanTimestamp.h"   // for TimeStamp
+#include "CeylanStringUtils.h" // for display
+#include "CeylanConsole.h"     // for Console
 
 
 #include <iostream>            // for cout, endl, cerr, etc.
@@ -22,6 +23,24 @@ using namespace Ceylan::System ;
 
 
 
+#ifdef CEYLAN_USES_CONFIG_H
+#include "CeylanConfig.h"      // for configure-time feature settings
+#endif // CEYLAN_USES_CONFIG_H
+
+
+/*
+ * In the case of the Nintendo DS, a convenient way of debugging is to use
+ * one of the two screens as a debug console. 
+ * To allow to look at all logs, even if they are produced quickly, a console
+ * (CeylanConsole) is used. It is created as soon as the LogAggregatorConsole
+ * is created, and, when this instance is deleted, it gives the opportunity to
+ * watch the stored logs.
+ *
+ * Hence a program using a LogHolder, whether it stops on error or not, should
+ * be able in most cases to let the user consult the logs before stopping.
+ *
+ */
+
 LogAggregatorConsole::LogAggregatorConsole( 
 		StandardStream consoleStream,
 		bool immediateWrite,
@@ -31,9 +50,20 @@ LogAggregatorConsole::LogAggregatorConsole(
 		LogAggregator( beSmart, useGlobalLevelOfDetail ),
 		_streamNumber( consoleStream ),
 		_outputStream( 0 ),
+		_console( 0 ),
 		_immediateWrite( immediateWrite )
 {
 
+#if CEYLAN_ARCH_NINTENDO_DS
+
+	// Use a default console on bottom screen:
+	_console = new System::Console() ;
+	
+	if ( _immediateWrite )
+		_console->setToForeground( true ) ;
+				
+#else // CEYLAN_ARCH_NINTENDO_DS
+		
 	switch( consoleStream )
 	{
 	
@@ -57,6 +87,7 @@ LogAggregatorConsole::LogAggregatorConsole(
 			
 	}
 	
+#endif // CEYLAN_ARCH_NINTENDO_DS
 	
 }
 
@@ -87,7 +118,32 @@ LogAggregatorConsole::~LogAggregatorConsole() throw()
 		}	
 		
 	}
+	
+	
+	if ( _console != 0 )
+	{
+	
+		try
+		{
 		
+			// Allows to read the logs:
+			_console->goInteractive() ;
+			
+			_console->addInBuffer( "Log system stopped." ) ;
+			
+		}
+		catch( const System::Console::ConsoleException & e )
+		{
+		
+			display( "LogAggregatorConsole shutdown failed: " + e.toString() ) ;
+			
+		}
+		
+		// When finished with it:
+		delete _console ;
+		
+	}	
+	
 }
 
 
@@ -103,6 +159,8 @@ void LogAggregatorConsole::aggregate() throw( LogAggregatorException )
 	}
 
 	CEYLAN_LOG( "LogAggregatorConsole aggregation started" ) ; 
+
+#if ! CEYLAN_ARCH_NINTENDO_DS
 	
 	Timestamp stamp ;
 	
@@ -116,6 +174,8 @@ void LogAggregatorConsole::aggregate() throw( LogAggregatorException )
 	
 	(*_outputStream) << stamp.toString() + " Log messages aggregated." ;
 	
+#endif // CEYLAN_ARCH_NINTENDO_DS
+
 }
 
 
@@ -159,9 +219,18 @@ void LogAggregatorConsole::write( const LogMessage & message ) const throw()
 {
 
 	CEYLAN_LOG( "Writing on console message " + message.toString() ) ;
+
+#if CEYLAN_ARCH_NINTENDO_DS
+
+	_console->addInBuffer( 
+		message.toString( getMessageVerbosityLevel( message ) ) ) ;
+			
+#else // CEYLAN_ARCH_NINTENDO_DS
 		
 	(*_outputStream) <<  
 		message.toString( getMessageVerbosityLevel( message ) ) + '\n' ;
+
+#endif // CEYLAN_ARCH_NINTENDO_DS
 	
 }
 
@@ -174,31 +243,38 @@ const string LogAggregatorConsole::toString( Ceylan::VerbosityLevels level )
 		+ ( _immediateWrite ? "" : "non-" ) 
 		+ string( "immediate mode. It " )
 		+ ( _useGlobalLevelOfDetail ? "uses" : "does not use" )
-		+ string( " a global level of detail for message output. " 
-			"It uses the standard " ) ;
+		+ string( " a global level of detail for message output. It uses " ) ;
+
+#if CEYLAN_ARCH_NINTENDO_DS
 	
+	res += "a Ceylan text console" ;
+				
+#else // CEYLAN_ARCH_NINTENDO_DS
+
 	switch( _streamNumber )
 	{
 	
 		case Output:
-			res += "output" ;
+			res += "the standard output stream" ;
 			break ;	 
 		
 		case Error:
-			res += "error" ;
+			res += "the standard error stream" ;
 			break ;	 
 		
 		case Log:
-			res += "log" ;
+			res += "the standard log stream" ;
 			break ;	 
 		
 		default:
-			res += "unknown (abnormal)" ;
+			res += "an unknown stream (abnormal)" ;
 			break ;	 
 		
 	}
 	
-	res += " stream. " + LogAggregator::toString( level ) ;
+#endif // CEYLAN_ARCH_NINTENDO_DS
+
+	res += ". " + LogAggregator::toString( level ) ;
 	
 	return res ;
 	
