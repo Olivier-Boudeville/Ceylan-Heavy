8e458c5c1b88ff3bdb5077678af3becf9be0743a
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-10-01 21:31:16 +0000

    A faulty checkin of a generated version had been performed, reverted.

--- src/code/generic/CeylanHeaderVersion.h
+++ src/code/generic/CeylanHeaderVersion.h
@@ -1,10 +1,20 @@
 #ifndef CEYLAN_HEADER_VERSION_H_
 #define CEYLAN_HEADER_VERSION_H_
 
-
 #include <string>
 
 
+
+/*
+ * This file is dedicated to the Windows version, as on UNIX it is overwritten
+ * by the configure-time generated one.
+ *
+ * @note This Windows version needs CeylanHeaderVersion.cc, whereas the UNIX
+ * one does not.
+ *
+ */
+ 
+ 
 namespace Ceylan
 {
 
@@ -17,19 +27,15 @@ namespace Ceylan
 	 * program or a library was compiled with, and the actual Ceylan library
 	 * it is then linked to.
 	 *
-	 */
-	CEYLAN_DLL const std::string actualCeylanHeaderLibtoolVersion 
-		= "0.6.0" ;
-
-	
-	/*
-	 * NEVER check-in this file if there is no more CEYLAN_LIBTOOL_VERSION 
-	 * above !
+	 * @note Cannot declare here :
+	 * 'extern CEYLAN_DLL const std::string actualCeylanHeaderLibtoolVersion 
+	 * 	= CEYLAN_LIBTOOL_VERSION ;' because with Visual C++ it leads to
+	 * multiple definitions for actualCeylanHeaderLibtoolVersion.
 	 *
 	 */
-	 		
+	extern CEYLAN_DLL const std::string actualCeylanHeaderLibtoolVersion ;
+		
 }
 
 
 #endif // CEYLAN_HEADER_VERSION_H_
-
