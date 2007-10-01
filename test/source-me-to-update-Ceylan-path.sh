dde247f6e697247732aab49aa14f0f7d19932278
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-10-01 18:06:00 +0000

    Allows to run tests for Windows on Cygwin.

--- test/source-me-to-update-Ceylan-path.sh
+++ test/source-me-to-update-Ceylan-path.sh
@@ -1,7 +1,21 @@
-# Update the environment so that the Ceylan library can be found.
+# This script is meant to be sourced, so that from a Cygwin shell
+# the tests can be launched locally (where they are built) but
+# dynamically linked with the appropriate Ceylan library.
+# This applies to LOANI-based Ceylan builds.
 
-# For Windows (must be sourced from the script directory) :
-PATH=`pwd`/../src/Debug:$PATH
+#Usage : source update-test-env.sh
+
+if [ ! -f "playTests.sh" ] ; then
+  echo "Error, this script must be sourced from Ceylan/trunk/test, nothing done." 1>&2
+else
+  LIBS_LOCATION=`cygpath -a .`../../../../../LOANI-installations/OSDL-libraries/debug-mt/dll
+  #echo $LIBS_LOCATION
+  #ls "$LIBS_LOCATION"
+  export PATH=$LIBS_LOCATION:$PATH
+fi
 
 # For most UNIX platforms :
 LD_LIBRARY_PATH=`pwd`/../src/code:$LD_LIBRARY_PATH
+
+#echo $PATH
+
