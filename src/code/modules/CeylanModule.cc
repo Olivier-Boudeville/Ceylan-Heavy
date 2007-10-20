511a03412f5526b6b62b6cf6a45bc1963858f43b
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-10-20 19:38:10 +0000

    Slight reformatting.

--- src/code/modules/CeylanModule.cc
+++ src/code/modules/CeylanModule.cc
@@ -15,11 +15,12 @@ using namespace Ceylan::Log ;
 
 
 
-ModuleException::ModuleException( const std::string & message ) throw() :
+ModuleException::ModuleException( const std::string & message ) throw():
 	Ceylan::Exception( message )
 {
 
 }
+
 
 
 ModuleException::~ModuleException() throw()
@@ -29,13 +30,14 @@ ModuleException::~ModuleException() throw()
 
 
 
+
 Module::Module( const string  & name, 
 				const string  & description, 
 				const string  & homepageURL,
                 const string  & author, 
 				const string  & authorMail,
                 const Version & version, 
-				const string  & licence ) throw() :
+				const string  & licence ) throw():
 		Object(),
         _name( name ),
         _description( description ),
@@ -47,13 +49,14 @@ Module::Module( const string  & name,
 {
 
 	setVersion( version ) ;
-		
+
     send( "Creating " + toString( low ) ) ;
-	
+
 }
 
 
-Module::Module() throw() :
+
+Module::Module() throw():
 		Object(),
         _name(),
         _description(),
@@ -69,89 +72,120 @@ Module::Module() throw() :
 }
 
 
+
 Module::~Module() throw()
 {
+
     send( "Deleting " + toString( low ) ) ;
+
 }
 
 
 
 string Module::getName() const throw( ModuleException )
 {
+
     return _name ;
+	
 }
+
 
 
 void Module::setName( const string & name ) throw( ModuleException )
 {
+
     _name = name ;
+	
 }
 
 
 
 string Module::getDescription() const throw()
 {
+
     return _description ;
+	
 }
+
 
 
 void Module::setDescription( const string & description ) throw()
 {
+
     _description = description ;
+	
 }
 
 
 
 string Module::getHomePage() const throw()
 {
+
     return _homepageURL ;
+	
 }
 
 
 void Module::setHomePage( const string & homePage ) throw()
 {
+
     _homepageURL = homePage ;
+	
 }
 
 
 
 string Module::getAuthor() const throw()
 {
+
     return _author ;
+	
 }
+
 
 
 void Module::setAuthor( const string & author ) throw()
 {
+
     _author = author ;
+	
 }
 
 
 
 string Module::getAuthorMail() const throw()
 {
+
     return _authorMail ;
+	
 }
+
 
 
 void Module::setAuthorMail( const string & authorMail ) throw()
 {
+
     _authorMail = authorMail ;
+	
 }
 
 
 
 const Version & Module::getVersion() const throw()
 {
+
     return _version ;
+	
 }
 
 
 void Module::setVersion( const Version & version ) throw()
 {
-	_version.setMajorNumber(   version.getMajorNumber() ) ;
-	_version.setMinorNumber(   version.getMinorNumber() ) ;
+
+	_version.setMajorNumber(   version.getMajorNumber()   ) ;
+	_version.setMinorNumber(   version.getMinorNumber()   ) ;
 	_version.setReleaseNumber( version.getReleaseNumber() ) ;
+	
 }
 
 
@@ -159,13 +193,17 @@ void Module::setVersion( const Version & version ) throw()
 
 string Module::getLicence() const throw()
 {
+
     return _licence ;
+	
 }
 
 
 void Module::setLicence( const string & licence ) throw()
 {
+
     _licence = licence ;
+	
 }
 
 
@@ -182,7 +220,7 @@ const string Module::toString( VerbosityLevels level ) const throw()
 
 	std::list<string> moduleList ;
 	
-	moduleList.push_back( "Description : " + _description + "." ) ;
+	moduleList.push_back( "Description: " + _description + "." ) ;
 	
 	if ( TextDisplayable::GetOutputFormat() == TextDisplayable::html )
 	{
@@ -196,15 +234,15 @@ const string Module::toString( VerbosityLevels level ) const throw()
 	else
 	{
 	
-    	moduleList.push_back( "Home page : " + _homepageURL + "." ) ;
-    	moduleList.push_back( "Author : " + _author + " (" 
+    	moduleList.push_back( "Home page: " + _homepageURL + "." ) ;
+    	moduleList.push_back( "Author: " + _author + " (" 
 			+ _authorMail + ")." ) ;
 			
 	}
 			
-    moduleList.push_back(" Released through licence " + _licence + "." ) ;
+    moduleList.push_back( "Released through licence " + _licence + "." ) ;
 
-    return res + " : " + formatStringList( moduleList ) ;
+    return res + ": " + formatStringList( moduleList ) ;
 
 }
 
