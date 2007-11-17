c9b6e89d7aa84a49cb0553c72492dc4cb0fb7bf8
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-11-17 09:10:29 +0000

    Comments updated a lot.

--- src/code/system/CeylanFIFO.h
+++ src/code/system/CeylanFIFO.h
@@ -67,17 +67,36 @@ namespace Ceylan
 	
 	
 	
+	
+
+		/// The atomic data that can be sent through the FIFO.
+		typedef Ceylan::Uint32 FIFOElement ;
+		
+			
 		/**
-		 * FIFO facilities, made of a logical bidirectional interrupt-based
-		 * FIFO.
+		 * Interrupt-based FIFO (First In, First Out) class for
+		 * command-based Inter-Process Communication (IPC).
+		 *
+		 * This logical bidirectional IPC bus is synchronized by interrupts only
+		 * (no polling), for the platforms that support that, i.e. the
+		 * Nintendo DS.
+		 *
+		 * This class is dedicated to the encapsulation of two hardware FIFO, 
+		 * notably for the two of the Nintendo DS, between the two ARMs.
 		 *
 		 * The FIFO in itself communicates only using the hardware FIFO: no
-		 * shared variable is used. But of course this communication system
+		 * shared variable is used, except two that are used to report ARM7 
+		 * status and error code. But of course this communication system
 		 * can be used to send pointers to (shared) variables.
 		 *
-		 * The system in itself is in C, as the ARM7 has quite little memory for
-		 * its executable. However the ARM9 can rely on this C++ abstraction for
-		 * simpler management.
+		 * The system in itself is in C for the ARM7, as it has quite little
+		 * memory for its executable.
+		 *
+		 * @see testCeylanFIFO.arm7.c for a full example implementation.
+		 *
+		 * The ARM9 can rely on this C++ abstraction for simpler management.
+		 *
+		 * @see testCeylanFIFO.arm9.cc for a full usage example.
 		 *
 		 * Each FIFO register is only 32-bit, thus as soon as a command needs to
 		 * specify an address (32 bits), it has to use more than one FIFO 
@@ -95,19 +114,38 @@ namespace Ceylan
 		 * An application-specific protocol shall be built on top of this 
 		 * generic FIFO. 
 		 *
+		 * Request management should be asynchronous: one should send a request,
+		 * store in the state of the FIFO child class, and return, without 
+		 * waiting directly in this send function the answer: the other ARM may
+		 * send a request of its own just after this ARM sent his request, and
+		 * the request of the other ARM would be read instead of the expected
+		 * answer.
+		 *
+		 * The FIFO can work on normal mode, or a more paranoid one, if
+		 * CEYLAN_SAFE_FIFO is equal to 1. According to the tests, both are
+		 * 100% reliable (if used properly of course), but the later may
+		 * detect errors by, notably, keeping track of command numbers to
+		 * ensure none is lost and they are managed in the right order.
+		 * Note that the CEYLAN_SAFE_FIFO flag must be set for both ARM
+		 * executables.
+		 *
+		 * There could be a second fail-over mechanism, based on the 4 bits 
+		 * that each ARM can set in the IPC sync register. It is for the moment
+		 * only used during the ARM handshake, in activation step.
+		 *
 		 * Ceylan, for its inner working, has to use too that FIFO, notably so
-		 * that logs from the ARM7 can be transferred and managed on the ARM9.
-		 * Thus the FIFO has to be shared, and conventions have to be 
-		 * respected. 
+		 * that the ARM7 state can be transferred and managed on the ARM9.
+		 * Thus the FIFO has to be shared between Ceylan and the user code, 
+		 * and conventions have to be respected. 
+		 *
 		 * Each command can be made of any number of FIFO elements (they can be
 		 * read one after the other and thus the size of the FIFO does not 
 		 * matter), but the first byte of its first element must be the command
-		 * identifier: 'commandID = GetFIFOCommandIDFrom( firstFIFOElement )'.
+		 * identifier.
+		 *
 		 * Command identifiers range from 0 to 255. Ceylan reserves all command
 		 * ID between 0 and 127, thus the application must choose its
-		 * identifiers in the 128-255 range (128 free slots). The three
-		 * remaining bytes and the potential next FIFO elements are
-		 * command-specific.
+		 * identifiers in the 128-255 range (128 free slots).
 		 *
 		 * Note that they are 128 free application-specific command identifiers 
 		 * from the ARM9 to the ARM7, and another 128 identifier in the other
@@ -116,21 +154,47 @@ namespace Ceylan
 		 * Note also that the answer to a command has to use itself a command
 		 * identifier, to be able to distinguish an answer for a new request.
 		 *
+		 * If not in safe mode (CEYLAN_SAFE_FIFO not equal to 1), the three
+		 * remaining bytes and the potential next FIFO elements are
+		 * command-specific. 
+		 * 
+		 * If in safe mode (CEYLAN_SAFE_FIFO equal to 1), only the two last
+		 * bytes of the command element can be used by the application, as the
+		 * command ID (first byte) is followed by a command count (second byte)
+		 * which allows to catch any lost FIFO element. For the moment no
+		 * element has been reported lost on a real DS, neither on the emulator.
+		 * 
+		 * To send requests, use in a SetEnabledInterrupts pair (to avoid being
+		 * interrupts during the sending):
+		 * 'FIFOElement commandElement = prepareFIFOCommand( myID ) ;' followed
+		 * by the sending of this command element (writeBlocking) and any number
+		 * of other elements. Then, just out of the SetEnabledInterrupts pair,
+		 * the other ARM must be notified of the request 
+		 * (ex: notifyCommandToARM7).
+		 *
+		 * @note Once a command has been written in the FIFO, the 
+		 * 'notifyCommandToARM7' method must be called, otherwise the ARM7 may
+		 * not manage it and block, as it does not perform any polling. There
+		 * is nevertheless a fail-over mechanism: when an ARM uses blocking
+		 * reads or writes, and if the FIFO cannot perform it (as respectively
+		 * empty or full), the sending ARM will trigger the receiving ARM so
+		 * that it manages any elements in its receive queue (thus emptying it)
+		 * and, possibly, outputs answers to found requests (thus filling its
+		 * send queue).
+		 *
+		 * To handle incoming requests, override
+		 * handleReceivedApplicationCommand, which will be given the incoming
+		 * command ID, and just read the relevant number of elements.
+		 *
 		 * The overriden handleReceivedApplicationCommand method is responsible
 		 * for reading all the associated elements, so that they will not 
 		 * disturb the next decoding of the handler. 
-		 *
-		 * A fail-over polling-based FIFO manager may be added for increased
-		 * safety, see it in the VBlank handler of the examples.
 		 *
 		 * @see testCeylanFIFO.arm9.cc and testCeylanFIFO.arm7.c for a complete 
 		 * example.
 		 *
 		 * Due to the use of a callback based on a static method, the FIFO
 		 * instance is expected to be a singleton.
-		 *
-		 * @note Once a command has been written in the FIFO, the 
-		 * 'notifyCommandToARM7' method must be called.
 		 *
 		 * @see testCeylanFIFO.arm9.cc to have a complete example showing how
 		 * to avoid the numerous pitfalls of IPC programming.
@@ -141,29 +205,8 @@ namespace Ceylan
 		 * the mother destructor would trigger a call to a pure virtual
 		 * method.
 		 *
-		 * Request management should be asynchronous: one should send a request,
-		 * store in the state of the FIFO child class, and return, without 
-		 * waiting directly in this send function the answer: the other ARM may
-		 * send a request of its own just after this ARM sent his request, and
-		 * the request of the other ARM would be read instead of the expected
-		 * answer.
-		 *
-		 */
-	
-
-		/// The atomic data that can be sent through the FIFO.
-		typedef Ceylan::Uint32 FIFOElement ;
-		
-			
-	
-		/**
-		 * Bidirectional interrupt-based FIFO (First In, Firt Out) class for
-		 * command-based Inter-Process Communication (IPC).
-		 *
-		 * This class is dedicated to the encapsulation of hardware FIFO, 
-		 * notably for the two of the Nintendo DS, between the two ARMs.
-		 *
-		 * @see the FIFO class for another means, more general, of doing IPC.
+		 * @see the Ceylan::System::Pipe class for another means, more general,
+		 * of doing IPC.
 		 *
 		 * Inspired from: http://www.double.co.nz/nintendo_ds/nds_develop7.html
 		 *
@@ -206,7 +249,8 @@ namespace Ceylan
 					public: 
 					
 						explicit FIFOEmpty( const std::string & reason ) 
-							throw() ; 
+							throw() ;
+							 
 				} ;
 
 
@@ -231,7 +275,7 @@ namespace Ceylan
 		
 		
 				/**
-				 * Activates the FIFO, and notify the ARM7 of the variables
+				 * Activates the FIFO, and notifies the ARM7 of the variables
 				 * it should use to store its status and error code.
 				 *
 				 * @note This method cannot be automatically called from the
@@ -244,10 +288,10 @@ namespace Ceylan
 				 * @note This method will wait a certain amount of time to
 				 * let the ARM7 update its status. If the ARM7 implementation
 				 * is faulty (ex: there is no code on the ARM7 executable to 
-				 * handle the FIFO and the ARM7 report mechanism, an exception
+				 * handle the FIFO and the ARM7 report mechanism), an exception
 				 * will be thrown.
 				 *
-				 * @throw FIFOException should the ARM9 time-out short of an
+				 * @throw FIFOException, should the ARM9 time-out short of an
 				 * ARM7 update of its status word.
 				 *
 				 */
@@ -257,12 +301,11 @@ namespace Ceylan
 				/**
 				 * Deactivates the FIFO.
 				 *
-				 * @note This method cannot be automatically called from the
-				 * constructor, as an asynchronous IRQ could be triggered on the
-				 * receive between the call to this method and the end of the
-				 * constructor. In that case handleReceivedCommand would
-				 * be called, whereas at this moment is still pure virtual
-				 * (since still in FIFO constructor).
+				 * @note This method can be called from the destructor of the 
+				 * most specialized child class inheriting from this one.
+				 * It cannot be called directly for the destructor of this
+				 * mother FIFO, as an IRQ could trigger a pure virtual
+				 * method call before this deactivate method is called.
 				 *
 				 */
 				virtual void deactivate() throw() ;
@@ -271,15 +314,19 @@ namespace Ceylan
 				/**
 				 * Prepare the first FIFO element for specified command.
 				 * Sets the specified ID in the returned FIFO element and,
-				 * if used, sets as well the command number.
+				 * if used (in safe mode), sets as well the command number.
 				 *
 				 * @note The two (if using command number) or three (if not)
 				 * remaining bytes can be used freely by application code.
 				 *
-				 * @param id the ID to set in the FIFO element.
+				 * @note Ensure there is exactly one command sent for each
+				 * call to this method, otherwise the command count will be
+				 * incorrect.
+				 *
+				 * @param id the command ID to set in the FIFO element.
 				 *
 				 * @return a FIFO element patched with the specified ID and,
-				 * possibly, command count.
+				 * possibly, command number.
 				 *
 				 */
 				virtual FIFOElement prepareFIFOCommand( FIFOCommandID id )
@@ -303,12 +350,13 @@ namespace Ceylan
 				 * relevant.
 				 *
 				 * @note Only lightweight operations should be performed here,
-				 * to avoid the saturation of the FIFO slots and 
+				 * to avoid the saturation of the FIFO slots.
 				 *
 				 */
 				virtual void handleReceivedApplicationCommand(
 						FIFOCommandID commandID, FIFOElement firstElement )
 					throw() = 0 ;
+				
 				
 				
 				
@@ -320,8 +368,7 @@ namespace Ceylan
 				 *
 				 * Reads the relevant shared variable.
 				 *
-				 * A zero status word means either no status available, or 
-				 * that the status word shared variable was not set already.
+				 * @see CeylanARM7Codes.h for list of known status codes.
 				 *
 				 */
 				virtual ARM7StatusWord getLastARM7StatusWord() throw() ;
@@ -343,8 +390,7 @@ namespace Ceylan
 				 *
 				 * Reads the relevant shared variable.
 				 *
-				 * A zero error code means either no error, or that the error
-				 * shared variable was not set already.
+				 * @see CeylanARM7Codes.h for list of known error codes.
 				 *
 				 */
 				virtual ARM7ErrorCode getLastARM7ErrorCode() throw() ;
@@ -430,16 +476,14 @@ namespace Ceylan
 				 * to add a polling-based layer of security to the IRQ-based
 				 * approach, in case an interrupt is lost.
 				 *
-				 * Do not use elsewhere than in the VBlank handler, as it
-				 * acknowledges the IRQ has been managed.
-				 *
 				 * This signature is mandatory. Public so that it can be 
 				 * registered as an handler externally.
 				 *
-				 * @note Just checks whether the FIFO is empty. If not, calls
+				 * Just checks whether the FIFO is empty. If not, calls
 				 * the handleReceivedCommand method.
 				 *
-				 * @see FIFOHandlerForFIFO
+				 * @note Not used here anymore as not needed: no polling needed,
+				 * and used to create synchronization issues.
 				 *
 				 */
 				static void VBlankHandlerForFIFO() ;
@@ -469,7 +513,7 @@ namespace Ceylan
 
 				/**
 				 * Tells whether there is data available for reading in the
-				 * receiving queue.
+				 * ARM9 receiving queue.
 				 *
 				 */
 				bool dataAvailableForReading() const throw() ;
@@ -477,7 +521,7 @@ namespace Ceylan
 				
 				/**
 				 * Tells whether there is space available for writing in the
-				 * sending queue.
+				 * ARM9 sending queue.
 				 *
 				 */
 				bool spaceAvailableForWriting() const throw() ;
@@ -491,10 +535,6 @@ namespace Ceylan
 				 * @throw FIFOException, including FIFOEmpty if the FIFO has
 				 * no such element.
 				 *
-				 * @note The associated interrupt will be acknowledged, even
-				 * if there was more than one element in the queue: elements may
-				 * remain there.
-				 *
 				 */
 				FIFOElement read() throw( FIFOException ) ;
 	
@@ -502,11 +542,9 @@ namespace Ceylan
 				/**
 				 * Helper method that returns the first FIFO element available
 				 * in the FIFO. If not already available, the method will wait
-				 * for it (potentially forever), using atomic sleeps.
-				 *
-				 * @note The associated interrupt will be acknowledged, even
-				 * if there was more than one element in the queue: elements may
-				 * remain there.
+				 * for it (potentially forever), while helping notifying 
+				 * never-ending reads and overcoming them, by triggering the
+				 * ARM7.
 				 *
 				 * @throw FIFOException in case a FIFO error is reported.
 				 *
@@ -530,7 +568,8 @@ namespace Ceylan
 				 * Helper methods that write the specified FIFO element in the
 				 * send queue. If there is no more space available in that
 				 * queue, the method will wait for it (potentially forever),
-				 * using atomic sleeps.
+				 * while helping notifying never-ending writes and overcoming
+				 * them, by triggering the ARM7.
 				 *
 				 * @throw FIFOException in case a FIFO error is reported.
 				 *
@@ -555,7 +594,7 @@ namespace Ceylan
 				
 				/**
 				 * Increments the current count of processed commands, in
-				 * [0,15], and update accordingly the IPC sync register.
+				 * [0,15], and updates accordingly the IPC sync register.
 				 *
 				 */
 				virtual void incrementProcessedCount() throw() ;
@@ -588,27 +627,28 @@ namespace Ceylan
 				
 				
 				
+				
 				// Static section.
 				
 					
 				/**
 				 * Callback called whenever the ARM7 triggers an IPC
-				 * Synchronize interrupt.
+				 * Synchronize interrupt on the ARM9.
 				 *
 				 * This signature is mandatory.
-				 *
-				 * @note Just results in a call to its virtual method 
-				 * counterpart.
 				 *
 				 * @see handleReceivedCommand
 				 *
 				 * @see VBlankHandlerForFIFO
+				 *
 				 */
 				static void SyncHandlerForFIFO() ;
 	
 	
 				/**
-				 * Manages a command being sent through the FIFO.
+				 * Manages a command sent through the FIFO.
+				 *
+				 * Static, to be called as an IRQ handler.
 				 *
 				 * @see handleReceivedCommand
 				 *
@@ -637,7 +677,7 @@ namespace Ceylan
 				 * 'ARM7StatusWord volatile *', as only the pointed value can
 				 * be modified by the ARM7. It worked on NoCashGBA emulator,
 				 * but not on the DS, until we declared as well the pointer
-				 * itself to be volatile.
+				 * itself to be volatile. Maybe a problem due to the ARM9 cache.
 				 *
 				 */
 				ARM7StatusWord volatile * volatile _arm7StatusWordPointer ;
@@ -652,7 +692,7 @@ namespace Ceylan
 				 * 'ARM7ErrorCode volatile *', as only the pointed value can
 				 * be modified by the ARM7. It worked on NoCashGBA emulator,
 				 * but not on the DS, until we declared as well the pointer
-				 * itself to be volatile.
+				 * itself to be volatile. Maybe a problem due to the ARM9 cache.
 				 *
 				 */
 				ARM7ErrorCode volatile * volatile _arm7ErrorCodePointer ;
@@ -662,7 +702,7 @@ namespace Ceylan
 				 * Records the overall number of sent commands to the FIFO.
 				 *
 				 * The number of a command may be stored in its FIFO element
-				 * for increasing reliability.
+				 * for increased reliability, in safe mode.
 				 *
 				 * @note 8-bit, hence wraps around after 255.
 				 *
@@ -678,7 +718,7 @@ namespace Ceylan
 				 * Records the overall number of received commands to the FIFO.
 				 *
 				 * The number of a command may be stored in its FIFO element
-				 * for increasing reliability.
+				 * for increased reliability, in safe mode.
 				 *
 				 * @note 8-bit, hence wraps around after 255.
 				 *
