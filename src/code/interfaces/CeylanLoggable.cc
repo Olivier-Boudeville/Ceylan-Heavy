7cb3173d9247699844275d63897d4dc691bc4a8e
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-11-17 09:01:44 +0000

    Slight reformatting.

--- src/code/interfaces/CeylanLoggable.cc
+++ src/code/interfaces/CeylanLoggable.cc
@@ -1,9 +1,9 @@
 #include "CeylanLoggable.h"
 
-#include "CeylanLogPlug.h"  // for GetTransport
+#include "CeylanLogPlug.h"  // for LogPlug::GetTransport
 #include "CeylanLog.h"      // for ProtocolSeparator
 
-// for getEmbeddedURI, getProtocolName,ProtocolSeparator  :
+// for getEmbeddedURI, getProtocolName,ProtocolSeparator:
 #include "CeylanUniformResourceIdentifier.h" 
 
 
@@ -16,23 +16,32 @@ using namespace Ceylan::Log ;
 const string Loggable::ProtocolName = "loggable" ;
 
 
-Loggable::Loggable( const string & name ) throw() : 
+
+Loggable::Loggable( const string & name ) throw(): 
 	LogSource( LogPlug::GetTransport() )
 {
+
 	setChannelName( name ) ;
+	
 }
+
 
 
 Loggable::~Loggable() throw()
 {
+
 }
+
 
 
 void Loggable::setChannelName( const std::string & channelName ) throw()
 {
+
 	LogSource::setChannelName( ProtocolName + Ceylan::URI::ProtocolSeparator 
 		+ channelName ) ;
+		
 }
+
 
 
 bool Loggable::IsALoggableChannelName( const string & channelName ) throw()
@@ -44,12 +53,14 @@ bool Loggable::IsALoggableChannelName( const string & channelName ) throw()
 }
 
 
+
 const string Loggable::GetEmbeddedChannelName( const string & fullChannelName )
 	throw()
 {	
 
-	// Removes protocol separator (typically : '//')
+	// Removes protocol separator (typically: '//')
 	return Ceylan::URI::getEmbeddedURI( fullChannelName, 
 		Ceylan::URI::ProtocolSeparator ) ;
 			
 }
+
