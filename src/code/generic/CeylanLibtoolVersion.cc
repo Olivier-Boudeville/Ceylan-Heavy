d03ae44c4e82f193625c4b7b10fd1886b1781123
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-11-17 09:23:31 +0000

    Slight reformatting.

--- src/code/generic/CeylanLibtoolVersion.cc
+++ src/code/generic/CeylanLibtoolVersion.cc
@@ -12,7 +12,7 @@ using namespace Ceylan ;
 
 
 LibtoolVersion::LibtoolVersion( VersionNumber current, 
-		VersionNumber revision, VersionNumber age ) throw( VersionException ) :
+		VersionNumber revision, VersionNumber age ) throw( VersionException ):
 	Version( current, revision, age )
 {
 
@@ -23,7 +23,7 @@ LibtoolVersion::LibtoolVersion( VersionNumber current,
 
 
 LibtoolVersion::LibtoolVersion( const std::string & versionText ) 
-		throw( VersionException ) :
+		throw( VersionException ):
 	Version( versionText )
 {
 
@@ -42,14 +42,19 @@ LibtoolVersion::~LibtoolVersion() throw()
 
 Version::VersionNumber LibtoolVersion::getCurrentInterfaceNumber() const throw()
 {
+
 	return _major ;
+	
 }
+
 
 
 void LibtoolVersion::setCurrentInterfaceNumber( VersionNumber newCurrent )
 	throw()
 {
+
 	_major = newCurrent ;
+	
 }
 
 
@@ -57,27 +62,37 @@ void LibtoolVersion::setCurrentInterfaceNumber( VersionNumber newCurrent )
 Version::VersionNumber LibtoolVersion::getRevisionInterfaceNumber() 
 	const throw()
 {
+
 	return _minor ;
+	
 }
+
 
 
 void LibtoolVersion::setRevisionInterfaceNumber( VersionNumber newRevision )
 	throw()
 {
+
 	_minor = newRevision ;
+	
 }
 
 
 
 Version::VersionNumber LibtoolVersion::getInterfaceAge() const throw()
 {
+
 	return _release ;
+	
 }
+
 
 
 void LibtoolVersion::setInterfaceAge( VersionNumber newAge ) throw()
 {
+
 	_release = newAge ;
+	
 }
 
 
@@ -87,12 +102,12 @@ bool LibtoolVersion::isCompatibleWith( const Version & expectedVersion ) const
 {
   	
  	if ( ! canBeComparedWith( expectedVersion ) )
-		throw VersionException( "LibtoolVersion::isCompatibleWith : "
+		throw VersionException( "LibtoolVersion::isCompatibleWith: "
 			+ toString( Ceylan::high ) + " and "
 			+ expectedVersion.toString( Ceylan::high ) 
 			+ " cannot be compared." ) ;
 		
-	// The cast has been checked by previous call :	
+	// The cast has been checked by previous call:	
  	const LibtoolVersion * originalVersion = 
 		dynamic_cast<const LibtoolVersion *>( & expectedVersion ) ;
 	
@@ -118,13 +133,14 @@ bool LibtoolVersion::isCompatibleWith( const Version & expectedVersion ) const
 		
 	/*
 	 * Here this library version is more recent than the user-expected one,
-	 * age will tell if compatibility is ensured :
+	 * age will tell if compatibility is ensured:
 	 *
 	 */	
 	return ( originalInterfaceNumber >=
 		( getCurrentInterfaceNumber() - getInterfaceAge() ) ) ; 
 		
 }
+
 
 
 const string LibtoolVersion::toString( VerbosityLevels level ) const throw()
@@ -139,7 +155,7 @@ const string LibtoolVersion::toString( VerbosityLevels level ) const throw()
 	if ( level == Ceylan::medium )
     	return "Libtool version " + rawVersion ;
 				
-	return "Libtool version : current interface number = " 
+	return "Libtool version: current interface number = " 
 		+ Ceylan::toNumericalString( _major ) 
 		+ ", current revision number = "
 		+ Ceylan::toNumericalString( _minor ) + ", interface age = "
@@ -148,20 +164,25 @@ const string LibtoolVersion::toString( VerbosityLevels level ) const throw()
 }
 
 
+
 bool LibtoolVersion::isUsualVersionSchemeCompliant() const throw()
 {
+
 	return false ;
+	
 }
+
 
 
 bool LibtoolVersion::canBeComparedWith( const Version & version ) const throw()
 {	
 	
-	// Only Libtool versions can be compared with Libtool versions :
+	// Only Libtool versions can be compared with Libtool versions:
 	
 	return ( dynamic_cast<const LibtoolVersion *>( & version ) != 0 ) ;
 	
 }
+
 
 
 void LibtoolVersion::checkConsistency() const throw( VersionException )
@@ -169,12 +190,12 @@ void LibtoolVersion::checkConsistency() const throw( VersionException )
 
 	/*
 	 * More restrictions apply to Libtool versions, age must be less than or
-	 * equal to current :
+	 * equal to current:
 	 *
 	 */
 	
 	if ( _release > _major )
-		throw VersionException( "Incorrect Libtool version : interface age ("
+		throw VersionException( "Incorrect Libtool version: interface age ("
 			+ Ceylan::toString( _release ) 
 			+ ") must be less than or equal to current ("
 			+ Ceylan::toString( _major ) + ")." ) ;
