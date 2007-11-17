03273228c9623a7d5d0643f6a93b3b502aff6c8f
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-11-17 09:19:33 +0000

    Slight reformatting.

--- src/code/logs/CeylanLogTransport.cc
+++ src/code/logs/CeylanLogTransport.cc
@@ -7,6 +7,7 @@ using namespace Ceylan::Log ;
 
 
 
+
 LogTransport::LogTransport() throw() 
 {
 
@@ -14,14 +15,20 @@ LogTransport::LogTransport() throw()
 }
 
 
+
 LogTransport::~LogTransport() throw() 
 {
 
 }
 
+
+
 const string LogTransport::toString( Ceylan::VerbosityLevels level ) 
 	const throw() 
 {
+
 	return "This LogTransport is a pure intermediary between "
 		"a LogSource and a LogListener" ; 
+		
 }	
+
