f83c82fa99fd2bc1b4c4f08c0455b66b85c773c0
Olivier Boudeville <olivier.boudeville@esperide.com>
2007-12-20 18:52:39 +0000

    Slight reformatting.

--- src/code/scripts/erlang/common/utils_test.erl
+++ src/code/scripts/erlang/common/utils_test.erl
@@ -7,13 +7,17 @@
 
 -define(Tested_module,utils).
 
+
+
 run() ->
 	io:format( "--> Testing module ~s.~n", [ ?Tested_module ] ),
-	io:format( "   Timestamp is ~s.~n", [ utils:get_timestamp() ] ),
+
+	io:format( "   Timestamp is ~s.~n", [ utils:get_textual_timestamp() ] ),
 	io:format( "   An image file would be ~s.~n", 
 		[ utils:get_image_file_gif( "MyImage" ) ] ),		
 	io:format( "   Output with term_toString : ~s, ~s and ~s.~n", 
 		[ utils:term_toString(an_atom), utils:term_toString([1,2]), 
 			utils:term_toString("A string")	]),
-	io:format( "--> End of test for module ~s.~n", [ ?Tested_module ] ),	
+
+	io:format( "--> End of test for module ~s.~n", [ ?Tested_module ] ),
 	erlang:halt().
