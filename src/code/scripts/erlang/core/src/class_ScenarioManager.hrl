675f421dc46d51caacee9d6dc60375bcd5c644d5
Olivier Boudeville <olivier.boudeville@esperide.com>
2008-06-13 20:01:33 +0000

    scenario_manager_name conditionally added.

--- src/code/scripts/erlang/core/src/class_ScenarioManager.hrl
+++ src/code/scripts/erlang/core/src/class_ScenarioManager.hrl
@@ -1,7 +1,11 @@
 
+-ifndef(scenario_manager_name).
+
 % The name under which the scenario manager process is to be registered:
 % (use ?scenario_manager_name to have it)
 -define(scenario_manager_name,ceylan_scenario_manager).
+
+-endif.
 
 
 % Determines how many ticks a fixed-length scenario should last by default:
