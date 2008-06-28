3909287dd8cb7a29e4f6d09d2d6d36b957dc8cd8
Olivier Boudeville <olivier.boudeville@esperide.com>
2008-06-28 18:48:57 +0000

    Timestamp and duration tested.

--- src/code/scripts/erlang/common/src/utils_test.erl
+++ src/code/scripts/erlang/common/src/utils_test.erl
@@ -10,11 +10,17 @@
 
 
 run() ->
+
 	io:format( "--> Testing module ~s.~n", [ ?Tested_module ] ),
 
-	io:format( "   Timestamp is ~s.~n", [ utils:get_textual_timestamp() ] ),
+	InitialTimestamp = utils:get_timestamp(),
+	
+	io:format( "   Timestamp is ~s.~n", [ 
+		utils:get_textual_timestamp(InitialTimestamp) ] ),
+	
 	io:format( "   An image file would be ~s.~n", 
-		[ utils:get_image_file_gif( "MyImage" ) ] ),		
+		[ utils:get_image_file_gif( "MyImage" ) ] ),	
+			
 	io:format( "   Output with term_toString : ~s, ~s and ~s.~n", 
 		[ utils:term_toString(an_atom), utils:term_toString([1,2]), 
 			utils:term_toString("A string")	]),
@@ -25,5 +31,12 @@ run() ->
 	io:format( "   Joining list ~w with separator '~s', result is : '~s'.~n",
 		[ToJoin,Separator,utils:join(Separator,ToJoin)] ),		
 
+	io:format( "   Converting an integer to a string: ~s.~n",
+		[ utils:integer_to_string(3245) ] ),		
+
+	io:format( "   Displaying the duration of this test: ~s.~n", 
+		[ utils:get_textual_duration( InitialTimestamp,
+			utils:get_timestamp() ) ] ),
+		
 	io:format( "--> End of test for module ~s.~n", [ ?Tested_module ] ),
 	erlang:halt().
