976d5077447e81af6e60131e38bb32d988605fe2
Olivier Boudeville <olivier.boudeville@esperide.com>
2008-07-12 19:56:08 +0000

    Almost empty test added.

--- src/code/scripts/erlang/common/src/file_utils_test.erl
+++ src/code/scripts/erlang/common/src/file_utils_test.erl
@@ -12,14 +12,6 @@ run() ->
 
 	io:format( "--> Testing module ~s.~n", [ ?Tested_module ] ),
 	
-	io:format( "   An image file would be ~s.~n", 
-		[ file_utils:get_image_file_gif( "MyImage" ) ] ),	
-			
-	Separator = ", ",
-	ToJoin = [ "January", "February", "March", "April", "May" ],
-	
-	io:format( "   Joining list ~w with separator '~s', result is : '~s'.~n",
-		[ToJoin,Separator,file_utils:join(Separator,ToJoin)] ),		
 		
 	io:format( "--> End of test for module ~s.~n", [ ?Tested_module ] ),
 	erlang:halt().
