555742216997a046f2f3f1bee8efca080a6b0e9c
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-02-12 21:17:31 +0000

    Rewriting in progress.

--- src/code/scripts/erlang/core/src/class_RandomManager.hrl
+++
@@ -1,5 +0,0 @@
-
-% The name under which the random manager process is to be registered:
-% (use ?random_manager_name to have it)
--define(random_manager_name,ceylan_random_manager).
-
--- src/code/scripts/erlang/core/src/class_SimulationManager.hrl
+++
@@ -1,5 +0,0 @@
-
-% The name under which the simulation manager process is to be registered:
-% (use ?simulation_manager_name to have it)
--define(simulation_manager_name,ceylan_simulation_manager).
-
