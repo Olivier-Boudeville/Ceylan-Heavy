3fcb66c32c824ee0c3a45423235a2fc59ed89806
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-03-26 20:34:07 +0000

    More examples added.

--- src/code/scripts/erlang/wooper/examples/class_Creature.erl
+++ src/code/scripts/erlang/wooper/examples/class_Creature.erl
@@ -6,21 +6,28 @@
 
 
 % Parameters taken by the constructor ('construct'):
--define(wooper_construct_parameters,Age,Gender).
+-define(wooper_construct_parameters, Age, Gender ).
 
 % Construction-related exported operators:
--define(wooper_construct_export,new/2,new_link/2,
-	synchronous_new/2,synchronous_new_link/2,construct/3,toString/1).
+% Declaring all variations of WOOPER standard life-cycle operations:
+% (template pasted, two replacements performed to update arities)
+-define(wooper_construct_export, new/2, new_link/2, 
+	synchronous_new/2, synchronous_new_link/2,
+	synchronous_timed_new/2, synchronous_timed_new_link/2,
+	remote_new/3, remote_new_link/3, remote_synchronous_new/3,
+	remote_synchronous_new_link/3, remote_synchronous_timed_new/3,
+	remote_synchronous_timed_new_link/3, construct/3 ).
+
 
 
 % Declarations of class-specific methods (besides inherited ones).
 % isHotBlooded/1 and canEat/2 are abstract here, hence not mentioned:
--define(wooper_method_export,getAge/1,setAge/2,declareBirthday/1,getGender/1,
-	getArbitraryNumber/1,testDirectMethodExecution/2).
+-define(wooper_method_export, getAge/1, setAge/2, declareBirthday/1, 
+	getGender/1, getArbitraryNumber/1, testDirectMethodExecution/2 ).
 
 
 % Non-method exported functions:
--export([example_fun/0]).
+-export([ example_fun/0, toString/1 ]).
 
 
 % Allows to define WOOPER base variables and methods for that class:
@@ -32,10 +39,6 @@ construct(State,?wooper_construct_parameters) ->
 	% No mother class.
 	?setAttributes(State, [ {age,Age}, {gender,Gender} ] ).
 	
-
-% This looks like a method, but it is not (returning only a string):	
-toString(State) ->
-	hashtable:toString( State#state_holder.attribute_table ).
 
 	
 
@@ -75,23 +78,30 @@ getArbitraryNumber(State) ->
 testDirectMethodExecution(State,NewAge) ->
 
 	io:format( "Testing executeOneway.~n" ),
-	{wooper_result,NewState} = executeOneway(State,setAge,NewAge),
+	NewState = executeOneway(State,setAge,NewAge),
 	
 	% Not the 36 returned by this class (347 given by the test of Mammal) :
 	347 = ?getAttribute(NewState,age),
 	
 	io:format( "Testing executeRequest.~n" ),
 	% 15 from child classes (Mammal or Reptile), not 10 from here:
-	{wooper_result,OtherState,15} =
-		executeRequest(NewState,getArbitraryNumber,[]),
+	{OtherState,15} = executeRequest(NewState,getArbitraryNumber,[]),
 
 	io:format( "Direct self-invocation success.~n" ),
 
 	?wooper_return_state_only(OtherState).
 
 
+
+% Helper function.
+
 % Just to show it can exist:	
 example_fun() ->
 	ok.
 	
 	
+% This looks like a method, but it is not (returning only a string):	
+% (function)
+toString(State) ->
+	hashtable:toString( State#state_holder.attribute_table ).
+
