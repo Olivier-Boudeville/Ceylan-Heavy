cb7476eea630d09047a1857432b57b8589e5c6ac
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-03-26 20:36:06 +0000

    Newer template for new variations integrated.

--- src/code/scripts/erlang/wooper/examples/class_Reptile.erl
+++ src/code/scripts/erlang/wooper/examples/class_Reptile.erl
@@ -7,14 +7,21 @@
 
 % Parameters taken by the constructor ('construct'). 
 % They are here the ones of the mother class (creature):
--define(wooper_construct_parameters,Age,Gender).
+-define(wooper_construct_parameters, Age, Gender ).
 
-% Construction-related exported operators:
--define(wooper_construct_export,new/2,new_link/2,
-	synchronous_new/2,synchronous_new_link/2,construct/3).
+
+% Declaring all variations of WOOPER standard life-cycle operations:
+% (template pasted, two replacements performed to update arities)
+-define( wooper_construct_export, new/2, new_link/2, 
+	synchronous_new/2, synchronous_new_link/2,
+	synchronous_timed_new/2, synchronous_timed_new_link/2,
+	remote_new/3, remote_new_link/3, remote_synchronous_new/3,
+	remote_synchronous_new_link/3, remote_synchronous_timed_new/3,
+	remote_synchronous_timed_new_link/3, construct/3 ).
+	
 
 % Declarations of class-specific methods (besides inherited ones).
--define(wooper_method_export,setAge/2,isHotBlooded/1,canMoult/1).
+-define(wooper_method_export, setAge/2, isHotBlooded/1, canMoult/1 ).
 
 
 
@@ -48,5 +55,4 @@ isHotBlooded(State) ->
 % All reptiles can moult:
 canMoult(State) ->
 	?wooper_return_state_result(State,true).
-	
 	
