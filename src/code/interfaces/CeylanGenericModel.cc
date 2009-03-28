7daedb0b2dd09d458f84b4f8952054b796248249
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-03-28 19:12:55 +0000

    Implementation of non-templated models added.

--- src/code/interfaces/CeylanGenericModel.cc
+++ src/code/interfaces/CeylanGenericModel.cc
@@ -3,9 +3,26 @@
 #include "CeylanGenericView.h"
 #include "CeylanOperators.h"
 
+
 using namespace Ceylan ;
+using namespace Ceylan::MVC ;
+
 
 using std::string ;
+
+
+
+/* 
+ * In this file following classes are implemented:
+ *
+ *   - BaseModel
+ *   - NoViewModel
+ *   - SingleViewModel
+ *   - MultipleViewModel
+ *
+ */
+
+
 
 
 // BaseModel section.
@@ -35,16 +52,59 @@ const string BaseModel::toString( Ceylan::VerbosityLevels level ) const throw()
 
 
 
-// SingleViewGenericModel section.
+// NoViewModel section.
 
 
-SingleViewGenericModel::SingleViewGenericModel( const BaseView & view ) :
+NoViewModel::NoViewModel() throw()
+{
+
+}
+
+
+
+NoViewModel::~NoViewModel() throw()
+{
+
+}
+
+
+
+void NoViewModel::addView( const BaseView & view ) const
+	throw( GenericMVCException )
+{
+
+	throw GenericMVCException( "NoViewModel::addView failed: "
+		"no view supported by this model." ) ;
+		
+}
+
+
+	
+const string NoViewModel::toString( Ceylan::VerbosityLevels level ) 
+	const throw()
+{
+
+	return "View-less model" ;
+	
+}
+
+
+
+
+
+// Non-templated SingleViewModel section.
+
+
+
+SingleViewModel::SingleViewModel( const BaseView & view ):
 	_view( & view )
 {
 
 }
 
-SingleViewGenericModel::SingleViewGenericModel() :
+
+
+SingleViewModel::SingleViewModel():
 	_view( 0 )
 {
 
@@ -52,51 +112,192 @@ SingleViewGenericModel::SingleViewGenericModel() :
 
 
 
-SingleViewGenericModel::~SingleViewGenericModel() throw()
+SingleViewModel::~SingleViewModel() throw()
 {
 
 	if ( _view != 0 )
+	{
+			
+		// View owned here:
 		delete _view ;
-	
+				
+	}
+		
 }
 
 
 
-void SingleViewGenericModel::addView( const BaseView & view ) 
+void SingleViewModel::setView( const BaseView & view )
+	throw( GenericMVCException )
+{
+
+	addView( view ) ;
+	
+}
+
+
+	
+void SingleViewModel::addView( const BaseView & view ) const 
 	throw( GenericMVCException )
 {
 
 	if ( _view != 0 )
-		throw GenericMVCException( "SingleViewGenericModel::addView failed: "
+		throw GenericMVCException( "SingleViewModel::addView failed: "
 			"a view was already registered." ) ;
-	
-	_view = & view ;
-	
+			
+	// Do as if this instance had not to be 'const':
+	const_cast<SingleViewModel *>(this)->_view = & view ;
+		
 }
+
+
 	
-
-
-const string SingleViewGenericModel::toString( Ceylan::VerbosityLevels level )
+const string SingleViewModel::toString( Ceylan::VerbosityLevels level ) 
 	const throw()
 {
 
 	if ( _view != 0 )
 	{
 	
-		// Beware to infinite recursion:
+		/*
+		 * Beware to infinite recursion:
+		 * (the view may display in turn this model)
+		 *
+		 */
+
 		if ( level == Ceylan::low )
-			return "Single-view generic model owning a view" ;
-		else	
-			return "Single-view generic model owning following view: " 
-				+ Ceylan::toString( _view ) ;
-			
-	}
+			return "Single-view controller-less model owning a view" ;
+		else
+			return "Single-view controller-less model owning " 
+				+ _view->toString( level ) ;
+
+	}		
 	else
 	{
 	
-		return "Single-view generic model with no view registered." ;
+		return "Single-view controller-less model "
+			"not associated to any view" ;
 	
-	}		
+	}
 	
+}
+
+
+
+
+
+// Non-templated MultipleViewModel section.
+
+
+
+MultipleViewModel::MultipleViewModel( const BaseView & view )
+{
+
+	_views.push_back( & view ) ;	
+	
+}
+
+
+
+MultipleViewModel::MultipleViewModel( 
+	const std::list<const BaseView *> & views )
+{
+
+	_views = views ;	
+	
+}
+
+
+
+MultipleViewModel::MultipleViewModel()
+{
+
+}
+
+
+
+MultipleViewModel::~MultipleViewModel() throw()
+{
+
+	deleteViews() ;
+		
+}
+
+
+
+void MultipleViewModel::setViews( 
+	const std::list<const BaseView *> & views ) throw( GenericMVCException )
+{
+
+	if ( ! _views.empty() )
+		throw GenericMVCException( "MultipleViewModel::setViews failed: "
+			"there was at least one view registered." ) ;
+					
+	_views = views ;
+	
+}
+
+
+	
+void MultipleViewModel::addView( const BaseView & view ) const
+	throw( GenericMVCException )
+{
+
+	const_cast<MultipleViewModel *>( this )->_views.push_back( & view ) ;
+		
+}
+
+
+	
+const string MultipleViewModel::toString( Ceylan::VerbosityLevels level )
+	const throw()
+{
+
+	if ( _views.empty() )
+		return "Multiple-view controller-less model "
+			"not associated to any view" ;
+			
+	/*
+	 * Beware to infinite recursion:
+	 * (the view may display in turn this model)
+	 *
+	 */
+
+	if ( level == Ceylan::low )
+		return "Multiple-view controller-less model owning " 
+			+ Ceylan::toString( _views.size() ) + " view(s)" ;
+
+
+	std::list<std::string> res ;
+	
+	for ( std::list<const BaseView *>::const_iterator it =
+		_views.begin(); it != _views.end() ; it++ )
+	{
+	
+		res.push_back( (*it)->toString( level ) ) ;
+		
+	}
+				
+	return "Multiple-view controller-less model "
+		"associated to following view(s): "
+				+ Ceylan::formatStringList( res ) ;
+	
+}
+
+
+
+void MultipleViewModel::deleteViews()
+{
+
+	for ( std::list<const BaseView *>::const_iterator it =
+		_views.begin(); it != _views.end(); it++ )
+	{
+			
+		delete *it ;
+				
+	}
+			
+	_views.clear() ;
+
 }
 
