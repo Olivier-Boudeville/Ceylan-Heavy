1c5ba32e118209eba1f9df688b1cd9f93026f043
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-05-28 20:59:25 +0000

    Test exceeds displaying, does some automatic checking as well.

--- src/code/scripts/erlang/common/src/hashtable_test.erl
+++ src/code/scripts/erlang/common/src/hashtable_test.erl
@@ -30,39 +30,52 @@
 % See the hashtable.erl tested module.
 
 -module(hashtable_test).
-% Directly depends on hashtable module.
+% Directly depends on the hashtable module.
+
 
 -export([run/0]).
 
+
 -define(Tested_module,hashtable).
+
 
 run() ->
 	io:format( "--> Testing module ~s.~n", [ ?Tested_module ] ),
-	MyH1=hashtable:new(0),
+	MyH1 = hashtable:new(0),
 	hashtable:display(MyH1),
-	MyH2=hashtable:new(4),
-	MyH3=hashtable:addEntry("MyFirstKey","MyFirstValue",MyH2),
-	MyH4=hashtable:addEntry("AnotherKey",[1,2,3],MyH3),
+	MyH2 = hashtable:new(4),
+	MyH3 = hashtable:addEntry("MyFirstKey","MyFirstValue",MyH2),
+	MyH4 = hashtable:addEntry("AnotherKey",[1,2,3],MyH3),
 	hashtable:display(MyH4),
-	io:format( "   Looking up for ~s: ~w~n", [ "MyFirstKey",
+	
+	io:format( "   Looking up for ~s: ~p~n", [ "MyFirstKey",
 		hashtable:lookupEntry("MyFirstKey",MyH4)]),
+	{value,"MyFirstValue"} = hashtable:lookupEntry("MyFirstKey",MyH4),
+		
 	io:format( "   Removing that entry.~n" ),
-	MyH5=hashtable:removeEntry("MyFirstKey",MyH4),
-	io:format( "   Looking up for ~s: ~w~n", [ "MyFirstKey",
+	MyH5 = hashtable:removeEntry("MyFirstKey",MyH4),
+	io:format( "   Looking up for ~s: ~p~n", [ "MyFirstKey",
 		hashtable:lookupEntry("MyFirstKey",MyH5)]),
+	{hashtable_key_not_found,"MyFirstKey"} =
+		hashtable:lookupEntry("MyFirstKey",MyH5),
+		
 	% removeEntry can also be used if the specified key is not here, will 
 	% return an identical table.	
 	hashtable:display(MyH5),
 	io:format( "   Testing double key registering.~n" ),
-	MyH6=hashtable:addEntry("AnotherKey",anything,MyH5),
+	MyH6 = hashtable:addEntry("AnotherKey",anything,MyH5),
 	hashtable:display(MyH6),
-	io:format( "   Enumerating the hash table: ~w~n",
+	io:format( "   Enumerating the hash table: ~p~n",
 		[hashtable:enumerate(MyH4)]),
-	io:format( "   Listing the hash table keys: ~w~n",
+	io:format( "   Listing the hash table keys: ~p~n",
 		[hashtable:keys(MyH4)]),
-	MyH7=hashtable:addEntry("Third key",3,MyH6),
+	["MyFirstKey","AnotherKey"] = hashtable:keys(MyH4),
+	
+	MyH7 = hashtable:addEntry("Third key",3,MyH6),
 	% MyH8 should have {AnotherKey,[1,2,3]} and {"Third key",3}:
-	MyH8=hashtable:merge(MyH4,MyH7),
+	MyH8 = hashtable:merge(MyH4,MyH7),
 	io:format( "   Merged table: ~s~n", [hashtable:toString(MyH8)]),
+	
 	io:format( "--> End of test for module ~s.~n", [ ?Tested_module ] ),	
 	erlang:halt().
+
