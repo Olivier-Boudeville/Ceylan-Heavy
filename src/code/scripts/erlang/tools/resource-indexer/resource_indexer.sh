be010125e1c8dcb65434be4bf4f190ea8b5734c4
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-06-03 10:10:11 +0000

    Updated so that it can be run from an install.

--- src/code/scripts/erlang/tools/resource-indexer/resource_indexer.sh
+++ src/code/scripts/erlang/tools/resource-indexer/resource_indexer.sh
@@ -2,6 +2,21 @@
 
 # Launcher for the Erlang resource indexer.
 
-beam_paths=". ../../common/src"
+scan_dir=`pwd`
 
-make -s resource_indexer.beam && erl +W w -- $* -pz ${beam_paths} +K true +A 8 -noshell -eval 'resource_indexer:main()' 
+# Going where the script is available, as we found the needed BEAM from there:
+indexer_dir=`dirname $0`
+
+#echo "indexer_dir = $indexer_dir"
+
+cd $indexer_dir
+
+
+# Paths are specified both for source directories and for installation ones:
+beam_paths=". ../../common/src ../../common/ebin"
+
+script_options="$*"
+
+#make -s resource_indexer.beam && 
+erl +W w -- ${script_options} -pz ${beam_paths} +K true +A 8 -noshell -eval 'resource_indexer:main()' 
+
