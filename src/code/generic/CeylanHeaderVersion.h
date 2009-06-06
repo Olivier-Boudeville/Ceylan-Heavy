b37b66aa3d1b7c42c38936cce9f9d2429356d535
Olivier Boudeville <olivier.boudeville@esperide.com>
2009-06-06 19:04:07 +0000

    Restored back (with standard header) after having been overwritten one more time...

--- src/code/generic/CeylanHeaderVersion.h
+++ src/code/generic/CeylanHeaderVersion.h
@@ -24,6 +24,7 @@
  */
 
 
+
 #ifndef CEYLAN_HEADER_VERSION_H_
 #define CEYLAN_HEADER_VERSION_H_
 
@@ -31,30 +32,40 @@
 #include <string>
 
 
+
+/*
+ * This file is dedicated to the non-UNIX versions (Windows, Nintendo DS, etc.),
+ * as on UNIX it is overwritten by the configure-time generated one, from
+ * CeylanHeaderVersion.h.in.
+ *
+ * @note This version needs CeylanHeaderVersion.cc, whereas the UNIX one does
+ * not.
+ *
+ */
+
+
 namespace Ceylan
 {
 
 
-	/**
-	 * This is the libtool version of the Ceylan headers, as defined in the
-	 * configure step.
-	 *
-	 * Allows to detect run-time mismatches between the Ceylan headers a 
-	 * program or a library was compiled with, and the actual Ceylan library
-	 * it is then linked to.
-	 *
-	 */
-	CEYLAN_DLL const std::string actualCeylanHeaderLibtoolVersion 
-		= "0.7.0" ;
+  /**
+   * This is the libtool version of the Ceylan headers.
+   *
+   * Allows to detect run-time mismatches between the Ceylan headers a
+   * program or a library was compiled with, and the actual Ceylan library
+   * it is then linked to.
+   *
+   * @note Cannot declare here:
+   * 'extern CEYLAN_DLL const std::string actualCeylanHeaderLibtoolVersion
+   *  = CEYLAN_LIBTOOL_VERSION ;' because with Visual C++ it leads to
+   * multiple definitions for actualCeylanHeaderLibtoolVersion.
+   *
+   */
+  extern CEYLAN_DLL const std::string actualCeylanHeaderLibtoolVersion ;
 
-	
-	/*
-	 * NEVER check-in this file if there is no more CEYLAN_LIBTOOL_VERSION 
-	 * above!
-	 *
-	 */
-	 		
+
 }
+
 
 
 #endif // CEYLAN_HEADER_VERSION_H_
