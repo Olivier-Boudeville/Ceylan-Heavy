c29a5d690e8f02f85f8333aaf95fa1587a75af26
Olivier Boudeville <olivier.boudeville@esperide.com>
2010-02-19 14:12:43 +0000

    User interface facilities added.

---
+++ src/code/scripts/erlang/common/src/user-interface/gui.erl
@@ -0,0 +1,41 @@
+% Copyright (C) 2003-2010 Olivier Boudeville
+%
+% This file is part of the Ceylan Erlang library.
+%
+% This library is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License or
+% the GNU General Public License, as they are published by the Free Software
+% Foundation, either version 3 of these Licenses, or (at your option) 
+% any later version.
+% You can also redistribute it and/or modify it under the terms of the
+% Mozilla Public License, version 1.1 or later.
+%
+% This library is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+% GNU Lesser General Public License and the GNU General Public License
+% for more details.
+%
+% You should have received a copy of the GNU Lesser General Public
+% License, of the GNU General Public License and of the Mozilla Public License
+% along with this library.
+% If not, see <http://www.gnu.org/licenses/> and
+% <http://www.mozilla.org/MPL/>.
+%
+% Author: Olivier Boudeville (olivier.boudeville@esperide.com)
+% Creation date: Monday, February 15, 2010.
+
+
+% Gathering of various facilities for Graphical User Interface.
+% See gui_test.erl for the corresponding test.
+-module(gui).
+
+
+-include("polygon.hrl").
+
+
+% Rendering of GUI elements.
+-export([ draw_line/3, draw_line/4, draw_lines/2, draw_lines/3,
+		draw_cross/2, draw_cross/3, draw_polygon/2 ]).
+
+
