5118bf84387bb2e56b953b1ff6879f9aab8ff752
Olivier Boudeville <olivier.boudeville@esperide.com>
2010-03-06 23:10:08 +0000

    Minor operations added.

--- src/code/scripts/erlang/common/src/maths/linear_2D.erl
+++ src/code/scripts/erlang/common/src/maths/linear_2D.erl
@@ -32,24 +32,55 @@
 
 
 
-
 % Operations on points:
--export([ square_distance/2, distance/2, 
-		  cross_product/2, roundify/1, get_integer_center/2, get_center/2 ]).
+-export([ are_close/2, is_within/3, square_distance/2, distance/2, 
+		  cross_product/2, roundify/1, get_integer_center/2, get_center/2,
+		  translate/2 ]).
+
+	
+% Operations on vectors:
+-export([ vectorize/2, square_magnitude/1, magnitude/1, scale/2, make_unit/1,
+		  normal_left/1, normal_right/1, dot_product/2 ]).
+
+
+% Operations on lines:
+-export([ get_line/2, intersect/2 ]).
+
+
+% Operations related to angles:
+-export([ is_strictly_on_the_right/3, is_obtuse/1, abs_angle_rad/3, angle_rad/3,
+		  abs_angle_deg/3, angle_deg/3 ]).
+
 
 % Operations on set of points:
 -export([ compute_smallest_enclosing_rectangle/1,
 		  compute_max_overall_distance/1, compute_convex_hull/1 ]).
+
 	
-% Operations on vectors:
--export([ vectorize/2, square_magnitude/1, magnitude/1, scale/2, make_unit/1 ]).
-
-
 % Temp:
 -export([ find_pivot/1, sort_by_angle/2 ]).
-	
+
+
+
+% For epsilon:
+-include("math_utils.hrl").
+
+
 
 % Point section.
+
+
+% Returns whether the two specified points are close, i.e. if they could be
+% considered as representing the same point (equality operator on points).
+are_close( _P1={X1,Y1}, _P2={X2,Y2} ) ->
+	math_utils:are_close(X1,X2) andalso math_utils:are_close(Y1,Y2).
+
+
+% Tells whether point P is within a distance D from point C, using some margin
+% to overcome numerical errors.
+is_within( P, C, D ) ->
+	% "Taylor series", square(epsilon) is negligible here:
+	square_distance( P, C ) < D * (D + ?epsilon). 
 
 
 % Returns the square of the distance between the two specified points.
@@ -59,6 +90,7 @@ square_distance( {X1,Y1}, {X2,Y2} ) ->
 	XDiff = X2-X1,
 	YDiff = Y2-Y1,
 	XDiff*XDiff + YDiff*YDiff.
+
 
 
 % Returns the distance between the two specified points.
@@ -94,6 +126,10 @@ get_center( {X1,Y1}, {X2,Y2} ) ->
 	{ (X1+X2)/2, (Y1+Y2)/2 }.
 
 
+% Returns a point corresponding to the specified point P translated by the
+% specified vector V.
+translate( _P={X,Y}, _V={Vx,Vy} ) ->
+	{X+Vx,Y+Vy}.
 
 
 % Section for sets of points.
@@ -288,8 +324,8 @@ sort_by_angle( Pivot={Xp,Yp}, [Point={X,Y}|T], LeftPoints, MiddlePoint,
 										   RightPoints );
 
 						false ->
-					        % The current point can be dropped on the floor, as it is
-					        % below the highest middle point:
+					        % The current point can be dropped on the floor, as
+					        % it is below the highest middle point:
 							sort_by_angle( Pivot, T, LeftPoints, MiddlePoint, 
 										   RightPoints )
 								
@@ -318,115 +354,6 @@ reverse_and_drop_angle( [], Acc ) ->
 reverse_and_drop_angle( [{_Tangent,Point}|T], Acc ) ->
 	reverse_and_drop_angle( T, [Point|Acc] ).
 
-
-
-
-
-% Convex hull section.
-
-
-
-% Computes the convex hull corresponding to the specified list of points.
-compute_convex_hull( Points ) ->
-
-	{Pivot,RemainingPoints} = linear_2D:find_pivot( Points ),
-
-	case length(RemainingPoints) of 
-		
-		Len when Len < 2 ->
-			throw( not_enough_point_for_convex_hull );
-
-		_Other ->
-			% We have at least 2 points in addition to the pivot.
-			%io:format( "Pivot is ~w, remaining points: ~w.~n", 
-			%		   [Pivot,RemainingPoints] ),
-
-			[P1,P2|T] = sort_by_angle( Pivot, RemainingPoints ),
- 
-			% Initially only the pivot is known to belong to the convex hull.
-			% We had P1, next to be validated against P2.
-			% We also add the pivot to the end of the NextPoints list, so that
-			% the hull can be closed.
-			compute_graham_scan_hull( _ToValidate=[P1,Pivot], 
-									  _NewPoint=P2, _NextPoints=(T++[Pivot]) )
-
-	end.
-
-
-
-
-% Computes the Graham scan for the specified list of points, expected to be
-% already sorted by increasing angle between the abscissa axis and the vector
-% from the pivot to each of these points (i.e. in increasing order of the angle
-% they and the point P make with the x-axis, in counter-clockwise order).
-% See: http://en.wikipedia.org/wiki/Graham_scan
-% Returns the corresponding convex hull, in clock-wise order.
-compute_graham_scan_hull( ToValidate, _Pivot, _NextPoints=[] ) ->
-	% Last new point is by construction always to pivot.
-
-	%io:format( "compute_graham_scan_hull: "
-	%		   "exhausted input points, returning: ~w.~n", [ToValidate] ),
-
-	ToValidate;
-
-compute_graham_scan_hull( ToValidate=[P2,P1|T], NewPoint, 
-						  NextPoints=[Next|OtherNext] ) ->
-
-	case is_on_the_right( P2, P1, NewPoint ) of
-
-		true ->
-			
-			%io:format( "compute_graham_scan_hull: point ~w is on the right of "
-			%		   "segment from ~w to ~w, keeping ~w.~n", 
-			%		   [P2, P1, NewPoint, P2] ),
- 
-			% Here, the point P2 is on the right of the segment going from P1 to
-			% the Next point, thus P2 can be kept and we can continue with the
-			% next points:
-			compute_graham_scan_hull( [NewPoint|ToValidate], Next,
-									   OtherNext );
-		
-		false ->
-
-			%io:format( "compute_graham_scan_hull: point ~w is on the left of "
-			%		   "segment from ~w to ~w, eliminating ~w.~n", 
-			%		   [P2, P1, NewPoint, P2] ),
-
-			% Here, the point P2 is on the left of (or in) the segment going
-			% from P1 to the Next point, thus P2 is to be discarded, and will
-			% have to check predecessor(s) of P2 against the Next point.  
-			%
-
-			compute_graham_scan_hull( [P1|T], NewPoint, NextPoints )
-
-
-	end;
-	
-% Note however that the first point examined after the pivot (FP1) may have to
-% be discarded because of the second. If we just removed FP1, then the
-% ToValidate list would just contain the pivot, thus triggering a function
-% clause. In that case we just have to replace FP1 by the next(FP1)=P2 here, and
-% thus ToValidate will always contain at least two elements:
-
-% This clause matches whenever we just removed the first point in the list
-% examined after the pivot. So the first parameter (ToValidate) is just a list
-% with one element, the pivot, that was added to close the hull. As we have no
-% intermediate point, we accept directly the next point (Next), knowing it will
-% be checked at the next recursion:
-%compute_graham_scan_hull( [Pivot], NewPoint, [Next|OtherNext] ) ->
-%	compute_graham_scan_hull( [NewPoint,Pivot], Next, OtherNext ).
-% A bit faster as we know L is actually [Pivot]:
-compute_graham_scan_hull( L, NewPoint, [Next|OtherNext] ) ->
-	compute_graham_scan_hull( [NewPoint|L], Next, OtherNext ).
-	
-
-% Returns whether P is on the left of the segment going from P1 to P2. Should P
-% be on that segment, then P will be considered as not being on the left: in the
-% convex hull, only necessary points will be kept, i.e. no point on the boundary
-% of the hull will be kept.
-is_on_the_right( _P={X,Y}, _P1={X1,Y1}, _P2={X2,Y2} ) ->	
-	DotProduct = (X2-X1)*(Y-Y1) - (Y2-Y1)*(X-X1),
-	DotProduct > 0.
 
 
 
@@ -465,5 +392,283 @@ make_unit( {0,0} ) ->
 
 make_unit( V ) ->
 	scale( V, 1/magnitude(V) ).
+
+
+
+% Returns a (non-unit) vector which is normal to specified vector V, and is on
+% the left of V in the standard basis.
+normal_left( _V={X,Y} ) ->
+	{-Y,X}.
+
+
+% Returns a (non-unit) vector which is normal to specified vector V, and is on
+% the right of V in the standard basis.
+normal_right( _V={X,Y} ) ->
+	{Y,-X}.
+
+
+% Returns the dot product of the two specified vectors.
+dot_product( _V1={X1,Y1}, _V2={X2,Y2} ) ->
+	X1*X2 + Y1*Y2.
+
+
+
+
+% Line section.
+
+
+% Returns the three coefficients {A,B,C} for the line passing by point P and
+% being perpendicular to vector V, whose equation A.x+B.y+C=0.
+get_line( _P={Xp,Yp}, _V={Vx,Vy} ) ->
+	% Here we know that:
+	% P is on the line: A.Xp+B.Yp+C=0  (I)
+	% Let M be (X,Y). If M is on the line, the PM.V=0 (null dot product), thus:
+	% (X-Xp)*Vx+(Y-Yp)*Vy=0 (II)
+	% We want to determine A, B and C:
+	% (II) is: Vx.X + Vy*Y - (Xp.Vx+Yp.Vy) = 0 thus:
+	A=Vx,
+	B=Vy,
+	C = - (Xp*Vx+Yp*Vy),
+	{A,B,C}.
+	
+
+% Returns the intersection of the two specified lines, if it is a point,
+% otherwise the atom no_point (the intersection can be void, if the lines are
+% parallel but different, or a full line, if they are the same line).
+% First line has for equation a.x+b.y+c=0, second has for equation u.x+v.y+w=0.
+intersect( _D1={A,B,C}, _D2={U,V,W} ) ->
+	% We will try to substitute y, as determined from first equation, into
+	% the second one:
+	case B of 
+		
+		0 ->
+			% Thus A.X = -C
+			case A of 
+				
+				0 ->
+					% Either empty or the same:
+					no_point;
+
+				_ ->
+					X = -C/A,
+					case V of 
+						
+						0 ->
+							no_point;
+
+						_ ->
+							Y=-(W+U*X)/V,
+							{X,Y}
+
+					end
+
+			end;
+
+		_ ->
+			% General case: Y= - (C+A.X)/B (I), will be replaced in second
+			% equation: 
+			case U of
+
+				0 ->
+					% Thus Y:
+					Y= -W/V,
+					% Now X from first:
+					case A of 
+
+						0 ->
+							no_point;
+						
+						_ ->
+							X= - (B*Y+C)/A,
+							{X,Y}
+
+					end;
+
+				_ ->
+					% General case, substituing (I) in second equation we have:
+					% (B.U-V.A).X = V.C-B.D 
+					case B*U-V*A of 
+
+						0 ->
+							no_point;
+
+						Denom ->
+							X = (V*C-B*W) / Denom,
+							Y = - (C+A*X) / B,
+							{X,Y}
+
+					end	
+
+			end
+
+	end.
+							
+
+% Angle section.
+
+
+% Returns true iff P is strictly on the right of the oriented segment going from
+% P1 to P2.
+is_strictly_on_the_right( P, P1, P2 ) ->	
+	Vec_P1P2 = vectorize( P1, P2 ),
+	RightNormal = normal_right( Vec_P1P2 ),
+	Vec_P1P  = vectorize( P1, P ),
+	DotProduct = dot_product( Vec_P1P, RightNormal ),
+	DotProduct > 0.
+
+
+% Returns whether specified angle (in degrees, canonical form) is obtuse.
+is_obtuse( AngleInDegrees ) ->
+
+	AngleInDegrees > 90 andalso AngleInDegrees < 180.
+
+
+% Returns the angle, in radians, between the vector AB and AC. 
+% Note: with this function we cannot tell whether one vector is ahead of the
+% other, i.e. if we should use the returned angle or its opposite to go from AB
+% to AC.
+abs_angle_rad( A, B, C ) ->
+	AB = vectorize(A,B),
+	AC = vectorize(A,C),
+	math:acos( dot_product( AB, AC )/ ( magnitude(AB)*magnitude(AC) ) ).
+
+
+% Returns the signed (oriented) angle, in radians, between the vector AB and AC.
+% Note: with this function we can tell that we must rotate counter-clockwise of
+% the returned angle to go from AB to AC.
+angle_rad( A, B, C ) ->
+	{X1,Y1} = vectorize(A,B),
+	{X2,Y2} = vectorize(A,C),
+	math:atan2( Y2, X2 ) - math:atan2( Y1, X1 ). 
+
+
+% Returns the angle, in canonical degrees, between the vector AB and AC. 
+% Note: with this function we cannot tell whether one vector is ahead of the
+% other, i.e. if we should use the returned angle or its opposite to go from AB
+% to AC.
+abs_angle_deg( A, B, C ) ->
+	 math_utils:canonify( math_utils:radian_to_degree( 
+							abs_angle_rad( A, B, C ) ) ).
+
+
+
+% Returns the signed (oriented) angle, in canonical degrees, between the vector
+% AB and AC.
+% Note: with this function we can tell that we must rotate counter-clockwise of
+% the returned angle to go from AB to AC.
+angle_deg( A, B, C ) ->
+	 math_utils:canonify( math_utils:radian_to_degree( 
+							angle_rad( A, B, C ) ) ).
+
+
+
+
+% Convex hull section.
+
+
+
+% Computes the convex hull corresponding to the specified list of points.
+% Returns the list of points that defines the hull.
+compute_convex_hull( Points ) ->
+
+	{Pivot,RemainingPoints} = find_pivot( Points ),
+
+	case length(RemainingPoints) of 
+		
+		Len when Len < 2 ->
+			throw( not_enough_points_for_convex_hull );
+
+		_Other ->
+			% We have at least 2 points in addition to the pivot.
+			%io:format( "Pivot is ~w, remaining points: ~w.~n", 
+			%		   [Pivot,RemainingPoints] ),
+
+			[P1,P2|T] = sort_by_angle( Pivot, RemainingPoints ),
+ 
+			% Initially only the pivot is known to belong to the convex hull.
+			% We had P1, next to be validated against P2.
+			% We also add the pivot to the end of the NextPoints list, so that
+			% the hull can be closed.
+			compute_graham_scan_hull( _ToValidate=[P1,Pivot], 
+									  _NewPoint=P2, _NextPoints=(T++[Pivot]) )
+
+	end.
+
+
+
+
+% Computes the Graham scan for the specified list of points, expected to be
+% already sorted by increasing angle between the abscissa axis and the vector
+% from the pivot to each of these points (i.e. in increasing order of the angle
+% they and the point P make with the x-axis, in counter-clockwise order).
+% See: http://en.wikipedia.org/wiki/Graham_scan
+% Returns the corresponding convex hull, in clock-wise order.
+compute_graham_scan_hull( ToValidate, _Pivot, _NextPoints=[] ) ->
+	% Last new point is by construction always to pivot.
+
+	%io:format( "compute_graham_scan_hull: "
+	%		   "exhausted input points, returning: ~w.~n", [ToValidate] ),
+
+	ToValidate;
+
+compute_graham_scan_hull( ToValidate=[P2,P1|T], NewPoint, 
+						  NextPoints=[Next|OtherNext] ) ->
+	
+	% Should P2 be on the line defined by P1 and NewPoint, then P2 will be
+	% considered as not being on the left: in the convex hull, only necessary
+	% points will be kept, i.e. no point on the boundary of the hull will be
+	% kept.
+	% Note: The test seems to be wrongly negated; however it is correct as
+	% actually we describe the algorithm as seen if represented in a basis whose
+	% ordinates are increasing when going from the top to the bottom (i.e. like
+	% when rendered on a classical GUI, the Y axis going then downward).
+	case is_strictly_on_the_right( P2, P1, NewPoint ) of
+
+		false ->
+			
+			%io:format( "compute_graham_scan_hull: point ~w is on the right of "
+			%		   "segment from ~w to ~w, keeping ~w.~n", 
+			%		   [P2, P1, NewPoint, P2] ),
+ 
+			% Here, the point P2 is on the right of the segment going from P1 to
+			% the Next point, thus P2 can be kept and we can continue with the
+			% next points:
+			compute_graham_scan_hull( [NewPoint|ToValidate], Next,
+									   OtherNext );
+		
+		true ->
+
+			%io:format( "compute_graham_scan_hull: point ~w is on the left of "
+			%		   "segment from ~w to ~w, eliminating ~w.~n", 
+			%		   [P2, P1, NewPoint, P2] ),
+
+			% Here, the point P2 is on the left of (or in) the segment going
+			% from P1 to the Next point, thus P2 is to be discarded, and will
+			% have to check predecessor(s) of P2 against the Next point.  
+			%
+
+			compute_graham_scan_hull( [P1|T], NewPoint, NextPoints )
+
+
+	end;
+	
+% Note however that the first point examined after the pivot (FP1) may have to
+% be discarded because of the second. If we just removed FP1, then the
+% ToValidate list would just contain the pivot, thus triggering a function
+% clause. In that case we just have to replace FP1 by the next(FP1)=P2 here, and
+% thus ToValidate will always contain at least two elements:
+
+% This clause matches whenever we just removed the first point in the list
+% examined after the pivot. So the first parameter (ToValidate) is just a list
+% with one element, the pivot, that was added to close the hull. As we have no
+% intermediate point, we accept directly the next point (Next), knowing it will
+% be checked at the next recursion:
+%compute_graham_scan_hull( [Pivot], NewPoint, [Next|OtherNext] ) ->
+%	compute_graham_scan_hull( [NewPoint,Pivot], Next, OtherNext ).
+% A bit faster as we know L is actually [Pivot]:
+compute_graham_scan_hull( L, NewPoint, [Next|OtherNext] ) ->
+	compute_graham_scan_hull( [NewPoint|L], Next, OtherNext ).
+	
+
 
 
