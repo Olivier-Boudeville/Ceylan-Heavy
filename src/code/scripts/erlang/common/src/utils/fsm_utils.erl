b11c1c7c06f46fbd8c7db981e7780d189d98c06d
Olivier Boudeville <olivier.boudeville@esperide.com>
2010-08-07 09:33:30 +0000

    Minor update.

--- src/code/scripts/erlang/common/src/utils/fsm_utils.erl
+++ src/code/scripts/erlang/common/src/utils/fsm_utils.erl
@@ -1,12 +1,11 @@
-% 
-% Copyright (C) 2003-2009 Olivier Boudeville
+% Copyright (C) 2003-2010 Olivier Boudeville
 %
 % This file is part of the Ceylan Erlang library.
 %
 % This library is free software: you can redistribute it and/or modify
 % it under the terms of the GNU Lesser General Public License or
 % the GNU General Public License, as they are published by the Free Software
-% Foundation, either version 3 of these Licenses, or (at your option) 
+% Foundation, either version 3 of these Licenses, or (at your option)
 % any later version.
 % You can also redistribute it and/or modify it under the terms of the
 % Mozilla Public License, version 1.1 or later.
@@ -28,6 +27,9 @@
 
 
 % Gathering of various convenient facilities.
+%
+% Note: mostly useless and deprecated.
+%
 % See fsm_utils_test.erl for the corresponding test.
 -module(fsm_utils).
 
@@ -42,6 +44,7 @@
 
 
 % Creates an attribute table appropriate to store a FSM state.
+%
 % setFsmAttribute, getFsmAttribute and getFsmAttr are to be used with these
 % variables too.
 create_blank_fsm_state() ->
@@ -49,21 +52,21 @@ create_blank_fsm_state() ->
 
 
 % Sets specified FSM state attribute.
-setFsmAttribute(FsmState,AttributeName,AttributeValue) ->
+setFsmAttribute( FsmState, AttributeName, AttributeValue ) ->
 	hashtable:addEntry( AttributeName, AttributeValue, FsmState ).
 
 
 % Retrieves specified FSM state attribute.
-% Return either the value, if the attribute is found, or 
-% { attribute_not_found, AttributeName }.			
-getFsmAttribute(FsmState,AttributeName) ->
+%
+% Returns either the value, if the attribute is found, or { attribute_not_found,
+% AttributeName }.
+getFsmAttribute( FsmState, AttributeName ) ->
 	case hashtable:lookupEntry( AttributeName, FsmState) of
-		
+
 		undefined ->
 			{ attribute_not_found, AttributeName } ;
-			
+
 		{value,Value} ->
 			Value
-			
+
 	end.
-				
