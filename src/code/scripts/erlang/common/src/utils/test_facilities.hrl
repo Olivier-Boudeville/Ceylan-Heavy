ef7b25c6ad3f8ea4f5506f3a8bcf8d2e72634a6a
Olivier Boudeville <olivier.boudeville@esperide.com>
2010-11-20 12:46:06 +0000

    Restored correct version.

--- src/code/scripts/erlang/common/src/utils/test_facilities.hrl
+++ src/code/scripts/erlang/common/src/utils/test_facilities.hrl
@@ -1,5 +1,4 @@
-%
-% Copyright (C) 2003-2009 Olivier Boudeville
+% Copyright (C) 2003-2010 Olivier Boudeville
 %
 % This file is part of the Ceylan Erlang library.
 %
@@ -36,15 +35,20 @@
 
 -ifdef(ExitAfterTest).
 
-testFinished() ->
-	io:format( "(test finished, interpreter halted)~n" ),	
+test_finished() ->
+	io:format( "--> End of test for modules ~p.~n", [ ?Tested_modules ] ),
+	% Cannot be here as we are not in WOOPER: check_pending_wooper_results(),
+	io:format( "(test finished, interpreter halted)~n" ),
+	% To ensure all outputs are indeed performed before the VM is halted:
+	timer:sleep(500),
 	erlang:halt().
-	
+
 -else.
 
-testFinished() ->
+test_finished() ->
+	io:format( "--> End of test for modules ~p.~n", [ ?Tested_modules ] ),
+	% Cannot be here as we are not in WOOPER: check_pending_wooper_results(),
 	io:format( "(test finished, interpreter still running)~n" ),
 	test_success.
-	
--endif.
 
+-endif.
