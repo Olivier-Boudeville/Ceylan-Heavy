98b840f2a251a6025403f6d5600a0126e337ce29
Olivier Boudeville <olivier.boudeville@esperide.com>
2014-04-20 16:01:19 +0200

    Made Python3-compliant.

--- src/code/scripts/python/startup.py
+++ src/code/scripts/python/startup.py
@@ -1,4 +1,4 @@
 import rlcompleter
 import readline
 readline.parse_and_bind( "tab: complete" )
-print "Python initialized according to Ceylan's standards."
+print( "Python initialized according to Ceylan's standards." )
